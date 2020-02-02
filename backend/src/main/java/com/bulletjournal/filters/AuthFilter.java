f438f2b4e040b92da2eccd50b4f97bebed6ade3e
Xin Yao <singerdmx@gmail.com>
2020-02-01 19:24:06 -0800

    Update AuthFilter

--- backend/src/main/java/com/bulletjournal/filters/AuthFilter.java
+++ backend/src/main/java/com/bulletjournal/filters/AuthFilter.java
@@ -5,7 +5,6 @@ import java.util.Enumeration;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -14,9 +13,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class AuthFilter implements Filter {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthFilter.class);
 
