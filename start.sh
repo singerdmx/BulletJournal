8209da274a03871f1d7c4add64b74eb27a926121
mwangxx0129 <mwangxx@gmail.com>
2020-02-02 05:14:42 -0500

    add log volumne

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 echo -e '\n==>step 1 / 2: create data volume mount points'
 mkdir -p "$HOME/docker/volumes/postgres"
+mkdir -p "$HOME/docker/volumes/log"
 
 echo -e '\n==>step 2 / 2: docker-compose up -d'
 docker-compose up -d
