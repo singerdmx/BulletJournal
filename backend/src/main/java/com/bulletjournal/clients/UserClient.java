cc8f1a4b8e66bbb80f6013e1808f2d78e2037b5c
Xin Yao <singerdmx@gmail.com>
2020-02-08 14:51:13 -0800

    Update UserClient to use header based auth

--- backend/src/main/java/com/bulletjournal/clients/UserClient.java
+++ backend/src/main/java/com/bulletjournal/clients/UserClient.java
@@ -6,6 +6,9 @@ import java.util.LinkedHashMap;
 
 import com.bulletjournal.config.AuthConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,54 +18,59 @@ import com.bulletjournal.controller.models.User;
 @Component
 public class UserClient {
 
-	public static final String USER_NAME_KEY = "discourse-user-name";
-	private static final String AVATAR_SIZE = "75";
-	private static final String THUMBNAIL_SIZE = "37";
-	private static final String SIZE_HOLDER = "{size}";
-	
-	private final RestTemplate restClient;
-	private final URI ssoEndPoint;
-	private final String ssoAPIParams;
-	private final AuthConfig authConfig;
-	
-	@Autowired
-	public UserClient(SSOConfig ssoConfig, AuthConfig authConfig) throws URISyntaxException {
-		this.restClient = new RestTemplate();
-		this.ssoEndPoint = new URI(ssoConfig.getEndpoint());
-		this.ssoAPIParams = ssoConfig.getAPIKey() == null ? null :
-				"?api_username=system&api_key=" + ssoConfig.getAPIKey();
-		this.authConfig = authConfig;
-	}
+    public static final String USER_NAME_KEY = "discourse-user-name";
+    private static final String AVATAR_SIZE = "75";
+    private static final String THUMBNAIL_SIZE = "37";
+    private static final String SIZE_HOLDER = "{size}";
 
-	@SuppressWarnings("rawtypes")
-	public User getUser(String username) {
-		// TODO: get user info from redis
+    private final RestTemplate restClient;
+    private final URI ssoEndPoint;
+    private final String ssoAPIKey;
+    private final AuthConfig authConfig;
 
-		String relativePath = "/u/" + username + ".json";
-		if (this.ssoAPIParams != null) {
-			relativePath += this.ssoAPIParams;
-		}
-		String url = this.ssoEndPoint.resolve(relativePath).toString();
-		LinkedHashMap userInfo = (LinkedHashMap) this.restClient.getForObject(url, LinkedHashMap.class).get("user");
-		String avatarTemplate = (String) userInfo.get("avatar_template");
-		String avatar = this.ssoEndPoint.resolve(avatarTemplate.replace(SIZE_HOLDER, AVATAR_SIZE)).toString();
-		String thumbnail = this.ssoEndPoint.resolve(avatarTemplate.replace(SIZE_HOLDER, THUMBNAIL_SIZE)).toString();
-		String timezone = null;
-		if (this.ssoAPIParams != null) {
-			// If user never sets timezone in sso provider, it defaults to "America/Los_Angeles"
-			timezone = (String) ((LinkedHashMap) userInfo.get("user_option")).get("timezone");
-		}
-		if (timezone == null) {
-			timezone = this.authConfig.getDefaultUserTimezone();
-		}
+    @Autowired
+    public UserClient(SSOConfig ssoConfig, AuthConfig authConfig) throws URISyntaxException {
+        this.restClient = new RestTemplate();
+        this.ssoEndPoint = new URI(ssoConfig.getEndpoint());
+        this.ssoAPIKey = ssoConfig.getAPIKey();
+        this.authConfig = authConfig;
+    }
 
-		String email = this.authConfig.getDefaultUserEmail();
-		if (this.ssoAPIParams != null) {
-			url = this.ssoEndPoint.resolve( "/u/" + username + "/emails.json" + this.ssoAPIParams).toString();
-			email = (String) this.restClient.getForObject(url, LinkedHashMap.class).get("email");
-		}
+    @SuppressWarnings("rawtypes")
+    public User getUser(String username) {
+        // TODO: get user info from redis
 
-		return new User(username, thumbnail, avatar, timezone, email);
-	}
-	
+        HttpEntity<LinkedHashMap> request = null;
+        if (this.ssoAPIKey != null) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Api-Username", "system");
+            headers.add("Api-Key", this.ssoAPIKey);
+            // build the request
+            request = new HttpEntity(headers);
+        }
+        String url = this.ssoEndPoint.resolve("/u/" + username + ".json").toString();
+        LinkedHashMap userInfo = (LinkedHashMap) this.restClient
+                .exchange(url, HttpMethod.GET, request, LinkedHashMap.class).getBody().get("user");
+        String avatarTemplate = (String) userInfo.get("avatar_template");
+        String avatar = this.ssoEndPoint.resolve(avatarTemplate.replace(SIZE_HOLDER, AVATAR_SIZE)).toString();
+        String thumbnail = this.ssoEndPoint.resolve(avatarTemplate.replace(SIZE_HOLDER, THUMBNAIL_SIZE)).toString();
+        String timezone = null;
+        if (this.ssoAPIKey != null) {
+            // If user never sets timezone in sso provider, it defaults to "America/Los_Angeles"
+            timezone = (String) ((LinkedHashMap) userInfo.get("user_option")).get("timezone");
+        }
+        if (timezone == null) {
+            timezone = this.authConfig.getDefaultUserTimezone();
+        }
+
+        String email = this.authConfig.getDefaultUserEmail();
+        if (this.ssoAPIKey != null) {
+            url = this.ssoEndPoint.resolve("/u/" + username + "/emails.json").toString();
+            email = (String) this.restClient
+                    .exchange(url, HttpMethod.GET, request, LinkedHashMap.class).getBody().get("email");
+        }
+
+        return new User(username, thumbnail, avatar, timezone, email);
+    }
+
 }
