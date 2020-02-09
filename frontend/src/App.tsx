35903438a999a26d4036ce6cd6b9d0a88824f3c4
Rongkai Liu <milkrong121@outlook.com>
2020-02-08 17:43:57 -0800

    Layout application  with a side menu and top nav and main content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Layout, Menu, Input } from 'antd';
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
+const { Search } = Input;
+
 class App extends React.Component {
-  render () {
+  render() {
     return (
       <div className="App">
-        Main
+        <Layout className="layout">
+          <Sider width={200} style={{ background: '#fff' }}>
+            <div className="icon">
+              <img src="favicon466.ico" alt="Icon" className="icon-img" />
+            </div>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['main']}
+              style={{height: '100%' }}
+            >
+              <Menu.Item key="main">Main</Menu.Item>
+              <Menu.Item key="next">Next</Menu.Item>
+            </Menu>
+          </Sider>
+            <Layout>
+              <Header className="header">
+                <div className="search-box">
+                  <Search />
+                </div>
+                <div>User Info</div>
+              </Header>
+              <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                Content
+              </Content>
+              <Footer style={{ textAlign: 'center' }}>
+                Bullet Journal ©2020 Created by{' '}
+              </Footer>
+            </Layout>
+        </Layout>
       </div>
-    )
+    );
   }
 }
 
