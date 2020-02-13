59309e9fcc6736ef99389340d39c7fcca73ea14d
Rongkai Liu <milkrong121@outlook.com>
2020-02-13 10:32:45 -0800

    fix issue in api call to resolve response

--- frontend/src/apis/userApis.ts
+++ frontend/src/apis/userApis.ts
@@ -1,10 +1,13 @@
-import { DEV } from "../config";
-import { doFetch } from "./api-helper";
+import { doFetch } from './api-helper';
 
 export const fetchUserInfo = () => {
-  return doFetch("/api/myself", "GET");
+  return doFetch('/api/myself', 'GET')
+    .then(res => res.json())
+    .catch(err => {
+      throw Error(err);
+    });
 };
 
 export const logoutUser = () => {
-  return doFetch("/api/myself/logout", "POST");
+  return doFetch('/api/myself/logout', 'POST');
 };
