c8cd4b5d568a51361b7db70115b3a05cbd6503bf
Rongkai Liu <milkrong121@outlook.com>
2020-02-13 04:17:53 -0800

    add modal form to create todo

--- frontend/src/pages/todo.pages.tsx
+++ frontend/src/pages/todo.pages.tsx
@@ -1,29 +1,74 @@
 import React from 'react';
 import TodoItem from '../components/todo-item/todo-item.component';
+import { List, Icon, Modal, Form, Input } from 'antd';
 import { RouteComponentProps } from 'react-router-dom';
-import { List, Icon } from 'antd';
 
-interface RouteParams {
+type RouteParams = {
+  category: string;
+};
+interface RouteProps extends RouteComponentProps<RouteParams> {
   category: string;
 }
 
-const fakeData = ['Frontend', 'Bakcend', 'UI designer'];
-const TodoPage = (props: RouteComponentProps<RouteParams>) => {
-  return (
-    <div className="todo">
-      <div className="todo-header">
-        <h2>{props.match.params.category.toUpperCase()} </h2>
-        <h2 className="add-todo-button"><Icon type="plus" />ADD</h2>
-      </div>
-      <div className="todo-list">
-        <List
-          itemLayout="horizontal"
-          dataSource={fakeData}
-          renderItem={item => <TodoItem title={item} />}
-        />
-      </div>
-    </div>
-  );
+type todoState = {
+  showForm: boolean;
 };
+
+const fakeData = ['Frontend', 'Bakcend', 'UI designer'];
+class TodoPage extends React.Component<RouteProps, todoState> {
+  state: todoState = {
+    showForm: false
+  };
+
+  showForm = () => {
+    this.setState({ showForm: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ showForm: false });
+  };
+
+  handleCreate = () => {
+    this.setState({ showForm: false });
+  };
+
+  render() {
+    return (
+      <div className="todo">
+        <div className="todo-header">
+          <h2>{this.props.match.params.category.toUpperCase()} </h2>
+          <h2 className="add-todo-button" onClick={this.showForm}>
+            <Icon type="plus" />
+            ADD
+          </h2>
+        </div>
+        <div className="todo-list">
+          <List
+            itemLayout="horizontal"
+            dataSource={fakeData}
+            renderItem={item => <TodoItem title={item} />}
+          />
+        </div>
+        <Modal
+          title="Create Task"
+          visible={this.state.showForm}
+          onOk={this.handleCreate}
+          onCancel={this.handleCancel}
+          centered={true}
+          okText="Create"
+        >
+          <Form>
+            <Form.Item>
+              <Input placeholder="titie" />
+            </Form.Item>
+            <Form.Item>
+              <Input.TextArea allowClear={true} />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </div>
+    );
+  }
+}
 
 export default TodoPage;
