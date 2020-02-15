22a7aa90d1fe3947b5b73d734e94b4448fbd8c01
Rongkai Liu <milkrong121@outlook.com>
2020-02-15 08:31:25 -0800

    add notification reducer to store

--- frontend/src/store/reducers.ts
+++ frontend/src/store/reducers.ts
@@ -1,6 +1,8 @@
 import { reducer as userReducer } from '../features/user-info/reducer';
+import { reducer as notificationReducer } from '../features/notification/reducer'
 
 
 export default {
-  user: userReducer
+  user: userReducer,
+  notifications: notificationReducer
 };
