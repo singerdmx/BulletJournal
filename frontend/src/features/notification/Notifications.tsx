e93e6db0519287c2998ef071650f0234426ba045
Rongkai Liu <milkrong121@outlook.com>
2020-02-16 12:35:12 -0800

    mark as read and time before in notification need to reinstall npm

--- frontend/src/features/notification/Notifications.tsx
+++ frontend/src/features/notification/Notifications.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { IState } from '../../store';
 import { connect } from 'react-redux';
 import { updateNotifications, Notification } from './reducer';
-import { List, Avatar } from 'antd';
+import { List, Avatar, Radio } from 'antd';
+import * as moment from 'moment';
+
+import './notification.styles.less';
 
 type NotificationsProps = {
   notifications: Notification[];
@@ -12,25 +15,27 @@ type NotificationsProps = {
 type titleProps = {
   title: String;
   type: String;
+  time: Number;
 };
 
-const ListTitle = ({ title, type }: titleProps) => {
-  if (type === 'JoinGroupEvent') {
-    const titleList = title.split(' ');
-    const name = titleList[0];
-    const project = titleList[titleList.length - 1];
-    const rest = titleList.slice(1, titleList.length - 1).join(' ');
-    return (
+const ListTitle = ({ title, type, time }: titleProps) => {
+  const titleList = title.split(' ');
+  const name = titleList[0];
+  const project = titleList[titleList.length - 1];
+  const rest = titleList.slice(1, titleList.length - 1).join(' ');
+
+  return type === 'JoinGroupEvent' ? (
+    <div className="notification-title">
       <span>
         <strong>{name}</strong> {rest} <strong>{project}</strong>
       </span>
-    );
-  }
-
-  return (
-      <span>
-        title
-      </span>
+      <span>{moment(time).fromNow()}</span>
+    </div>
+  ) : (
+    <div className="notification-title">
+      <span>{title}</span>
+      <span>{moment(time).fromNow()}</span>
+    </div>
   );
 };
 
@@ -45,10 +50,16 @@ class NotificationList extends React.Component<NotificationsProps> {
         itemLayout="horizontal"
         dataSource={this.props.notifications}
         renderItem={item => (
-          <List.Item>
+          <List.Item extra={<Radio></Radio>}>
             <List.Item.Meta
               avatar={<Avatar src={item.originator.avatar} />}
-              title={<ListTitle title={item.title} type={item.type}/>}
+              title={
+                <ListTitle
+                  title={item.title}
+                  type={item.type}
+                  time={item.timestamp}
+                />
+              }
               description={item.content ? item.content : ''}
             />
           </List.Item>
