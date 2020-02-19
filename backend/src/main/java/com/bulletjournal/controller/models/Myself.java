070ac9dab2b3ff5ae63a7d6f1f5e10c82fd9fe75
Xin Yao <singerdmx@gmail.com>
2020-02-18 22:43:50 -0800

    Add updateMyself API

---
+++ backend/src/main/java/com/bulletjournal/controller/models/Myself.java
@@ -0,0 +1,29 @@
+package com.bulletjournal.controller.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Myself extends User {
+
+    private String timezone;
+
+    public Myself() {
+    }
+
+    public Myself(User user, String timezone) {
+        super(user.getId(), user.getName(), user.getThumbnail(), user.getAvatar());
+        this.timezone = timezone;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public boolean hasTimezone() {
+        return this.timezone != null;
+    }
+}
