de82c944b7a8aedb89beefa6468e5739fbb1ce6b
Xin Yao <singerdmx@gmail.com>
2020-02-19 01:32:59 -0800

    Group name may have space. Update the split by space logic.

--- frontend/src/components/notification/list-title.component.tsx
+++ frontend/src/components/notification/list-title.component.tsx
@@ -2,42 +2,66 @@ import React from 'react';
 import moment from 'moment';
 
 type titleProps = {
-    title: string;
-    type: string;
-    time: number;
-  };
-  
-  const ListTitle = ({ title, type, time }: titleProps) => {
-    const titleList = title.split(' ');
-    switch (type) {
-      case 'JoinGroupEvent':
-      case 'RemoveUserFromGroupEvent':
-      case 'JoinGroupResponseEvent':
-        return (
-            <div className="notification-title">
-              <span>
-                <strong>{titleList[0]}</strong> {titleList.slice(1, titleList.length - 1).join(' ')} <strong>{titleList[titleList.length - 1]}</strong>
-              </span>
-              <span>{moment(time).fromNow()}</span>
-            </div>
-          );
-      case 'DeleteGroupEvent':
-        return (
-          <div className="notification-title">
-              <span>
-                {titleList[0]} <strong>{titleList[1]}</strong> {titleList.slice(2, titleList.length).join(' ')}
-              </span>
-              <span>{moment(time).fromNow()}</span>
-          </div>
-        );
-      default:
-          return (
-              <div className="notification-title">
-                  <span>{title}</span>
-                  <span>{moment(time).fromNow()}</span>
-              </div>
-          );
-    }
-  };
+  title: string;
+  type: string;
+  time: number;
+};
 
-  export default ListTitle;
+function getTitleText(title: string, type: string, time: number): JSX.Element {
+  let titleText = (<span>{title}</span>);
+  let beg = 0;
+  switch (type) {
+    case 'JoinGroupEvent':
+      beg = title.indexOf(' invited you to join Group ');
+      titleText = (
+        <span>
+          <strong>{title.slice(0, beg)}</strong> invited you to join Group <strong>{title.slice(beg + 27)}</strong>
+        </span>
+      );
+      break;
+    case 'RemoveUserFromGroupEvent':
+      beg = title.indexOf(' removed you from Group ');
+      titleText = (
+        <span>
+          <strong>{title.slice(0, beg)}</strong> removed you from Group <strong>{title.slice(beg + 24)}</strong>
+        </span>
+      );
+      break;
+    case 'JoinGroupResponseEvent':
+      beg = title.indexOf(' declined your invitation to join Group ');
+      titleText = (
+        <span>
+          <strong>{title.slice(0, beg)}</strong> declined your invitation to join Group <strong>{title.slice(beg + 40)}</strong>
+        </span>
+      );
+      if (beg < 0) {
+        beg = title.indexOf(' accepted your invitation to join Group ');
+        titleText = (
+          <span>
+            <strong>{title.slice(0, beg)}</strong> accepted your invitation to join Group <strong>{title.slice(beg + 40)}</strong>
+          </span>
+        );
+      }
+      break;
+    case 'DeleteGroupEvent':
+      beg = title.indexOf(' is deleted');
+      titleText = (
+        <span>
+          Group <strong>{title.slice(6, beg)}</strong> is deleted
+        </span>
+      );
+      break;
+  }
+  return titleText;
+}
+
+const ListTitle = ({ title, type, time }: titleProps) => {
+  return (
+    <div className="notification-title">
+        {getTitleText(title, type, time)}
+        <span>{moment(time).fromNow()}</span>
+    </div>
+  );
+};
+
+export default ListTitle;
