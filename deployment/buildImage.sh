80ed20588f870e52d391464c92c5b313dd594577
Xin Yao <singerdmx@gmail.com>
2020-02-18 16:52:26 -0800

    add buildImage script for deployment

---
+++ deployment/buildImage.sh
@@ -0,0 +1,14 @@
+cd ../discourse-auth-proxy
+docker build -t xcode1024/auth-proxy .
+
+cd ./frontend
+docker build -t xcode1024/bulletjournal-frontend .
+cd ..
+
+cd ./backend
+DOCKER_BUILDKIT=1 docker build -t xcode1024/bulletjournal-backend .
+cd ../deployment
+
+docker push xcode1024/auth-proxy:latest
+docker push xcode1024/bulletjournal-frontend:latest
+docker push xcode1024/bulletjournal-backend:latest
