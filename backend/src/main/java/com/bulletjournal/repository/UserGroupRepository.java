d606fdf937684df9c6b319fa16052e581dc0d524
mwangxx0129 <mwangxx@gmail.com>
2020-02-20 22:43:36 -0500

    fix user_group_group_accepted index

--- backend/src/main/java/com/bulletjournal/repository/UserGroupRepository.java
+++ backend/src/main/java/com/bulletjournal/repository/UserGroupRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 
 @Repository
 public interface UserGroupRepository extends JpaRepository<UserGroup, UserGroupKey> {
-    List<UserGroup> findAllByGroupId(Long groupId);
     List<UserGroup> findAllByGroupIdAndAccepted(Long groupId, Boolean accepted);
 }
