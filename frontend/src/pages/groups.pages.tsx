e876cf9353611d746e1341255da8c8cd827db476
Rongkai Liu <milkrong121@outlook.com>
2020-02-21 16:58:51 -0800

    build group cards

---
+++ frontend/src/pages/groups.pages.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { updateGroups } from '../features/group/actions';
+import { IState } from '../store';
+import { GroupsWithOwner } from '../features/group/interfaces';
+import GroupCard from '../components/group-card/group-card.component';
+
+type GroupsProps = {
+  groups: GroupsWithOwner[];
+  updateGroups: () => void;
+};
+
+class GroupsPage extends React.Component<GroupsProps> {
+  componentDidMount() {
+    this.props.updateGroups();
+  }
+
+  render() {
+    const groupsByOwner = this.props.groups;
+    return (
+      <div className="groups-page">
+        {groupsByOwner && groupsByOwner.map(groupOwner => {
+            return groupOwner.groups.map(group => (
+                <GroupCard group={group} />
+            ))
+        })}
+      </div>
+    );
+  }
+}
+  
+const mapStateToProps = (state: IState) => ({
+    groups: state.group.groups
+});
+
+export default connect(mapStateToProps, {updateGroups})(GroupsPage);
