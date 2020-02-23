d64f04088037d139afd2608564ceade86de392c8
Xin Yao <singerdmx@gmail.com>
2020-02-23 11:26:47 -0800

    updateExpandedMyself on settings mount

--- frontend/src/components/settings/account.tsx
+++ frontend/src/components/settings/account.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import TimezonePicker from './timezone';
 import { connect } from 'react-redux';
 import { IState } from '../../store';
+import { updateExpandedMyself } from '../../features/myself/actions';
 
-type AccountProps = {};
+type AccountProps = {
+  updateExpandedMyself: () => void;
+};
 
 class Account extends React.Component<AccountProps> {
+  componentDidMount() {
+    this.props.updateExpandedMyself();
+  }
   render() {
     return (
       <div style={{ display: 'flex', alignItems: 'center' }}>
@@ -19,4 +25,4 @@ const mapStateToProps = (state: IState) => ({
   timezone: state.myself.timezone
 });
 
-export default connect(mapStateToProps, {})(Account);
+export default connect(mapStateToProps, { updateExpandedMyself })(Account);
