d835409354ba86cec4fb66b187930ebcdfc5f1e7
Xin Yao <singerdmx@gmail.com>
2020-02-23 17:30:52 -0800

    Update getProjectItems API

--- backend/src/main/java/com/bulletjournal/controller/ProjectItemController.java
+++ backend/src/main/java/com/bulletjournal/controller/ProjectItemController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
@@ -15,7 +16,9 @@ public class ProjectItemController {
     protected static final String PROJECT_ITEMS_ROUTE = "/api/projectItems";
 
     @GetMapping(PROJECT_ITEMS_ROUTE)
-    public ProjectItems getProjectItems(@Valid @RequestParam List<ProjectType> types) {
+    public List<ProjectItems> getProjectItems(
+            @Valid @RequestParam List<ProjectType> types,
+            @NotBlank @RequestParam String timezone) {
         return null;
     }
 }
--- backend/src/main/java/com/bulletjournal/controller/models/ProjectItems.java
+++ backend/src/main/java/com/bulletjournal/controller/models/ProjectItems.java
@@ -2,16 +2,20 @@ package com.bulletjournal.controller.models;
 
 import com.bulletjournal.repository.models.Transaction;
 
-import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectItems {
 
-    @Valid
     private List<Task> tasks = new ArrayList<>();
-    @Valid
+
     private List<Transaction> transactions = new ArrayList<>();
+
+    @NotBlank
+    @Size(min = 10, max = 10)
+    private String date; // "yyyy-MM-dd"
 
     public List<Task> getTasks() {
         return tasks;
@@ -27,5 +31,13 @@ public class ProjectItems {
 
     public void setTransactions(List<Transaction> transactions) {
         this.transactions = transactions;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
     }
 }
