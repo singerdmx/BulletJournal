b51847a2ece02db5b5982ba20b864d9b79736e31
Michael Zhou <michael.zhouzzy@gmail.com>
2020-02-29 12:30:46 -0800

    Update Interval Helper Test with different zone

--- backend/src/test/java/com/bulletjournal/controller/utils/IntervalHelperTest.java
+++ backend/src/test/java/com/bulletjournal/controller/utils/IntervalHelperTest.java
@@ -3,6 +3,7 @@ package com.bulletjournal.controller.utils;
 import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static org.junit.Assert.*;
@@ -34,6 +35,30 @@ public class IntervalHelperTest {
         assertEquals(20, zonedEndDateTime.getDayOfMonth());
         assertEquals(11, zonedEndDateTime.getHour());
         assertEquals(5, zonedEndDateTime.getMinute());
+    }
+
+    @Test
+    public void testIntervalWithDifferentZone() {
+        String date = "2020-02-20";
+        String time = "11:05";
+        String timezone = "America/Los_Angeles";
+
+        ZonedDateTime zonedStartDateTime = IntervalHelper.getStartTime(date, time, timezone);
+        assertNotNull(zonedStartDateTime);
+        assertEquals(2020, zonedStartDateTime.getYear());
+        assertEquals(2, zonedStartDateTime.getMonthValue());
+        assertEquals(20, zonedStartDateTime.getDayOfMonth());
+        assertEquals(11, zonedStartDateTime.getHour());
+        assertEquals(5, zonedStartDateTime.getMinute());
+
+        ZoneId chinaZoneID= ZoneId.of("Asia/Shanghai");
+        ZonedDateTime zonedStartDateTimeInChina = zonedStartDateTime.withZoneSameInstant(chinaZoneID);
+        assertNotNull(zonedStartDateTime);
+        assertEquals(2020, zonedStartDateTimeInChina.getYear());
+        assertEquals(2, zonedStartDateTimeInChina.getMonthValue());
+        assertEquals(21, zonedStartDateTimeInChina.getDayOfMonth());
+        assertEquals(3, zonedStartDateTimeInChina.getHour());
+        assertEquals(5, zonedStartDateTimeInChina.getMinute());
     }
 
     @Test
