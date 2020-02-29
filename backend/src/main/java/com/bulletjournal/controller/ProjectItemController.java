02a6523cc046913d710b9a8255db54f5e017c605
Michael Zhou <michael.zhouzzy@gmail.com>
2020-02-28 20:38:18 -0800

    Add project items api, need to update unit test

--- backend/src/main/java/com/bulletjournal/controller/ProjectItemController.java
+++ backend/src/main/java/com/bulletjournal/controller/ProjectItemController.java
@@ -4,8 +4,10 @@ import com.bulletjournal.clients.UserClient;
 import com.bulletjournal.controller.models.ProjectItems;
 import com.bulletjournal.controller.models.ProjectType;
 import com.bulletjournal.controller.utils.IntervalHelper;
+import com.bulletjournal.controller.utils.ProjectItemsGrouper;
 import com.bulletjournal.repository.TaskDaoJpa;
 import com.bulletjournal.repository.TransactionDaoJpa;
+import com.bulletjournal.repository.models.Transaction;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +19,6 @@ import javax.validation.constraints.NotBlank;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class ProjectItemController {
@@ -33,33 +34,34 @@ public class ProjectItemController {
     @GetMapping(PROJECT_ITEMS_ROUTE)
     public List<ProjectItems> getProjectItems(
             @Valid @RequestParam List<ProjectType> types,
-            @NotBlank @RequestParam String date,
-            @RequestParam Optional<String> time,
+            @NotBlank @RequestParam String startDate,
+            @NotBlank @RequestParam String endDate,
+            @RequestParam String time,
             @NotBlank @RequestParam String timezone) {
 
         List<ProjectItems> projectItems = new ArrayList<>();
-
         String username = MDC.get(UserClient.USER_NAME_KEY);
-        ZonedDateTime startTime = IntervalHelper.getStartTime(date, time.orElseGet(() -> {
-            return null;
-        }), timezone);
-        ZonedDateTime endTime = IntervalHelper.getEndTime(date, time.orElseGet(() -> {
-            return null;
-        }), timezone);
+
+        // Set start time and end time
+        ZonedDateTime startTime = IntervalHelper.getStartTime(startDate, time, timezone);
+        ZonedDateTime endTime = IntervalHelper.getEndTime(endDate, time, timezone);
+
+        // Get dates between start time and end time
+        List<ZonedDateTime> intervalDates = IntervalHelper.getIntervalDateList(startTime, endTime);
 
         for (ProjectType projectType : types) {
             switch (projectType.getValue()) {
                 case 0: // Task
                     break;
                 case 2: // Ledger
-                    ProjectItems projectItem = new ProjectItems();
-                    projectItem.setTransactions(transactionDaoJpa.
-                            findTransactionsByInterval(username, startTime, endTime));
-                    projectItems.add(projectItem);
+                    List<Transaction> transactions = transactionDaoJpa
+                            .findTransactionsByInterval(username, startTime, endTime);
+                    ProjectItemsGrouper.groupTransactionsByDate(projectItems, transactions, intervalDates);
                     break;
                 default:
             }
         }
         return projectItems;
     }
+
 }
