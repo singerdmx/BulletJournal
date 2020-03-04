7209163b453797639f48e7bd3145b79d24461f8a
Rongkai Liu <milkrong121@outlook.com>
2020-03-04 00:13:10 -0800

    update style of project page and install draggable

--- frontend/src/pages/project.pages.tsx
+++ frontend/src/pages/project.pages.tsx
@@ -5,11 +5,7 @@ import { IState } from '../store';
 import { connect } from 'react-redux';
 import { Menu, Dropdown } from 'antd';
 import { getProject } from '../features/project/actions';
-import {
-  TeamOutlined,
-  MoreOutlined,
-  EditOutlined
-} from '@ant-design/icons';
+import { TeamOutlined, MoreOutlined, EditOutlined } from '@ant-design/icons';
 
 type ProjectPathParams = {
   projectId: string;
@@ -26,14 +22,22 @@ type ProjectPageProps = {
 
 const editMenu = (
   <Menu>
-    <Menu.Item key="changeName"><EditOutlined />&nbsp;Change Name</Menu.Item>
-    <Menu.Item key="changeGroup"><EditOutlined />&nbsp;Change Group</Menu.Item>
-    <Menu.Item key="changeDescription"><EditOutlined />&nbsp;Change Description</Menu.Item>
+    <Menu.Item key="changeName">
+      <EditOutlined />
+      &nbsp;Change Name
+    </Menu.Item>
+    <Menu.Item key="changeGroup">
+      <EditOutlined />
+      &nbsp;Change Group
+    </Menu.Item>
+    <Menu.Item key="changeDescription">
+      <EditOutlined />
+      &nbsp;Change Description
+    </Menu.Item>
   </Menu>
 );
 
 class ProjectPage extends React.Component<ProjectPageProps & ProjectPathProps> {
-
   componentDidMount() {
     const projectId = this.props.match.params.projectId;
     this.props.getProject(parseInt(projectId));
@@ -53,30 +57,32 @@ class ProjectPage extends React.Component<ProjectPageProps & ProjectPathProps> {
   render() {
     const { project } = this.props;
     return (
-      <div className='todo'>
-        <div className='todo-header'>
+      <div className="project">
+        <div className="project-header">
           <h2>{project.name}</h2>
-          <span className='add-todo-button'>
-            <h2 onClick={(e) => this.onClickGroup(project.group.id)}
-              title={project.group && `Group: ${project.group.name}`}>
-              <TeamOutlined />
-              {project.group && project.group.users.length}
-            </h2>
-          </span>
-          <Dropdown overlay={editMenu} trigger={['click']}>
-            <MoreOutlined title='Edit'/>
-          </Dropdown>
+          <div className="project-control">
+            <span className="add-project-button">
+              <h2
+                onClick={e => this.onClickGroup(project.group.id)}
+                title={project.group && `Group: ${project.group.name}`}
+              >
+                <TeamOutlined />
+                {project.group && project.group.users.length}
+              </h2>
+            </span>
+            <Dropdown overlay={editMenu} trigger={['click']}>
+              <h2 style={{cursor : 'pointer', marginLeft : '0.5em'}}><MoreOutlined title="Edit"/></h2>
+            </Dropdown>
+          </div>
         </div>
-        <div>
-          {project.description}
-        </div>
+        <div>{project.description}</div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state: IState) => ({
-  project: state.project.project,
+  project: state.project.project
 });
 
 export default connect(mapStateToProps, { getProject })(ProjectPage);
