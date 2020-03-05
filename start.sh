ee806a786c2827c9b146f96445a5805e5ec73a0c
mwangxx0129 <mwangxx@gmail.com>
2020-03-04 22:18:38 -0500

    fix path

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 echo -e '\n==>step 1 / 2: create data volume mount points'
 mkdir -p "$HOME/docker/volumes/postgres"
 mkdir -p "$HOME/docker/volumes/log"
+mkdir -p "$HOME/docker/volumes/elasticsearch"
 
 echo -e '\n==>step 2 / 2: docker-compose up -d'
 
