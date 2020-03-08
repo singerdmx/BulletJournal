5a030f734024b55a85dfaa5b5b9d20781b142fa1
Michael Zhou <michael.zhouzzy@gmail.com>
2020-03-07 19:00:05 -0800

    Etag Generator added SHA256 and ADLER32 support

--- backend/src/main/java/com/bulletjournal/controller/utils/EtagGenerator.java
+++ backend/src/main/java/com/bulletjournal/controller/utils/EtagGenerator.java
@@ -5,8 +5,12 @@ import org.springframework.util.DigestUtils;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Set;
+import java.util.zip.Adler32;
+import java.util.zip.Checksum;
 
 /**
  * Etag Generator Class contains generate Etag
@@ -15,14 +19,6 @@ import java.util.Set;
  * - To_String: Concatenate String of args to one String for hashing
  */
 public class EtagGenerator {
-
-    public enum HashType {
-        TO_HASHCODE, TO_STRING
-    }
-
-    public enum HashAlgorithm {
-        MD5, ADLER32, SHA256
-    }
 
     public static <T> String getHash(HashType hashType, T arg) {
         if (hashType == HashType.TO_HASHCODE) {
@@ -59,16 +55,33 @@ public class EtagGenerator {
 
         inputStreamBuilder(hashType, inputBuilder, args);
 
-        // Convert input string into Byte Array
-        InputStream inputStream = new ByteArrayInputStream(inputBuilder.toString().getBytes());
         outputBuilder.append("\"0");
 
         if (HashAlgorithm.MD5 == hashAlg) {
             // MD5 Hash
             try {
+                // Convert input string into Byte Array
+                InputStream inputStream = new ByteArrayInputStream(inputBuilder.toString().getBytes());
                 DigestUtils.appendMd5DigestAsHex(inputStream, outputBuilder);
             } catch (IOException ex) {
                 throw new IllegalStateException(ex);
+            }
+        } else if (HashAlgorithm.SHA256 == hashAlg) {
+            // SHA256 Hash
+            try {
+                outputBuilder.append(
+                        bytesToHex(MessageDigest.getInstance("SHA3_256").digest(inputBuilder.toString().getBytes())));
+            } catch (NoSuchAlgorithmException ex) {
+                throw new IllegalArgumentException(ex);
+            }
+        } else if (HashAlgorithm.ADLER32 == hashAlg) {
+            // ALDER32 Hash
+            try {
+                Checksum checksum = new Adler32();
+                checksum.update(inputBuilder.toString().getBytes(), 0, inputBuilder.toString().getBytes().length);
+                outputBuilder.append(checksum.getValue());
+            } catch (Exception ex) {
+                throw new IllegalArgumentException(ex);
             }
         } else {
             // No Hash Function selected
@@ -78,4 +91,23 @@ public class EtagGenerator {
         outputBuilder.append('"');
         return outputBuilder.toString();
     }
+
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : hash) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public enum HashType {
+        TO_HASHCODE, TO_STRING
+    }
+
+    public enum HashAlgorithm {
+        MD5, ADLER32, SHA256
+    }
+    
 }
