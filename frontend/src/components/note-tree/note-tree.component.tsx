0f65fd6d183258af0b9913e7491785ffc258ee84
Xin Yao <singerdmx@gmail.com>
2020-03-09 00:40:34 -0700

    Reformat note-tree

--- frontend/src/components/note-tree/note-tree.component.tsx
+++ frontend/src/components/note-tree/note-tree.component.tsx
@@ -1,44 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, RouteComponentProps } from 'react-router';
-import { TreeNodeNormal } from 'antd/lib/tree/Tree';
-import { Tree } from 'antd';
+import React, {useEffect, useState} from 'react';
+import {connect} from 'react-redux';
+import {withRouter, RouteComponentProps} from 'react-router';
+import {TreeNodeNormal} from 'antd/lib/tree/Tree';
+import {Tree} from 'antd';
 import {
-  deleteNote,
-  updateNotes,
-  putNote
+    deleteNote,
+    updateNotes,
+    putNote
 } from '../../features/notes/actions';
-import { Note } from '../../features/notes/interface';
-import { IState } from '../../store';
+import {Note} from '../../features/notes/interface';
+import {IState} from '../../store';
 
 type NotesProps = {
-  notes: Note[];
-  projectId: number,
-  deleteNote: (NoteId: number) => void;
-  updateNotes: (projectId: number) => void;
-  putNote: (projectId: number, notes: Note[]) => void;
+    notes: Note[];
+    projectId: number,
+    deleteNote: (NoteId: number) => void;
+    updateNotes: (projectId: number) => void;
+    putNote: (projectId: number, notes: Note[]) => void;
 };
 
 const getTree = (
     data: Note[],
     name: string,
     index: number
-  ): TreeNodeNormal[] => {
+): TreeNodeNormal[] => {
     let res = [] as TreeNodeNormal[];
     data.forEach((item: Note) => {
-      const node = {} as TreeNodeNormal;
-      if (item.subNotes && item.subNotes.length) {
-        node.children = getTree(item.subNotes, item.name, index);
-      } else {
-        node.children = [] as TreeNodeNormal[];
-      }
-      node.title = <span>{item.name}</span>;
-      node.key = item.id.toString();
-      res.push(node);
+        const node = {} as TreeNodeNormal;
+        if (item.subNotes && item.subNotes.length) {
+            node.children = getTree(item.subNotes, item.name, index);
+        } else {
+            node.children = [] as TreeNodeNormal[];
+        }
+        node.title = <span>{item.name}</span>;
+        node.key = item.id.toString();
+        res.push(node);
     });
     return res;
-  };
-
+};
 
 
 const onDragEnter = (info: any) => {
@@ -47,53 +46,54 @@ const onDragEnter = (info: any) => {
     // setState({
     //   expendKey: info.expandedKeys,
     // });
-  };
+};
 
-  const findNoteById = (notes: Note[], noteId: number): Note => {
+const findNoteById = (notes: Note[], noteId: number): Note => {
     let res = {} as Note;
-    const searchNote = notes.find(item=>item.id===noteId);
-    if(searchNote){
+    const searchNote = notes.find(item => item.id === noteId);
+    if (searchNote) {
         res = searchNote;
-    }else{
-        for(let i = 0; i < notes.length; i++){
+    } else {
+        for (let i = 0; i < notes.length; i++) {
             const searchSubNote = findNoteById(notes[i].subNotes, noteId);
-            if(searchSubNote){
+            if (searchSubNote) {
                 res = searchSubNote;
             }
         }
     }
-    return res;;
+    return res;
+    ;
 }
 
 
 const dragNoteById = (notes: Note[], noteId: number): Note[] => {
     let res = [] as Note[];
-    notes.forEach((item, index)=>{
+    notes.forEach((item, index) => {
         let note = {} as Note;
         const subNotes = dragNoteById(item.subNotes, noteId);
         note = {...item, subNotes: subNotes};
-        if(note.id!==noteId) res.push(note);
-        
+        if (note.id !== noteId) res.push(note);
+
     })
     return res;
 }
 
 const DropNoteById = (notes: Note[], dropId: number, dropNote: Note): Note[] => {
     let res = [] as Note[];
-    notes.forEach((item, index)=>{
+    notes.forEach((item, index) => {
         let note = {} as Note;
         let subNotes = [] as Note[];
-        if(item.id===dropId) {
+        if (item.id === dropId) {
             subNotes = item.subNotes;
             subNotes.push(dropNote);
-        }else{
+        } else {
             subNotes = DropNoteById(item.subNotes, dropId, dropNote);
         }
         note = {...item, subNotes: subNotes}
         res.push(note);
     })
     return res;
-} 
+}
 
 const onDrop = (notes: Note[], putNote: Function, projectId: number) => (info: any) => {
     const targetNote = findNoteById(notes, parseInt(info.dragNode.key));
@@ -103,24 +103,22 @@ const onDrop = (notes: Note[], putNote: Function, projectId: number) => (info: a
 }
 
 const NoteTree: React.FC<RouteComponentProps & NotesProps> = props => {
-    const { projectId, notes, putNote } = props;
-    useEffect(()=>{
+    const {projectId, notes, putNote} = props;
+    useEffect(() => {
         if (projectId) {
-          props.updateNotes(projectId);
+            props.updateNotes(projectId);
         }
     }, [projectId])
     let treeNote = getTree(notes, `project${projectId}`, projectId);
 
-    return (<Tree 
-                className="draggable-tree"
-                draggable
-                blockNode
-                onDragEnter={onDragEnter}
-                onDrop={onDrop(notes, putNote, projectId)}
-                treeData={treeNote}/>);
+    return (<Tree
+        className="draggable-tree"
+        draggable
+        blockNode
+        onDragEnter={onDragEnter}
+        onDrop={onDrop(notes, putNote, projectId)}
+        treeData={treeNote}/>);
 }
-
-
 
 const mapStateToProps = (state: IState) => ({
     projectId: state.project.project.id,
@@ -128,7 +126,7 @@ const mapStateToProps = (state: IState) => ({
 });
 
 export default connect(mapStateToProps, {
-  deleteNote,
-  updateNotes,
-  putNote
+    deleteNote,
+    updateNotes,
+    putNote
 })(withRouter(NoteTree));
