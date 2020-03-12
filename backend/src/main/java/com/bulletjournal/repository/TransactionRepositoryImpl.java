effef94a8c1bcb5ad6a02282599f41494c630fe7
Xin Yao <singerdmx@gmail.com>
2020-03-12 08:57:44 -0700

    Fix checkstyle

--- backend/src/main/java/com/bulletjournal/repository/TransactionRepositoryImpl.java
+++ backend/src/main/java/com/bulletjournal/repository/TransactionRepositoryImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 @Transactional
-public class TransactionRepositoryImpl implements TransactionRepositoryCustom{
+public class TransactionRepositoryImpl implements TransactionRepositoryCustom {
 
     @PersistenceContext
     EntityManager entityManager;
