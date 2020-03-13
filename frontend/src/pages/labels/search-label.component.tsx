d177cb94c3ec30b05188ac82b2db705c0490933a
Rongkai Liu <milkrong121@outlook.com>
2020-03-13 10:46:20 -0700

    call getItems by label

--- frontend/src/pages/labels/search-label.component.tsx
+++ frontend/src/pages/labels/search-label.component.tsx
@@ -1,67 +1,64 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { Form, message, AutoComplete, Input, PageHeader, Tag } from 'antd';
-import { Label } from '../../features/label/interface';
-import {
-  addSelectedLabel,
-  removeSelectedLabel
-} from '../../features/label/actions';
+import { Form, Select, PageHeader, Tag, Button } from 'antd';
+import { Label, stringToRGB } from '../../features/label/interface';
+import { getItemsByLabels } from '../../features/label/actions';
 import { IState } from '../../store';
+import { SearchOutlined } from '@ant-design/icons';
 
 type LabelSearchProps = {
   labelOptions: Label[];
-  labelsSelected: Label[];
-  addSelectedLabel: (val: string) => void;
-  removeSelectedLabel: (val: string) => void;
+  getItemsByLabels: (labels: []) => void;
   endSearching: () => void;
 };
 
 const LabelsSearching: React.FC<LabelSearchProps> = props => {
   const [form] = Form.useForm();
+
+  const handleSearch = () => {
+    form.validateFields().then(values => {
+      if (!values) {
+        return;
+      }
+      props.getItemsByLabels(values.selectLabels);
+    });
+  };
+  const tagsRender = (props: any) => {
+    console.log(props);
+  };
   return (
     <div className="labels-search-container">
-      <PageHeader title="Search Item With Label" subTitle="Click To Choose Label" onBack={props.endSearching} />
+      <PageHeader
+        title="Search Item With Label"
+        subTitle="Click To Choose Label"
+        onBack={props.endSearching}
+      />
       <div className="label-search-input">
-        <Form form={form}>
+        <Form form={form} layout="inline" onFinish={handleSearch}>
           <Form.Item
-            name="labelName"
+            name="selectLabels"
             rules={[{ required: true, message: 'Please input Label Name!' }]}
+            style={{ flex: 5 }}
           >
-            <AutoComplete
-              children={
-                <Input
-                  placeholder="Press Enter To Get Result"
-                  className="label-search-input"
-                  onPressEnter={e => {
-                    form
-                      .validateFields()
-                      .then(values => {
-                        // call search api here
-                        form.resetFields();
-                      })
-                      .catch(info => message.error(info));
-                  }}
-                />
-              }
-              options={props.labelOptions}
-              onSelect={value => props.addSelectedLabel(value)}
-            />
+            <Select mode="multiple">
+              {props.labelOptions &&
+                props.labelOptions.map(option => {
+                  return (
+                    <Select.Option key={option.id} value={option.id}>
+                      {option.value}
+                    </Select.Option>
+                  );
+                })}
+            </Select>
+          </Form.Item>
+          <Form.Item style={{ flex: 1 }}>
+            <Button shape="circle" type="primary" htmlType="submit">
+              <SearchOutlined />
+            </Button>
           </Form.Item>
         </Form>
       </div>
-      <div className="selected-labels">
-        {props.labelsSelected.map(label => (
-          <Tag
-            key={label.id}
-            closable
-            onClose={() => props.removeSelectedLabel(label.value)}
-          >
-            {label.value}
-          </Tag>
-        ))}
-      </div>
-      <div className="search-results">
-      </div>
+      <div className="search-results"></div>
     </div>
   );
 };
@@ -71,6 +68,5 @@ const mapStateToProps = (state: IState) => ({
 });
 
 export default connect(mapStateToProps, {
-  addSelectedLabel,
-  removeSelectedLabel
+  getItemsByLabels
 })(LabelsSearching);
