c898828cb70af33b198e9499a681a233dc0ecfd5
Qianhe Chen <chenqianhe0317@gmail.com>
2020-03-16 14:48:24 -0700

    third line of add transaction

--- frontend/src/components/modals/add-transaction.component.tsx
+++ frontend/src/components/modals/add-transaction.component.tsx
@@ -8,7 +8,8 @@ import {
   Select,
   Avatar,
   InputNumber,
-  Radio
+  Radio,
+  TimePicker
 } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 import { connect } from 'react-redux';
@@ -19,7 +20,23 @@ import { Project } from '../../features/project/interface';
 import { Group } from '../../features/group/interface';
 import './modals.styles.less';
 import { updateExpandedMyself } from '../../features/myself/actions';
+import { zones } from '../settings/constants';
 const { Option } = Select;
+const currentZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const currentCountry = currentZone && currentZone.split('/')[0];
+zones.sort((a, b) => {
+  if (currentZone && currentZone === a) {
+    return -1;
+  }
+  if (
+    currentCountry &&
+    a.includes(currentCountry) &&
+    !b.includes(currentCountry)
+  ) {
+    return -1;
+  }
+  return 0;
+});
 var LocaleCurrency = require('locale-currency'); //currency code
 
 type TransactionProps = {
@@ -93,11 +110,7 @@ const AddTransaction: React.FC<RouteComponentProps &
             </Form.Item>
             <div style={{ display: 'flex' }}>
               <span>Payer&nbsp;&nbsp;</span>
-              <Form.Item
-                name='payerName'
-                rules={[{ required: true, message: 'Missing Payer Name!' }]}
-                style={{ width: '100%' }}
-              >
+              <Form.Item name='payerName' style={{ width: '100%' }}>
                 {!props.group.users ? null : (
                   <Select defaultValue={props.group.users[0].name}>
                     {props.group.users.map(user => {
@@ -119,7 +132,7 @@ const AddTransaction: React.FC<RouteComponentProps &
                 style={{ display: 'flex' }}
               >
                 <InputNumber
-                  placeholder='Enter amount'
+                  placeholder='Enter Amount'
                   style={{ width: '70%' }}
                 />
                 <span>
@@ -139,19 +152,27 @@ const AddTransaction: React.FC<RouteComponentProps &
               </Form.Item>
             </div>
 
-            <Form.Item
-              name='Date'
-              rules={[{ required: true, message: 'Missing Date!' }]}
-            >
-              <DatePicker />
-            </Form.Item>
+            <div style={{ display: 'flex' }}>
+              <Form.Item name='Date' style={{ width: '180px' }}>
+                <DatePicker />
+              </Form.Item>
 
-            <Form.Item
-              name='time'
-              rules={[{ required: true, message: 'Missing time!' }]}
-            >
-              <Input placeholder='Enter time' allowClear />
-            </Form.Item>
+              <Form.Item name='time' style={{ width: '180px' }}>
+                <TimePicker allowClear format='HH:mm' />
+              </Form.Item>
+
+              <Form.Item name='timezone'>
+                <Select showSearch={true} placeholder='Select a timezone'>
+                  {zones.map((zone: string, index: number) => (
+                    <Option key={zone} value={zone}>
+                      <Tooltip title={zone} placement='right'>
+                        {zone}
+                      </Tooltip>
+                    </Option>
+                  ))}
+                </Select>
+              </Form.Item>
+            </div>
           </Form>
         </Modal>
       </div>
