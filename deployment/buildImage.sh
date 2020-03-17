8a5d16cae8d97226ee64f9c62df113508257629d
mwangxx0129 <mwangxx@gmail.com>
2020-03-16 21:51:52 -0400

    release 1.1.0

--- deployment/buildImage.sh
+++ deployment/buildImage.sh
@@ -1,15 +1,25 @@
+#!/bin/sh
+DOCKERHUB_NAME="mwangxx"
+echo $DOCKERHUB_NAME
+
+if [ "$#" -ne 1 ]; then
+    echo "Miss image version"
+    exit 1
+fi
+
 # cd ../discourse-auth-proxy
 # docker build -t xcode1024/auth-proxy .
+
 cd ..
 
 cd ./frontend
-docker build -t xcode1024/bulletjournal-frontend .
+docker build -t $DOCKERHUB_NAME/bulletjournal-frontend:$1 .
 cd ..
 
 cd ./backend
-DOCKER_BUILDKIT=1 docker build -t xcode1024/bulletjournal-backend .
+DOCKER_BUILDKIT=1 docker build -t $DOCKERHUB_NAME/bulletjournal-backend:$1 .
 cd ../deployment
 
 # docker push xcode1024/auth-proxy:latest
-# docker push xcode1024/bulletjournal-frontend:latest
-# docker push xcode1024/bulletjournal-backend:latest
+docker push $DOCKERHUB_NAME/bulletjournal-frontend:$1
+docker push $DOCKERHUB_NAME/bulletjournal-backend:$1
