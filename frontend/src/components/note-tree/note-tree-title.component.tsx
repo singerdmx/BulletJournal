ab9f2f1355cd6454fcb69b24cd1d72b59a87f7ce
pengw00 <water685@hotmail.com>
2020-03-18 20:26:04 -0500

    add note control icon

--- frontend/src/components/note-tree/note-tree-title.component.tsx
+++ frontend/src/components/note-tree/note-tree-title.component.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {
-    FormOutlined
+    FormOutlined,
+    InfoCircleOutlined,
+    MessageOutlined,
+    MoreOutlined
   } from '@ant-design/icons';
 
 type NoteProps = {
@@ -10,9 +13,14 @@ type NoteProps = {
 const NoteTitle: React.FC<NoteProps> = props => {
     const { title } = props;
 
-    return (<div style={{width: '100%'}}>
+    return (<div style={{width: '100%', height: '2rem', position: 'relative', lineHeight: '2rem'}}>
         <FormOutlined/>
-        <span style={{padding: '0 5px'}}>{title}</span>
+        <span style={{padding: '0 5px', height: '100%'}}>{title}</span>
+        <div style={{ width: '30%', height: '100%', position: 'absolute', top: 0, right: 0, display: 'flex', justifyContent: 'space-evenly', alignItems: 'center'}}>
+            <InfoCircleOutlined />
+            <MessageOutlined />
+            <MoreOutlined style={{transform: 'rotate(90deg)', fontSize: '20px'}}/>
+        </div>
     </div>);
 }
 
