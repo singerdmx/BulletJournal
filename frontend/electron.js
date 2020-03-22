2f3678d7fef488964e1f3ca9a3cdf16fce172694
Rongkai Liu <milkrong121@outlook.com>
2020-03-21 18:12:13 -0700

    change default size of app

--- frontend/electron.js
+++ frontend/electron.js
@@ -9,7 +9,7 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
+  mainWindow = new BrowserWindow({width: 1200, height: 800});
   mainWindow.loadURL(isDev ? 'http://localhost:81' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
