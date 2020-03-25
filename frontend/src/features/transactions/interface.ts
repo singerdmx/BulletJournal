b48d3592c6e327c2bc37758cc32d73c108e7dc11
Qianhe Chen <chenqianhe0317@gmail.com>
2020-03-24 22:12:49 -0700

    added interface for transaction page

--- frontend/src/features/transactions/interface.ts
+++ frontend/src/features/transactions/interface.ts
@@ -1,10 +1,23 @@
-import { ProjectItem } from "../myBuJo/interface";
+import { ProjectItem } from '../myBuJo/interface';
 
 export interface Transaction extends ProjectItem {
-    amount: number,
-    payer: string,
-    date: string,
-    time: string,
-    timezone: string,
-    transactionType: number
+  amount: number;
+  payer: string;
+  date: string;
+  time: string;
+  timezone: string;
+  transactionType: number;
+}
+
+export enum FrequencyType {
+  WEEKLY = 'WEEKLY',
+  MONTHLY = 'MONTHLY',
+  YEARLY = 'YEARLY'
+}
+
+export enum LedgerSummaryType {
+  DEFAULT = 'DEFAULT',
+  PAYER = 'PAYER',
+  LABEL = 'LABEL',
+  TIMELINE = 'TIMELINE'
 }
