072eb4028d8ccf80d6d485254581a6dab2480f7b
Rongkai Liu <milkrong121@outlook.com>
2020-03-25 20:07:18 -0700

    finish left side of note page title

--- frontend/src/pages/note/note.pages.tsx
+++ frontend/src/pages/note/note.pages.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {connect} from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { getNote } from '../../features/notes/actions';
 import { IState } from '../../store';
 import { Note } from '../../features/notes/interface';
-import { Tooltip, Tag } from 'antd';
+import { Tooltip, Tag, Avatar } from 'antd';
 import { stringToRGB } from '../../features/label/interface';
 import { icons } from '../../assets/icons/index';
 import { TagOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 
 import './note-page.styles.less';
 
 type NoteProps = {
-  note : Note
-}
+  note: Note;
+};
 
 interface NotePageHandler {
-  getNote : (noteId : number) => void
+  getNote: (noteId: number) => void;
 }
 
-const NotePage: React.FC<NotePageHandler & NoteProps>  = (props) => {
-  const {note} = props;
+const NotePage: React.FC<NotePageHandler & NoteProps> = props => {
+  const { note } = props;
   const { noteId } = useParams();
 
   const getIcon = (icon: string) => {
@@ -30,27 +31,39 @@ const NotePage: React.FC<NotePageHandler & NoteProps>  = (props) => {
 
   React.useEffect(() => {
     noteId && props.getNote(parseInt(noteId));
-  }, [])
+  }, []);
   return (
     <div className="note-page">
+      <Tooltip placement="top" title={note.owner} className="note-avatar">
+        <span>
+          <Avatar size="large" src={note.ownerAvatar} />
+        </span>
+      </Tooltip>
       <div className="note-title">
         {note.name}
-      </div>
-      <div className="note-labels">
-        {note.labels && note.labels.map(label => {
-            return (
-              <Tooltip placement="top" title="Click to Check or Edit" key={label.id}>
-                <Tag
-                  className="labels"
-                  color={stringToRGB(label.value)}
-                  style={{ cursor: 'pointer' }}
+        <div className="note-labels">
+          {note.labels &&
+            note.labels.map(label => {
+              return (
+                <Tooltip
+                  placement="top"
+                  title="Click to Check or Edit"
+                  key={label.id}
                 >
-                  {getIcon(label.icon)} &nbsp;
-                  {label.value}
-                </Tag>
-              </Tooltip>
-            );
-          })}
+                  <Tag
+                    className="labels"
+                    color={stringToRGB(label.value)}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    <Link to="/labels">
+                      {getIcon(label.icon)} &nbsp;
+                      {label.value}
+                    </Link>
+                  </Tag>
+                </Tooltip>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
@@ -58,6 +71,6 @@ const NotePage: React.FC<NotePageHandler & NoteProps>  = (props) => {
 
 const mapStateToProps = (state: IState) => ({
   note: state.note.note
-})
+});
 
-export default connect(mapStateToProps, {getNote})(NotePage);
+export default connect(mapStateToProps, { getNote })(NotePage);
