8c72bf63e0d439fb023b0104a762fde3a9e2ce22
Xin Yao <singerdmx@gmail.com>
2020-03-26 14:15:43 -0700

    delete note - confirm there are children

--- frontend/src/components/modals/edit-note.component.tsx
+++ frontend/src/components/modals/edit-note.component.tsx
@@ -1,48 +1,44 @@
-import React, { useEffect, useState } from 'react';
-import { Avatar, Form, Input, Modal, Select, Tooltip, Button } from 'antd';
+import React, {useState} from 'react';
+import {Input, Modal} from 'antd';
 import './modals.styles.less';
-import { patchNote } from '../../features/notes/actions';
-import { connect } from 'react-redux';
-import { IState } from '../../store';
-import { Note } from '../../features/notes/interface';
+import {patchNote} from '../../features/notes/actions';
+import {connect} from 'react-redux';
+import {IState} from '../../store';
+import {Note} from '../../features/notes/interface';
 
 
 type NoteProps = {
     note: Note;
     patchNote: (noteId: number, content: string) => void;
-    patchLoading: boolean;
     visible: boolean;
     setVisible: (visible: boolean) => void;
-  };
+};
 
 const EditNote: React.FC<NoteProps> = props => {
-  const { visible, setVisible, note, patchNote } = props;
+    const {visible, setVisible, note, patchNote} = props;
     const [value, setValue] = useState('');
     const onOk = () => {
-      patchNote(note.id, value)
-      setVisible(!visible)
+        patchNote(note.id, value);
+        setVisible(!visible)
     };
 
-
     return (<div>
-      <Modal
-          title="Title"
-          visible={props.visible}
-          onOk={onOk}
-          confirmLoading={props.patchLoading}
-          okText="Confirm"
-          cancelText="Cancel"
-          onCancel={()=>setVisible(!visible)}
+        <Modal
+            title="Title"
+            visible={props.visible}
+            onOk={onOk}
+            okText="Confirm"
+            cancelText="Cancel"
+            onCancel={() => setVisible(!visible)}
         >
-          <div><Input placeholder='text' onChange={e=>setValue(e.target.value)}/></div>
+            <div><Input placeholder='text' onChange={e => setValue(e.target.value)}/></div>
         </Modal>
     </div>);
-}
+};
 
 const mapStateToProps = (state: IState) => ({
-  patchLoading: state.note.patchLoading
 });
 
-export default connect(mapStateToProps, { patchNote })(
+export default connect(mapStateToProps, {patchNote})(
     EditNote
-  );
+);
--- frontend/src/components/project-item/note-item.component.tsx
+++ frontend/src/components/project-item/note-item.component.tsx
@@ -1,104 +1,109 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
-  DeleteTwoTone,
-  DragOutlined,
-  EditTwoTone,
-  FormOutlined,
-  InfoCircleOutlined,
-  MessageOutlined,
-  MoreOutlined
+    DeleteTwoTone,
+    EditTwoTone,
+    FormOutlined,
+    InfoCircleOutlined,
+    MessageOutlined,
+    MoreOutlined,
+    RightCircleOutlined
 } from '@ant-design/icons';
-import { deleteNote, moveNote } from '../../features/notes/actions';
-import { Note } from '../../features/notes/interface';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import {deleteNote, moveNote} from '../../features/notes/actions';
+import {Note} from '../../features/notes/interface';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import EditNote from '../modals/edit-note.component';
 
-import { Popover } from 'antd';
+import {Popconfirm, Popover} from 'antd';
 
 type NoteProps = {
-  note: Note;
-  deleteNote: (noteId: number) => void;
-  // onEdit: (noteId: number) => void;
-  moveNote: (noteId: number, targetProject: number) => void;
+    note: Note;
+    deleteNote: (noteId: number) => void;
+    // onEdit: (noteId: number) => void;
+    moveNote: (noteId: number, targetProject: number) => void;
 };
 
 const Content: React.FC<NoteProps> = props => {
-  const { note, deleteNote, moveNote } = props;
-  const [visible, setVisible] = useState(false);
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <div>
-        Delete
-        <DeleteTwoTone
-          twoToneColor="#f5222d"
-          onClick={() => deleteNote(note.id)}
-        />
-      </div>
-      <div>
-        <span>Edit</span>
-        <EditTwoTone onClick={()=>setVisible(!visible)} />
-        <EditNote visible={visible} note={note} setVisible={setVisible}/>
-      </div>
-      <div>
-        Move <DragOutlined onClick={() => moveNote(note.id, 14)} />
-      </div>
-    </div>
-  );
+    const {note, deleteNote, moveNote} = props;
+    const [visible, setVisible] = useState(false);
+    return (
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+            <Popconfirm
+                title="Deleting Note also deletes its child notes. Are you sure?"
+                okText="Yes"
+                cancelText="No"
+                onConfirm={() => deleteNote(note.id)}
+                className="group-setting"
+                placement="bottom"
+            >
+                <div>
+                    Delete
+                    <DeleteTwoTone twoToneColor="#f5222d"/>
+                </div>
+            </Popconfirm>
+            <div>
+                <span>Edit</span>
+                <EditTwoTone onClick={() => setVisible(!visible)}/>
+                <EditNote visible={visible} note={note} setVisible={setVisible}/>
+            </div>
+            <div>
+                Move <RightCircleOutlined onClick={() => moveNote(note.id, 14)}/>
+            </div>
+        </div>
+    );
 };
 
 const alignConfig = {
-  offset: [10, -5]
+    offset: [10, -5]
 };
 
 const NoteItem: React.FC<NoteProps> = props => {
-  const { note, deleteNote, moveNote } = props;
+    const {note, deleteNote, moveNote} = props;
 
-  return (
-    <div
-      style={{
-        width: '100%',
-        height: '2rem',
-        position: 'relative',
-        lineHeight: '2rem'
-      }}
-    >
-      
-      <Link to={`/note/${note.id}`}>
-        <FormOutlined />
-        <span style={{ padding: '0 5px', height: '100%' }}>{note.name}</span>
-      </Link>
-      <div
-        style={{
-          width: '300px',
-          height: '100%',
-          position: 'absolute',
-          top: 0,
-          right: 0,
-          display: 'flex',
-          justifyContent: 'flex-end',
-          alignItems: 'center'
-        }}
-      >
-        <InfoCircleOutlined style={{ marginRight: '1em' }} />
-        <MessageOutlined style={{ marginRight: '1em' }} />
-        <Popover
-          align={alignConfig}
-          placement="bottomRight"
-          style={{ top: -10 }}
-          title={null}
-          content={
-            <Content note={note} deleteNote={deleteNote} moveNote={moveNote} />
-          }
-          trigger="click"
+    return (
+        <div
+            style={{
+                width: '100%',
+                height: '2rem',
+                position: 'relative',
+                lineHeight: '2rem'
+            }}
         >
-          <MoreOutlined
-            style={{ transform: 'rotate(90deg)', fontSize: '20px' }}
-          />
-        </Popover>
-      </div>
-    </div>
-  );
+
+            <Link to={`/note/${note.id}`}>
+                <FormOutlined/>
+                <span style={{padding: '0 5px', height: '100%'}}>{note.name}</span>
+            </Link>
+            <div
+                style={{
+                    width: '300px',
+                    height: '100%',
+                    position: 'absolute',
+                    top: 0,
+                    right: 0,
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    alignItems: 'center'
+                }}
+            >
+                <InfoCircleOutlined style={{marginRight: '1em'}}/>
+                <MessageOutlined style={{marginRight: '1em'}}/>
+                <Popover
+                    align={alignConfig}
+                    placement="bottomRight"
+                    style={{top: -10}}
+                    content={
+                        <Content note={note} deleteNote={deleteNote} moveNote={moveNote}/>
+                    }
+                    trigger="click"
+                >
+                    <MoreOutlined
+                        style={{transform: 'rotate(90deg)', fontSize: '20px'}}
+                    />
+                </Popover>
+            </div>
+        </div>
+    );
 };
 
-export default connect(null, { deleteNote, moveNote })(NoteItem);
+export default connect(null, {deleteNote, moveNote})(NoteItem);
