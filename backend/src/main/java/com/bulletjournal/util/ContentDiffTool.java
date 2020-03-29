6b22e2b05f50a332babcf15ba95ad0e4c52e32ab
Xin Yao <singerdmx@gmail.com>
2020-03-28 19:25:26 -0700

    Fix checkstyle

--- backend/src/main/java/com/bulletjournal/util/ContentDiffTool.java
+++ backend/src/main/java/com/bulletjournal/util/ContentDiffTool.java
@@ -15,27 +15,28 @@ public class ContentDiffTool {
 
     private DiffMatchPatch diffMatchPatch = new DiffMatchPatch();
 
-    private ContentDiffTool() {}
+    private ContentDiffTool() {
+    }
 
     public String computeDiff(String oldText, String newText) {
         return diffMatchPatch.patchToText(
-            diffMatchPatch.patchMake(
-                diffMatchPatch.diffMain(oldText, newText)
-            )
+                diffMatchPatch.patchMake(
+                        diffMatchPatch.diffMain(oldText, newText)
+                )
         );
     }
 
     public String applyDiff(String oldText, String diff) {
         List<DiffMatchPatch.Patch> patches = diffMatchPatch.patchFromText(diff);
         Object[] output = diffMatchPatch.patchApply(
-            (LinkedList<DiffMatchPatch.Patch>) patches, oldText);
+                (LinkedList<DiffMatchPatch.Patch>) patches, oldText);
         String newText = (String) output[0];
         boolean[] status = (boolean[]) output[1];
         // check if all patches are applied successfully
         for (boolean s : status) {
             if (!s) {
                 LOGGER.error("Some patches in diff failed to apply, old text:\n {}, \n diff:\n {}",
-                    oldText, diff);
+                        oldText, diff);
                 break;
             }
         }
