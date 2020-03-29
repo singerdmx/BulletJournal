e653bc78d1e1f7085f8713658d787439d6bc7392
Xin Yao <singerdmx@gmail.com>
2020-03-29 12:40:15 -0700

    Add @JsonInclude(JsonInclude.Include.NON_NULL)

--- backend/src/main/java/com/bulletjournal/controller/models/LedgerSummary.java
+++ backend/src/main/java/com/bulletjournal/controller/models/LedgerSummary.java
@@ -1,7 +1,10 @@
 package com.bulletjournal.controller.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LedgerSummary {
 
     private Double balance;
--- backend/src/main/java/com/bulletjournal/controller/models/TransactionsSummary.java
+++ backend/src/main/java/com/bulletjournal/controller/models/TransactionsSummary.java
@@ -1,5 +1,8 @@
 package com.bulletjournal.controller.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TransactionsSummary {
 
     private String name;
