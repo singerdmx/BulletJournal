2712dddfd3d5b2202ca8a785f9804ebcd3dc49cf
Xin Yao <singerdmx@gmail.com>
2020-03-30 21:24:16 -0700

    Fix typo

--- frontend/src/components/project-item/transaction-item.component.tsx
+++ frontend/src/components/project-item/transaction-item.component.tsx
@@ -11,17 +11,15 @@ import { Popconfirm, Popover, Tag, Tooltip, Avatar, List } from 'antd';
 import { Transaction } from '../../features/transactions/interface';
 import { icons } from '../../assets/icons';
 
-type TransationProps = {
+type TransactionProps = {
     transaction: Transaction;
 };
 
-const TransactionItem: React.FC<TransationProps> = props => {
+const TransactionItem: React.FC<TransactionProps> = props => {
   const { transaction } = props;
 
   return (
-    <List.Item style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-        <div>{transaction.owner}</div>
-    </List.Item>
+      <div>{transaction.owner}</div>
   );
 };
 
