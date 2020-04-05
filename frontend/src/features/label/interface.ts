f9baf1d38af99c9711ada69fdf8edde400157cb5
Xin Yao <singerdmx@gmail.com>
2020-04-05 16:13:20 -0700

    Update label color

--- frontend/src/features/label/interface.ts
+++ frontend/src/features/label/interface.ts
@@ -1,17 +1,17 @@
 export interface Label {
-  id: number;
-  value: string;
-  icon: string;
+    id: number;
+    value: string;
+    icon: string;
 }
 
-export function stringToRGB(str: string) {
-  let hash: number = 0;
-  for (let i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
+const colors = ['magenta', 'red', 'volcano', 'orange', 'gold', 'lime', 'green',
+    'cyan', 'blue', 'geekblue', 'purple'];
 
-  let c = (hash & 0x00FFFFFF)
-      .toString(16)
-      .toUpperCase()
-  return '#' + '00000'.substring(0, 6 - c.length) + c;
+export function stringToRGB(str: string) {
+    let hash: number = 0;
+    for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    return colors[hash % colors.length];
 }
