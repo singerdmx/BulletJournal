3efe4a42f556cd92f7227d9afa904137681e82f8
Rongkai Liu <milkrong121@outlook.com>
2020-04-11 00:27:14 -0700

    add actions to content item

--- frontend/src/components/note-content/content-item.component.tsx
+++ frontend/src/components/note-content/content-item.component.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
-import { List, Button } from 'antd';
+import { List, Button, Avatar } from 'antd';
 import { Content } from '../../features/myBuJo/interface';
 import BraftEditor from 'braft-editor';
 import NoteEditorDrawer from '../note-editor/editor-drawer.component';
+import { deleteContent } from '../../features/notes/actions';
+import { connect } from 'react-redux';
 
 type NoteContentProps = {
   content: Content;
   noteId: number;
 };
 
-const NoteContentItem: React.FC<NoteContentProps> = ({ content, noteId }) => {
+interface NoteContentHandler {
+  deleteContent: (noteId: number, contentId: number) => void;
+}
+
+const NoteContentItem: React.FC<NoteContentProps & NoteContentHandler> = ({
+  content,
+  noteId,
+  deleteContent,
+}) => {
   const contentState = BraftEditor.createEditorState(content.text);
   const contentText = contentState.toText();
   const [displayMore, setDisplayMore] = useState(false);
@@ -29,19 +39,33 @@ const NoteContentItem: React.FC<NoteContentProps> = ({ content, noteId }) => {
     setDisplayMore(true);
   };
 
-  return (
-    <List.Item key={content.id}>
-      <List.Item.Meta avatar={content.ownerAvatar} />
-      {contentText.length > 300
-        ? `${contentText.slice(0, 300)}...`
-        : contentText}
+  const handleDelete = (contentId: number) => {
+    deleteContent(noteId, contentId);
+  };
 
-      <Button type="link" onClick={startReading}>
-        {contentText.length > 300 ? 'Read More' : 'Read'}
-      </Button>
-      <Button type="link" onClick={startEdit}>
-        Edit
-      </Button>
+  return (
+    <List.Item
+      key={content.id}
+      actions={[
+        <Button type="link" onClick={startReading}>
+          {contentText.length > 300 ? 'Read More' : 'Read'}
+        </Button>,
+        <Button type="link" onClick={startEdit}>
+          Edit
+        </Button>,
+        <Button type="link" danger onClick={() => handleDelete(content.id)}>
+          Delete
+        </Button>,
+      ]}
+    >
+      <List.Item.Meta
+        avatar={<Avatar src={content.ownerAvatar} />}
+        description={
+          contentText.length > 300
+            ? `${contentText.slice(0, 300)}...`
+            : contentText
+        }
+      />
 
       <NoteEditorDrawer
         readonly={readMode}
@@ -54,4 +78,4 @@ const NoteContentItem: React.FC<NoteContentProps> = ({ content, noteId }) => {
   );
 };
 
-export default NoteContentItem;
+export default connect(null, { deleteContent })(NoteContentItem);
