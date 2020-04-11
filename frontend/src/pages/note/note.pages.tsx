4d82db7b0d09595057adb82f7a2f69382e1eeb86
Rongkai Liu <milkrong121@outlook.com>
2020-04-11 00:01:27 -0700

    fix note page

--- frontend/src/pages/note/note.pages.tsx
+++ frontend/src/pages/note/note.pages.tsx
@@ -1,18 +1,23 @@
 // page display contents of notes
 // react imports
-import React, {useState} from 'react';
-import {useHistory, useParams} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { connect } from 'react-redux';
 // features
 //actions
-import {deleteNote, getNote} from '../../features/notes/actions';
+import { deleteNote, getNote } from '../../features/notes/actions';
 
-import {IState} from '../../store';
+import { IState } from '../../store';
 // components
 import NoteEditorDrawer from '../../components/note-editor/editor-drawer.component';
 // antd imports
-import {Button, Popconfirm, Tooltip} from 'antd';
-import {DeleteTwoTone, PlusCircleTwoTone, TagOutlined, UpSquareOutlined,} from '@ant-design/icons';
+import { Button, Popconfirm, Tooltip } from 'antd';
+import {
+  DeleteTwoTone,
+  PlusCircleTwoTone,
+  TagOutlined,
+  UpSquareOutlined,
+} from '@ant-design/icons';
 // modals import
 import EditNote from '../../components/modals/edit-note.component';
 import MoveProjectItem from '../../components/modals/move-project-item.component';
@@ -20,106 +25,124 @@ import ShareProjectItem from '../../components/modals/share-project-item.compone
 
 import './note-page.styles.less';
 import 'braft-editor/dist/index.css';
-import {ProjectType} from '../../features/project/constants';
-import NoteDetailPage, {NoteProps} from "./note-detail.pages";
+import { ProjectType } from '../../features/project/constants';
+import NoteDetailPage, { NoteProps } from './note-detail.pages';
 
 interface NotePageHandler {
-    getNote: (noteId: number) => void;
-    deleteNote: (noteId: number) => void;
+  getNote: (noteId: number) => void;
+  deleteNote: (noteId: number) => void;
 }
 
 // get icons by string name
 
 const NotePage: React.FC<NotePageHandler & NoteProps> = (props) => {
-    // hook history in router
-    const history = useHistory();
-    const {note, deleteNote} = props;
-    // get id of note from router
-    const {noteId} = useParams();
-    // state control drawer displaying
-    const [showEditor, setEditorShow] = useState(false);
-    const [labelEditable, setLabelEditable] = useState(false);
+  // hook history in router
+  const history = useHistory();
+  const { note, deleteNote } = props;
+  // get id of note from router
+  const { noteId } = useParams();
+  // state control drawer displaying
+  const [showEditor, setEditorShow] = useState(false);
+  const [labelEditable, setLabelEditable] = useState(false);
 
-    // listening on the empty state working as componentDidmount
-    React.useEffect(() => {
-        noteId && props.getNote(parseInt(noteId));
-    }, [noteId]);
-    // show drawer
-    const createHandler = () => {
-        setEditorShow(true);
-    };
+  // listening on the empty state working as componentDidmount
+  React.useEffect(() => {
+    noteId && props.getNote(parseInt(noteId));
+  }, [noteId]);
+  // show drawer
+  const createHandler = () => {
+    setEditorShow(true);
+  };
 
-    const labelEditableHandler = () => {
-        setLabelEditable((labelEditable) => !labelEditable);
-    };
+  const handleClose = () => {
+    setEditorShow(false);
+  };
 
-    const createContentElem = (<Button onClick={createHandler}>
-        <PlusCircleTwoTone/>
-        New
-    </Button>);
+  const labelEditableHandler = () => {
+    setLabelEditable((labelEditable) => !labelEditable);
+  };
 
-    const noteEditorElem = (<div className='note-drawer'>
-        <NoteEditorDrawer
-            noteId={note.id}
-            visible={showEditor}
-            setVisible={setEditorShow}
+  const createContentElem = (
+    <Button onClick={createHandler}>
+      <PlusCircleTwoTone />
+      New
+    </Button>
+  );
+
+  const noteEditorElem = (
+    <div className="note-drawer">
+      <NoteEditorDrawer
+        readonly={false}
+        noteId={note.id}
+        visible={showEditor}
+        onClose={handleClose}
+      />
+    </div>
+  );
+
+  const noteOperation = () => {
+    return (
+      <div className="note-operation">
+        <Tooltip title="Manage Labels">
+          <div>
+            <TagOutlined onClick={labelEditableHandler} />
+          </div>
+        </Tooltip>
+        <EditNote note={note} mode="icon" />
+        <MoveProjectItem
+          type={ProjectType.NOTE}
+          projectItemId={note.id}
+          mode="icon"
         />
-    </div>);
-
-    const noteOperation = () => {
-        return <div className='note-operation'>
-            <Tooltip title='Manage Labels'>
-                <div>
-                    <TagOutlined onClick={labelEditableHandler}/>
-                </div>
-            </Tooltip>
-            <EditNote note={note} mode='icon'/>
-            <MoveProjectItem
-                type={ProjectType.NOTE}
-                projectItemId={note.id}
-                mode='icon'
+        <ShareProjectItem
+          type={ProjectType.NOTE}
+          projectItemId={note.id}
+          mode="icon"
+        />
+        <Tooltip title="Delete">
+          <Popconfirm
+            title="Deleting Note also deletes its child notes. Are you sure?"
+            okText="Yes"
+            cancelText="No"
+            onConfirm={() => {
+              deleteNote(note.id);
+              history.goBack();
+            }}
+            className="group-setting"
+            placement="bottom"
+          >
+            <div>
+              <DeleteTwoTone twoToneColor="#f5222d" />
+            </div>
+          </Popconfirm>
+        </Tooltip>
+        <Tooltip title="Go to Parent BuJo">
+          <div>
+            <UpSquareOutlined
+              onClick={(e) => history.push(`/projects/${note.projectId}`)}
             />
-            <ShareProjectItem
-                type={ProjectType.NOTE}
-                projectItemId={note.id}
-                mode='icon'
-            />
-            <Tooltip title='Delete'>
-                <Popconfirm
-                    title='Deleting Note also deletes its child notes. Are you sure?'
-                    okText='Yes'
-                    cancelText='No'
-                    onConfirm={() => {
-                        deleteNote(note.id);
-                        history.goBack();
-                    }}
-                    className='group-setting'
-                    placement='bottom'
-                >
-                    <div>
-                        <DeleteTwoTone twoToneColor='#f5222d'/>
-                    </div>
-                </Popconfirm>
-            </Tooltip>
-            <Tooltip title='Go to Parent BuJo'>
-                <div>
-                    <UpSquareOutlined
-                        onClick={(e) => history.push(`/projects/${note.projectId}`)}
-                    />
-                </div>
-            </Tooltip>
-        </div>
-    };
+          </div>
+        </Tooltip>
+      </div>
+    );
+  };
 
-    return (<NoteDetailPage note={note} labelEditable={labelEditable} noteOperation={noteOperation}
-                            createContentElem={createContentElem} noteEditorElem={noteEditorElem}/>);
+  return (
+    <NoteDetailPage
+      note={note}
+      labelEditable={labelEditable}
+      noteOperation={noteOperation}
+      createContentElem={createContentElem}
+      noteEditorElem={noteEditorElem}
+    />
+  );
 };
 
 const mapStateToProps = (state: IState) => ({
-    note: state.note.note,
+  note: state.note.note,
 });
 
 export default connect(mapStateToProps, {
-    deleteNote,
-    getNote,
+  deleteNote,
+  getNote,
 })(NotePage);
