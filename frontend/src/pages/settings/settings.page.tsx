9bc79822667c23c1391985eb321bad8302a41d07
Xin Yao <singerdmx@gmail.com>
2020-04-13 19:23:32 -0700

    Update Settings page

--- frontend/src/pages/settings/settings.page.tsx
+++ frontend/src/pages/settings/settings.page.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import {Tabs, Tooltip} from 'antd';
 import Account from '../../components/settings/account';
-import { loginGoogleCalendar } from '../../apis/calendarApis';
-import {
-  GoogleOutlined,
-  AppleOutlined,
-  SwapOutlined
-} from '@ant-design/icons';
+import {loginGoogleCalendar} from '../../apis/calendarApis';
+import {AppleOutlined, GoogleOutlined, SwapOutlined} from '@ant-design/icons';
 import './setting.style.less';
+import {useLocation} from "react-use";
 
-const { TabPane } = Tabs;
+const {TabPane} = Tabs;
 
 const handleGoogleCalendarLogin = () => {
   loginGoogleCalendar().then(res => {
@@ -17,29 +14,46 @@ const handleGoogleCalendarLogin = () => {
   });
 };
 
-const SettingPage = () => {
+type SettingProps = {
+};
+
+const SettingPage: React.FC<SettingProps> = (props) => {
+  const location = useLocation();
+  let defaultKey = location.hash;
+  let calendarKey = '#google';
+  if (!defaultKey) {
+    defaultKey = 'Account'
+  }
+
+  if (defaultKey.substr(0, 1) == '#') {
+    calendarKey = defaultKey;
+    defaultKey = 'calendarSync';
+  }
+
+  console.log(calendarKey)
   return (
-    <div className='setting'>
-      <Tabs defaultActiveKey='account'>
-        <TabPane tab='Account' key='account'>
-          <Account />
-        </TabPane>
-        <TabPane tab='Calendar Sync' key='calendarSync'>
-          <div>
-            <Tabs type="card">
-              <TabPane tab={<span><GoogleOutlined /> Google Calendar</span>} key="GoogleCalendar">
-                <div className='calendar-login-button' onClick={() => handleGoogleCalendarLogin()}>
-                  <SwapOutlined /><span>{' '}Connect</span>
-                </div>
-                Enjoy a 2-way sync between your scheduled tasks and your Google Calendar.
-              </TabPane>
-              <TabPane tab={<span><AppleOutlined /> Apple Calendar</span>} key="AppleCalendar">
-              </TabPane>
-            </Tabs>
-          </div>
-        </TabPane>
-      </Tabs>
-    </div>
+      <div className='setting'>
+        <Tabs defaultActiveKey={defaultKey}>
+          <TabPane tab='Account' key='account'>
+            <Account/>
+          </TabPane>
+          <TabPane tab='Calendar Sync' key='calendarSync'>
+            <div>
+              <Tabs type="card" defaultActiveKey={calendarKey}>
+                <TabPane tab={<span><GoogleOutlined/> Google Calendar</span>} key="#google">
+                  <div className='calendar-login-button'>
+                    <Tooltip title='Enjoy a 2-way sync between your scheduled tasks and your Google Calendar'>
+                      <span onClick={() => handleGoogleCalendarLogin()}><SwapOutlined/><span>{' '}Connect</span></span>
+                    </Tooltip>
+                  </div>
+                </TabPane>
+                <TabPane tab={<span><AppleOutlined/> Apple Calendar</span>} key="#apple">
+                </TabPane>
+              </Tabs>
+            </div>
+          </TabPane>
+        </Tabs>
+      </div>
   );
 };
 
