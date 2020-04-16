27f5efbe1530e09f6223a716b2ddb648406833aa
Qianhe Chen <chenqianhe0317@gmail.com>
2020-04-15 22:23:25 -0700

    edit logic

--- frontend/src/components/content/content-list.component.tsx
+++ frontend/src/components/content/content-list.component.tsx
@@ -1,31 +1,36 @@
 import React, { useEffect } from 'react';
 import { List } from 'antd';
 import { connect } from 'react-redux';
+import { ProjectItem } from '../../features/myBuJo/interface';
 import { updateNoteContents } from '../../features/notes/actions';
 import { IState } from '../../store';
 import { Content } from '../../features/myBuJo/interface';
 import ContentItem from './content-item.component';
 
 type ContentListProps = {
-  noteId: number;
+  projectItem: ProjectItem;
   contents: Content[];
   updateNoteContents: (noteId: number) => void;
 };
 
 const ContentList: React.FC<ContentListProps> = ({
-  noteId,
+  projectItem,
   contents,
   updateNoteContents,
 }) => {
   useEffect(() => {
-    noteId && updateNoteContents(noteId);
-  }, [noteId]);
+    projectItem && projectItem.id && updateNoteContents(projectItem.id);
+  }, [projectItem]);
 
   return (
     <List itemLayout='vertical'>
       {contents &&
         contents.map((content) => (
-          <ContentItem noteId={noteId} key={content.id} content={content} />
+          <ContentItem
+            projectItem={projectItem}
+            key={content.id}
+            content={content}
+          />
         ))}
     </List>
   );
--- frontend/src/pages/note/note-detail.pages.tsx
+++ frontend/src/pages/note/note-detail.pages.tsx
@@ -3,62 +3,68 @@
 import React from 'react';
 // features
 //actions
-import {Note} from '../../features/notes/interface';
+import { Note } from '../../features/notes/interface';
 // components
 import NoteContentList from '../../components/content/content-list.component';
 // antd imports
-import {Avatar, Divider, Tooltip} from 'antd';
+import { Avatar, Divider, Tooltip } from 'antd';
 import './note-page.styles.less';
 import 'braft-editor/dist/index.css';
-import {ProjectType} from '../../features/project/constants';
+import { ProjectType } from '../../features/project/constants';
 import DraggableLabelsList from '../../components/draggable-labels/draggable-label-list.component';
 // modals import
 
 export type NoteProps = {
-    note: Note;
+  note: Note;
 };
 
 type NoteDetailProps = {
-    labelEditable: boolean;
-    noteOperation: Function;
-    createContentElem: React.ReactNode;
-    noteEditorElem: React.ReactNode;
-}
+  labelEditable: boolean;
+  noteOperation: Function;
+  createContentElem: React.ReactNode;
+  noteEditorElem: React.ReactNode;
+};
 
 const NoteDetailPage: React.FC<NoteProps & NoteDetailProps> = (props) => {
-    const {note, labelEditable, noteOperation, noteEditorElem, createContentElem} = props;
+  const {
+    note,
+    labelEditable,
+    noteOperation,
+    noteEditorElem,
+    createContentElem,
+  } = props;
 
-    return (
-        <div className='note-page'>
-            <Tooltip placement='top' title={note.owner} className='note-avatar'>
+  return (
+    <div className='note-page'>
+      <Tooltip placement='top' title={note.owner} className='note-avatar'>
         <span>
-          <Avatar size='large' src={note.ownerAvatar}/>
+          <Avatar size='large' src={note.ownerAvatar} />
         </span>
-            </Tooltip>
+      </Tooltip>
 
-            <div className='note-title'>
-                <div className='label-and-name'>
-                    {note.name}
-                    <DraggableLabelsList
-                        mode={ProjectType.NOTE}
-                        labels={note.labels}
-                        editable={labelEditable}
-                        itemId={note.id}
-                    />
-                </div>
-
-                {noteOperation()}
-            </div>
-            <Divider/>
-            <div className='content'>
-                <div className='content-list'>
-                    <NoteContentList noteId={note.id}/>
-                </div>
-                {createContentElem}
-            </div>
-            {noteEditorElem}
+      <div className='note-title'>
+        <div className='label-and-name'>
+          {note.name}
+          <DraggableLabelsList
+            mode={ProjectType.NOTE}
+            labels={note.labels}
+            editable={labelEditable}
+            itemId={note.id}
+          />
         </div>
-    );
+
+        {noteOperation()}
+      </div>
+      <Divider />
+      <div className='content'>
+        <div className='content-list'>
+          <NoteContentList projectItem={note} />
+        </div>
+        {createContentElem}
+      </div>
+      {noteEditorElem}
+    </div>
+  );
 };
 
 export default NoteDetailPage;
