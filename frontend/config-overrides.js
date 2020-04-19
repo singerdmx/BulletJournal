d29098abc942ecb65d21da5c7985b24003100285
Rongkai Liu <milkrong121@outlook.com>
2020-04-19 13:26:32 -0700

    move all color config to config-overrides

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,13 +1,18 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const path = require('path');
+const { updateConfig } = require('react-app-rewire-antd-theme');
 
-module.exports = override(
-  fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
-    style: 'true'
-  }),
-  addLessLoader({
-    javascriptEnabled: true,
-    modifyVars: { '@primary-color' : '#fcd4e5'},
-  })
-);
+const options = {
+  stylesDir: path.join(__dirname, './src/styles'),
+  antDir: path.join(__dirname, './node_modules/antd'),
+  varFile: path.join(__dirname, './src/styles/vars.less'),
+  mainLessFile: path.join(__dirname, './src/styles/main.less'),
+  themeVariables: ['@primary-color'],
+  indexFileName: 'index.html',
+  outputFilePath: path.join(__dirname, './public/color.less'),
+  customColorRegexArray: [/^darken\(.*\)$/],
+};
+
+module.exports = function override(config, env) {
+  config = updateConfig(config, env, options);
+  return config;
+};
