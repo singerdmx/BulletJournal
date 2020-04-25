672a08fc5369eac81e7e29141eb83ff54e3fadb5
Xin Yao <singerdmx@gmail.com>
2020-04-25 00:48:15 -0700

    Fix TaskRepository

--- backend/src/main/java/com/bulletjournal/repository/TaskRepository.java
+++ backend/src/main/java/com/bulletjournal/repository/TaskRepository.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface TaskRepository extends JpaRepository<Task, Long>, TaskRepositoryCustom {
     List<Task> findTaskByProject(Project project);
 
-    @Query(value = "SELECT * FROM tasks WHERE :assignee = ANY(tasks.assignees)", nativeQuery = true)
+    @Query(value = "SELECT * FROM tasks WHERE :assignee = ANY(tasks.assignees) AND tasks.recurrence_rule IS NOT NULL", nativeQuery = true)
     List<Task> findTasksByAssigneesAndRecurrenceRuleNotNull(String assignee);
 
     Optional<Task> findTaskByGoogleCalendarEventId(String googleCalendarEventId);
@@ -23,10 +23,10 @@ public interface TaskRepository extends JpaRepository<Task, Long>, TaskRepositor
             " AND tasks.start_time >= ':now' AND tasks.reminder_date_time <= ':now'", nativeQuery = true)
     List<Task> findRemindingTasks(@Param("assignee") String assignee, @Param("now") String now);
 
-    @Query(value = "SELECT * FROM tasks WHERE :assignee = ANY(tasks.assignees) AND tasks.startTime IS NOT NULL AND " +
-            "tasks.endTime IS NOT NULL AND " +
-            "((tasks.startTime >= :startTime AND tasks.startTime <= :endTime) OR " +
-            "(tasks.endTime >= :startTime AND tasks.endTime <= :endTime))", nativeQuery = true)
+    @Query(value = "SELECT * FROM tasks WHERE :assignee = ANY(tasks.assignees) AND tasks.start_time IS NOT NULL AND " +
+            "tasks.end_time IS NOT NULL AND " +
+            "((tasks.start_time >= ':startTime' AND tasks.start_time <= ':endTime') OR " +
+            "(tasks.end_time >= ':startTime' AND tasks.end_time <= ':endTime'))", nativeQuery = true)
     List<Task> findTasksOfAssigneeBetween(@Param("assignee") String assignee,
                                           @Param("startTime") String startTime,
                                           @Param("endTime") String endTime);
