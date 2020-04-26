fc87309fc058543efae36ed41f49b0a64af6c236
Xiaozheng(Will) Guo <xguo.tufts@gmail.com>
2020-04-25 17:09:04 -0700

    fix db backup dir

--- deployment/db_backup.sh
+++ deployment/db_backup.sh
@@ -5,5 +5,5 @@ dbContainerId=$(sudo docker ps -aqf "name=db")
 
 timestamp=$(date +"%F-%T")
 
-docker exec -it $dbContainerId sh -c "mkdir -p /var/lib/postgresql/data/backup; pg_dumpall --clean --dbname=postgresql://postgres:docker@localhost:5432/postgres | gzip > /var/db_backup/db_$timestamp.gz"
+docker exec -it $dbContainerId sh -c "mkdir -p /var/db_backup; pg_dumpall --clean --dbname=postgresql://postgres:docker@localhost:5432/postgres | gzip > /var/db_backup/db_$timestamp.gz"
 
