cc890aba137420125204a9881d7da8ea7aa5a2d1
Xin Yao <singerdmx@gmail.com>
2020-04-27 20:17:59 -0700

    Mount auth proxy bin folder

--- deployment/start.sh
+++ deployment/start.sh
@@ -4,6 +4,7 @@ echo -e '\n==>step 1 / 2: create data volume mount points'
 mkdir -p "$HOME/docker/volumes/postgres"
 mkdir -p "$HOME/docker/volumes/log"
 mkdir -p "$HOME/docker/volumes/elasticsearch"
+mkdir -p "$HOME/docker/auth_proxy/bin"
 
 echo -e '\n==>step 2 / 2: docker-compose up -d'
 docker-compose -f docker-compose.yml -f ../elk/docker-compose-prod.yml up -d
