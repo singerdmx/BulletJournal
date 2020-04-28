03af928c41d85113db9999064d3ac4c3402a6cfe
Qianhe Chen <chenqianhe0317@gmail.com>
2020-04-28 15:22:19 -0700

    google calendar event list

--- frontend/src/features/calendarSync/actions.ts
+++ frontend/src/features/calendarSync/actions.ts
@@ -1,14 +1,34 @@
 import { actions } from './reducer';
-import {GoogleCalendarEvent} from "./interface";
+import { GoogleCalendarEvent } from './interface';
 
-export const googleTokenExpirationTimeUpdate = () => actions.googleTokenExpirationTimeUpdate({});
-export const googleCalendarEventListUpdate = (calendarId: string, timezone: string, startDate?: string, endDate?: string) =>
-    actions.googleCalendarEventListUpdate({calendarId: calendarId, timezone: timezone, startDate: startDate, endDate: endDate});
-export const googleCalendarCreateEvents = (projectId: number, events: GoogleCalendarEvent[]) =>
-    actions.googleCalendarCreateEvents({projectId: projectId, events: events});
+export const googleTokenExpirationTimeUpdate = () =>
+  actions.googleTokenExpirationTimeUpdate({});
+export const googleCalendarEventListUpdate = (
+  calendarId: string,
+  timezone: string,
+  startDate?: string,
+  endDate?: string
+) =>
+  actions.googleCalendarEventListUpdate({
+    calendarId: calendarId,
+    timezone: timezone,
+    startDate: startDate,
+    endDate: endDate,
+  });
+export const googleCalendarCreateEvents = (
+  projectId: number,
+  events: GoogleCalendarEvent[]
+) =>
+  actions.googleCalendarCreateEvents({ projectId: projectId, events: events });
 export const updateWatchedProject = (calendarId: string) =>
-    actions.watchedProjectUpdate({calendarId: calendarId});
+  actions.watchedProjectUpdate({ calendarId: calendarId });
 export const watchCalendar = (calendarId: string, projectId: number) =>
-    actions.watchCalendar({calendarId: calendarId, projectId: projectId});
+  actions.watchCalendar({ calendarId: calendarId, projectId: projectId });
 export const unwatchCalendar = (calendarId: string) =>
-    actions.unwatchCalendar({calendarId: calendarId});
+  actions.unwatchCalendar({ calendarId: calendarId });
+export const googleCalendarEventListReceived = (
+  googleCalendarEventList: GoogleCalendarEvent[]
+) =>
+  actions.googleCalendarEventListReceived({
+    googleCalendarEventList: googleCalendarEventList,
+  });
