07634732d6727940a552142526e43669d36d6131
Rongkai Liu <milkrong121@outlook.com>
2020-05-04 09:35:49 -0700

    add unregister

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import createStore from './store';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { unregister } from './serviceWorker';
 import PublicPage from './Public';
 
 const store = createStore();
@@ -20,6 +21,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+unregister();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
