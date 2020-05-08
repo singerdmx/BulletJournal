e00996a1953f051dff756010b2128514136f81db
Xin Yao <singerdmx@gmail.com>
2020-05-08 00:12:14 -0700

    Remove loggerMiddleware

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,8 +1,7 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createLogger } from 'redux-logger';
+import {applyMiddleware, createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
-import { combineReducers } from 'redux-starter-kit';
+import {combineReducers} from 'redux-starter-kit';
 import sagas from './sagas';
 import reducers from './reducers';
 
@@ -11,13 +10,10 @@ export type IState = ReturnType<typeof reducer>;
 
 export default () => {
   const composeEnhancers = composeWithDevTools({});
-  const loggerMiddleware = createLogger({ collapsed: true, duration: true });
   const sagaMiddleware = createSagaMiddleware();
-  const middleware = [loggerMiddleware, sagaMiddleware]
+  const middleware = [sagaMiddleware];
   const middlewares = applyMiddleware(...middleware);
   const store = createStore(reducer, composeEnhancers(middlewares));
-  //enable access to store from window
-  (window as any).store = store;
   sagaMiddleware.run(sagas);
 
   return store;
