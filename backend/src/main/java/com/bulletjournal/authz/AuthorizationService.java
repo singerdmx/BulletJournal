e5275025729651a114f6b9d9deee90172f2cd1a3
Xin Yao <singerdmx@gmail.com>
2020-05-18 22:40:00 -0700

    Fix checkstyle

--- backend/src/main/java/com/bulletjournal/authz/AuthorizationService.java
+++ backend/src/main/java/com/bulletjournal/authz/AuthorizationService.java
@@ -12,11 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Component
 public class AuthorizationService {
