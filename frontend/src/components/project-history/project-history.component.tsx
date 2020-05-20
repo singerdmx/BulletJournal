de2771d14ba8f1e8273ad45141d4203e9de1f65b
Xin Yao <singerdmx@gmail.com>
2020-05-20 16:00:49 -0700

    Update project history

--- frontend/src/components/project-history/project-history.component.tsx
+++ frontend/src/components/project-history/project-history.component.tsx
@@ -1,88 +1,83 @@
 // note item component for note tree
 // react import
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 // antd imports
-import {
-  LinkOutlined,
-} from '@ant-design/icons';
-import { Avatar, Table, Tooltip, Empty } from 'antd';
-import { getIcon } from '../modals/show-project-history.component';
+import {Avatar, Empty, Table, Tooltip} from 'antd';
+import {getIcon} from '../modals/show-project-history.component';
 // assets import
-import { IState } from '../../store';
-import { Activity } from '../../features/project/interface';
+import {IState} from '../../store';
+import {Activity} from '../../features/project/interface';
 import moment from 'moment';
 import {User} from "../../features/group/interface";
 import {ContentAction} from "../../features/project/constants";
 
 type ProjectHistoryProps = {
-  activities: Activity[];
+    activities: Activity[];
 };
 
 const ProjectHistory: React.FC<ProjectHistoryProps> = (props) => {
-  const { activities } = props;
-  const columns = [
-     {
-      title: '',
-      dataIndex: 'action',
-      key: 'action',
-         render: (a: ContentAction) => <Tooltip placement='left' title={a.toString().replace(/_/g, " ")}>
-             {getIcon(a.toString())}
-      </Tooltip>
-     },
-     {
-      title: 'User',
-      dataIndex: 'originator',
-      key: 'originator',
-      render: (u: User) => <Tooltip title={u.alias}>
-        <Avatar src={u.avatar} />
-      </Tooltip>
+    const {activities} = props;
+    const columns = [
+        {
+            title: '',
+            dataIndex: 'action',
+            key: 'action',
+            render: (a: ContentAction) => <Tooltip placement='left' title={a.toString().replace(/_/g, " ")}>
+                {getIcon(a.toString())}
+            </Tooltip>
+        },
+        {
+            title: 'User',
+            dataIndex: 'originator',
+            key: 'originator',
+            render: (u: User) => <Tooltip title={u.alias}>
+                <Avatar src={u.avatar}/>
+            </Tooltip>
 
-    },
-    {
-      title: 'Activity',
-      dataIndex: 'activity',
-      key: 'activity',
-      render: (a: string) => parseActivity(a)
-    },
-    {
-      title: 'Time',
-      dataIndex: 'activityTime',
-      key: 'activityTime',
-      render: (a: string) => moment(a).fromNow()
-    },
-    {
-      title: '',
-      dataIndex: 'link',
-      key: 'link',
-      render: (l: string) => l ? <Link to={l}>
-          <LinkOutlined /> </Link>: null
-    },
-  ];
-  function parseActivity(activity: string): JSX.Element {
-    const res = (
-      <span>
-        {activity.split('##').map((a, index) => {
-          if (index % 2 === 1) {
-            return <strong key={index}>{a}</strong>;
-          }
-          return a;
+        },
+        {
+            title: 'Activity',
+            dataIndex: 'description',
+            key: 'description',
+        },
+        {
+            title: 'Time',
+            dataIndex: 'activityTime',
+            key: 'activityTime',
+            render: (a: string) => moment(a).fromNow()
+        },
+    ];
+
+    function parseActivity(a: Activity) {
+        const res: any = {...a}
+        let description = (
+            <span>
+        {a.activity.split('##').map((a, index) => {
+            if (index % 2 === 1) {
+                return <strong key={index}>{a}</strong>;
+            }
+            return a;
         })}
       </span>
+        );
+        if (a.link) {
+            description = <Link to={a.link}> {description} </Link>
+        }
+        res['description'] = description;
+        return res;
+    }
+
+    if (!activities || activities.length === 0) {
+        return <Empty/>;
+    }
+
+    return (
+        <div>
+            <Table columns={columns} dataSource={activities.map(a => parseActivity(a))}/>
+        </div>
     );
-    return res;
-  }
-
-  if (!activities || activities.length === 0) {
-    return <Empty />;
-  }
-
-  return (
-      <div>
-        <Table columns={columns} dataSource={activities} />
-      </div>
-  );
 };
 
 const mapStateToProps = (state: IState) => ({});
