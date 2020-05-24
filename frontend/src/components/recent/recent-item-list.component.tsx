4cf7aaf3d4ebc3bab74f61c77d184d12d24e4ae6
Qianhe Chen <chenqianhe0317@gmail.com>
2020-05-24 16:09:17 -0700

    fix bug

--- frontend/src/components/recent/recent-item-list.component.tsx
+++ frontend/src/components/recent/recent-item-list.component.tsx
@@ -1,80 +1,81 @@
-import React from 'react';
-import moment from "moment";
-import {dateFormat} from "../../features/myBuJo/constants";
-import {DatePicker, Divider, Tooltip} from "antd";
-import {Link} from "react-router-dom";
-import {IState} from "../../store";
-import {connect} from "react-redux";
-import {updateRecentItemsDates} from "../../features/recent/actions";
+import React, { useEffect } from 'react';
+import moment from 'moment';
+import { dateFormat } from '../../features/myBuJo/constants';
+import { DatePicker, Divider, Tooltip } from 'antd';
+import { Link } from 'react-router-dom';
+import { IState } from '../../store';
+import { connect } from 'react-redux';
+import { updateRecentItemsDates } from '../../features/recent/actions';
+import { updateExpandedMyself } from '../../features/myself/actions';
 
-const {RangePicker} = DatePicker;
+const { RangePicker } = DatePicker;
 
 type RecentItemProps = {
-    todoSelected: boolean;
-    ledgerSelected: boolean;
-    noteSelected: boolean;
-    timezone: string;
-    startDate: string;
-    endDate: string;
-    updateRecentItemsDates: (startDate: string, endDate: string) => void;
+  todoSelected: boolean;
+  ledgerSelected: boolean;
+  noteSelected: boolean;
+  timezone: string;
+  startDate: string;
+  endDate: string;
+  updateRecentItemsDates: (startDate: string, endDate: string) => void;
+  updateExpandedMyself: (updateSettings: boolean) => void;
 };
 
-const RecentItemList: React.FC<RecentItemProps> = (
-    {
-        todoSelected,
-        ledgerSelected,
-        noteSelected,
-        timezone,
-        startDate,
-        endDate,
-        updateRecentItemsDates
-    }) => {
+const RecentItemList: React.FC<RecentItemProps> = ({
+  todoSelected,
+  ledgerSelected,
+  noteSelected,
+  timezone,
+  startDate,
+  endDate,
+  updateRecentItemsDates,
+  updateExpandedMyself,
+}) => {
+  useEffect(() => {
+    updateExpandedMyself(true);
+  }, []);
 
-    const handleRangeChange = (dates: any, dateStrings: string[]) => {
-        updateRecentItemsDates(dateStrings[0], dateStrings[1]);
-    };
+  const handleRangeChange = (dates: any, dateStrings: string[]) => {
+    updateRecentItemsDates(dateStrings[0], dateStrings[1]);
+  };
 
-    return <div>
-        <div className="todo-panel">
-            <RangePicker
-                ranges={{
-                    Today: [moment(), moment()],
-                    'This Week': [moment().startOf('week'), moment().endOf('week')],
-                    'This Month': [
-                        moment().startOf('month'),
-                        moment().endOf('month'),
-                    ],
-                }}
-                allowClear={false}
-                value={[
-                    moment(startDate, dateFormat),
-                    moment(endDate, dateFormat),
-                ]}
-                format={dateFormat}
-                onChange={handleRangeChange}
-            />
-            <Tooltip placement="top" title="Change Time Zone">
-                <Link to="/settings" style={{paddingLeft: '30px'}}>
-                    {timezone}
-                </Link>
-            </Tooltip>
-        </div>
-        <Divider/>
-        <div>
-        </div>
+  return (
+    <div>
+      <div className='todo-panel'>
+        <RangePicker
+          ranges={{
+            Today: [moment(), moment()],
+            'This Week': [moment().startOf('week'), moment().endOf('week')],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+          }}
+          allowClear={false}
+          value={[moment(startDate, dateFormat), moment(endDate, dateFormat)]}
+          format={dateFormat}
+          onChange={handleRangeChange}
+        />
+        <Tooltip placement='top' title='Change Time Zone'>
+          <Link to='/settings' style={{ paddingLeft: '30px' }}>
+            {timezone}
+          </Link>
+        </Tooltip>
+      </div>
+      <Divider />
+      <div></div>
     </div>
+  );
 };
 
 const mapStateToProps = (state: IState) => ({
-    timezone: state.myself.timezone,
-    startDate: state.recent.startDate,
-    endDate: state.recent.endDate,
-    todoSelected: state.myBuJo.todoSelected,
-    ledgerSelected: state.myBuJo.ledgerSelected,
-    noteSelected: state.myBuJo.noteSelected,
-    items: state.recent.items
+  timezone: state.myself.timezone,
+  startDate: state.recent.startDate,
+  endDate: state.recent.endDate,
+  todoSelected: state.myBuJo.todoSelected,
+  ledgerSelected: state.myBuJo.ledgerSelected,
+  noteSelected: state.myBuJo.noteSelected,
+  items: state.recent.items,
 });
 
 export default connect(mapStateToProps, {
-    updateRecentItemsDates,
+  updateRecentItemsDates,
+  updateExpandedMyself,
 })(RecentItemList);
