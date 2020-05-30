8bca526f2422f07c6b512f79d88b2d26d1e43631
mwangxx0129 <mwangxx@gmail.com>
2020-05-29 23:57:04 -0400

    enable key space event in redis

--- backend/src/main/java/com/bulletjournal/config/RedisConfig.java
+++ backend/src/main/java/com/bulletjournal/config/RedisConfig.java
@@ -5,12 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
-@EnableRedisRepositories(basePackages = "com.bulletjournal.redis")
+@EnableRedisRepositories(basePackages = "com.bulletjournal.redis", enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
     @Autowired
