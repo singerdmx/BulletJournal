390ac569bc7311874c855b7e33dd89328c60c311
Michael Zhou <michael.zhouzzy@gmail.com>
2020-06-11 01:18:27 -0700

    Completed search result validation and return SearchResult model to frontend

--- backend/src/main/java/com/bulletjournal/controller/QueryController.java
+++ backend/src/main/java/com/bulletjournal/controller/QueryController.java
@@ -1,6 +1,8 @@
 package com.bulletjournal.controller;
 
 import com.bulletjournal.clients.UserClient;
+import com.bulletjournal.controller.models.SearchResult;
+import com.bulletjournal.es.SearchService;
 import com.bulletjournal.es.repository.SearchIndexDaoJpa;
 import com.bulletjournal.es.repository.models.SearchIndex;
 import com.bulletjournal.repository.NoteDaoJpa;
@@ -12,7 +14,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 
 @RestController
@@ -30,9 +32,13 @@ public class QueryController {
 
     protected static final String SEARCH_ROUTE = "/api/query";
     private static final Logger LOGGER = LoggerFactory.getLogger(NoteController.class);
+    private static final String CONTENT_TYPE_SUFFIX = "content";
+    private static final char SEARCH_INDEX_SPLITTER = '@';
 
     @Autowired
     private SearchIndexDaoJpa searchIndexDaoJpa;
+    @Autowired
+    private SearchService searchService;
     @Autowired
     private TaskDaoJpa taskDaoJpa;
     @Autowired
@@ -44,53 +50,78 @@ public class QueryController {
      * [Query Controller Search API] (Fuzziness Search)
      * Search API takes a string of term and send a search query to Elastic Search JPA with
      * requested term and username.
+     * <p>
+     * Validate search results before response. If result is invalid, remove search index from
+     * elastic search database and skip this result.
      *
      * @param term user input term to be searched in Elastic Search DB
-     * @return a list of returned Search Indices
+     * @return a list of returned SearchResult. Each search result contains id and list of highlights
      */
     @GetMapping(SEARCH_ROUTE)
     @ResponseStatus(HttpStatus.OK)
-    public List<SearchHit<SearchIndex>> search(@Valid @RequestParam @NotBlank String term) {
+    public List<SearchResult> search(@Valid @RequestParam @NotBlank String term) {
         String username = MDC.get(UserClient.USER_NAME_KEY);
-        return searchIndexDaoJpa.search(username, term).getSearchHits();
+
+        List<SearchResult> results = new ArrayList<>();
+        List<SearchIndex> invalidResults = new ArrayList<>();
+
+        searchIndexDaoJpa.search(username, term).getSearchHits().forEach(searchHit -> {
+            SearchIndex index = searchHit.getContent();
+
+            // If search result is not present in db, add result to invalid results list
+            if (!searchResultExists(index, username)) {
+                invalidResults.add(index);
+                return;
+            }
+            SearchResult searchResult = new SearchResult();
+            searchResult.setId(index.getId());
+
+            // Iterate through highlights and add them to Search Result Highlights
+            Map<String, List<String>> highlights = searchHit.getHighlightFields();
+            for (String keyword : highlights.keySet()) {
+                searchResult.setOrDefaultHighlights(highlights.get(keyword));
+            }
+            results.add(searchResult);
+        });
+
+        // Batch remove all invalid results from ElasticSearch
+        searchService.removeInvalidSearchResults(invalidResults);
+        return results;
     }
 
     /**
-     * Validate search results present in DB. If not present, will filter out the indices.
-     * TODO: Remove Invalid Search Indices
-     *
-     * @param searchIndices list of search indices
-     * @param username      requester username
-     * @return a list of search indices after filtered
-     */
-    private List<SearchIndex> validateSearchResults(List<SearchIndex> searchIndices, String username) {
-        return searchIndices
-                .stream()
-                .filter(s -> searchResultExists(s, username))
-                .collect(Collectors.toList());
-    }
-
-    /**
-     * Called by validateSearchResults to validate single search index
+     * Validate search result still exists.
+     * <p>
+     * If the "resource not found" exception thrown by DaoJpa, return false to indicate resource not exist.
+     * Otherwise, return true only if result is not null.
      *
      * @param searchIndex target search index
      * @param username    requester username
-     * @return boolean whether the search result is valid
+     * @return true if search result is valid
      */
     private boolean searchResultExists(SearchIndex searchIndex, String username) {
-        String searchIndexId = searchIndex.getParentId();
-        String type = searchIndexId.substring(0, searchIndexId.indexOf('@'));
-        String id = searchIndexId.substring(searchIndexId.indexOf('@') + 1);
+        String searchIndexId = searchIndex.getId();
+        String type = searchIndexId.substring(0, searchIndexId.indexOf(SEARCH_INDEX_SPLITTER));
+        String id = searchIndexId.substring(searchIndexId.indexOf(SEARCH_INDEX_SPLITTER) + 1);
+
         if (StringUtils.isBlank(type) || StringUtils.isBlank(id)) {
             throw new IllegalStateException("Search Index Format is Incorrect");
         }
 
+        boolean isContent = type.endsWith(CONTENT_TYPE_SUFFIX);
+
         ProjectItemDaoJpa projectItemDaoJpa = getProjectItemDao(type);
-        return projectItemDaoJpa.getProjectItem(Long.parseLong(id), username) != null;
+
+        try {
+            return isContent ? projectItemDaoJpa.getContent(Long.parseLong(id), username) != null :
+                    projectItemDaoJpa.getProjectItem(Long.parseLong(id), username) != null;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 
     /**
-     * Get Project Item Dao based on input type
+     * Get ProjectItemDaoJpa based on input type
      *
      * @param type requested dao type
      * @return ProjectItemDao instance
@@ -98,10 +129,13 @@ public class QueryController {
     private ProjectItemDaoJpa getProjectItemDao(String type) {
         switch (type) {
             case "task":
+            case "task_content":
                 return taskDaoJpa;
             case "transaction":
+            case "transaction_content":
                 return transactionDaoJpa;
             case "note":
+            case "note_content":
                 return noteDaoJpa;
             default:
                 throw new IllegalArgumentException("Cannot recognize input type");
