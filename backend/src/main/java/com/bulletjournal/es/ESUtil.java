c18873ac3d1d28a0a12008a83d18808340d0b633
levy <levy0601@gmail.com>
2020-06-16 23:46:47 -0700

    change highlight tag

--- backend/src/main/java/com/bulletjournal/es/ESUtil.java
+++ backend/src/main/java/com/bulletjournal/es/ESUtil.java
@@ -33,8 +33,8 @@ public class ESUtil {
             note.setId((long) i);
             note.setType(ContentType.NOTE);
             note.setName("Note" + i);
-            note.setNameHighlights(Collections.singletonList("The quick brown <em classname= 'highlight'>fox</em> jumps over the lazy dog"));
-            note.setContentHighlights(Collections.singletonList("The quick brown <em classname= 'highlight'>fox</em> jumps over the lazy dog"));
+            note.setNameHighlights(Collections.singletonList("The quick brown <em class= 'highlight'>fox</em> jumps over the lazy dog"));
+            note.setContentHighlights(Collections.singletonList("The quick brown <em class= 'highlight'>fox</em> jumps over the lazy dog"));
             mockList.add(note);
         }
 
