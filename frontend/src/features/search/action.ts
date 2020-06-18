61dad5fa83bbd1e44ae69bfdf7c93ce493a53b46
Xin Yao <singerdmx@gmail.com>
2020-06-18 14:46:10 -0700

    Fix

--- frontend/src/features/search/action.ts
+++ frontend/src/features/search/action.ts
@@ -1,6 +1,6 @@
 import {actions} from './reducer';
 
-export const search = (term: string, scrollId?: string) => actions.search({searchTerm: term, scrollId: scrollId});
+export const search = (term: string, scrollId?: string) => actions.search({term: term, scrollId: scrollId});
 export const updateSearchPageNo = (searchPageNo: number) =>
     actions.updateSearchPageNo({searchPageNo: searchPageNo});
 export const updateSearchTerm = (term: string) =>
