8c29a4514b2a7be391f685e74687819eadebdf3c
Xin Yao <singerdmx@gmail.com>
2020-06-21 15:27:48 -0700

    Initial implementation of task status page

--- frontend/src/pages/task-status/task-status.pages.tsx
+++ frontend/src/pages/task-status/task-status.pages.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import './task-status.styles.less';
 import {useHistory, useParams} from "react-router-dom";
 import {Tooltip} from "antd";
 import {UpSquareOutlined} from '@ant-design/icons';
+import {IState} from "../../store";
+import {getTasksByOrder} from "../../features/tasks/actions";
+import {connect} from "react-redux";
+import {Task} from "../../features/tasks/interface";
+import TaskItem from "../../components/project-item/task-item.component";
+import {ProjectItemUIType} from "../../features/project/constants";
 
-type TaskStatusProps = {};
+type TaskStatusProps = {
+    timezone: string;
+    tasksByOrder: Task[];
+    getTasksByOrder: (
+        projectId: number,
+        timezone: string,
+        startDate?: string,
+        endDate?: string
+    ) => void;
+};
 
-const TaskStatusPage: React.FC<TaskStatusProps> = (props) => {
+const TaskStatusPage: React.FC<TaskStatusProps> = (
+    {
+        getTasksByOrder,
+        timezone,
+        tasksByOrder
+    }) => {
     const {projectId} = useParams();
     const history = useHistory();
+    useEffect(() => {
+        if (!projectId) {
+            return;
+        }
+        getTasksByOrder(parseInt(projectId), timezone, undefined, undefined);
+    }, [projectId]);
     return (
         <div className='task-status-page'>
             <div className='task-operation'>
@@ -22,10 +48,33 @@ const TaskStatusPage: React.FC<TaskStatusProps> = (props) => {
                 </Tooltip>
             </div>
             <div>
+                {
+                    tasksByOrder.map((task, index) => {
+                        return (
+                            <div key={index} style={{display: 'flex', alignItems: 'center'}}>
+                                <TaskItem
+                                    task={task}
+                                    type={ProjectItemUIType.ORDER}
+                                    readOnly={false}
+                                    inProject={false}
+                                    inModal={false}
+                                    isComplete={false}
+                                    completeOnlyOccurrence={false}
+                                />
+                            </div>
+                        );
+                    })
+                }
             </div>
         </div>
     );
 };
 
+const mapStateToProps = (state: IState) => ({
+    timezone: state.settings.timezone,
+    tasksByOrder: state.task.tasksByOrder,
+});
 
-export default TaskStatusPage;
+export default connect(mapStateToProps, {
+    getTasksByOrder,
+})(TaskStatusPage);
