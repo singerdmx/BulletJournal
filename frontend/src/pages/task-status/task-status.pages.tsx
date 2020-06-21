7e0c4e59eb73f39b76541fb5272f102a18b11568
Xin Yao <singerdmx@gmail.com>
2020-06-21 16:16:45 -0700

    Update task status page

--- frontend/src/pages/task-status/task-status.pages.tsx
+++ frontend/src/pages/task-status/task-status.pages.tsx
@@ -2,14 +2,16 @@ import React, {useEffect} from 'react';
 
 import './task-status.styles.less';
 import {useHistory, useParams} from "react-router-dom";
-import {Tooltip} from "antd";
-import {UpSquareOutlined} from '@ant-design/icons';
+import {Collapse, Tooltip} from "antd";
+import {CaretRightOutlined, UpSquareOutlined} from '@ant-design/icons';
 import {IState} from "../../store";
 import {getTasksByOrder} from "../../features/tasks/actions";
 import {connect} from "react-redux";
-import {Task} from "../../features/tasks/interface";
-import TaskItem from "../../components/project-item/task-item.component";
+import {Task, TaskStatus} from "../../features/tasks/interface";
 import {ProjectItemUIType} from "../../features/project/constants";
+import TaskItem from "../../components/project-item/task-item.component";
+
+const {Panel} = Collapse;
 
 type TaskStatusProps = {
     timezone: string;
@@ -36,20 +38,39 @@ const TaskStatusPage: React.FC<TaskStatusProps> = (
         }
         getTasksByOrder(parseInt(projectId), timezone, undefined, undefined);
     }, [projectId]);
-    return (
-        <div className='task-status-page'>
-            <div className='task-operation'>
-                <Tooltip title='Go to BuJo'>
-                    <div>
-                        <UpSquareOutlined
-                            onClick={(e) => history.push(`/projects/${projectId}`)}
-                        />
-                    </div>
-                </Tooltip>
-            </div>
-            <div>
+
+    const inProgressTasks = [] as Task[];
+    const nextToDoTasks = [] as Task[];
+    const readyTasks = [] as Task[];
+    const onHoldTasks = [] as Task[];
+    const tasks = [] as Task[];
+    tasksByOrder.forEach((t) => {
+        switch (t.status) {
+            case TaskStatus.IN_PROGRESS:
+                inProgressTasks.push(t);
+                break;
+            case TaskStatus.NEXT_TO_DO:
+                nextToDoTasks.push(t);
+                break;
+            case TaskStatus.READY:
+                readyTasks.push(t);
+                break;
+            case TaskStatus.ON_HOLD:
+                onHoldTasks.push(t);
+                break;
+            default:
+                tasks.push(t);
+        }
+    });
+
+    const getPanel = (status: TaskStatus | undefined, tasks: Task[]) => {
+        if (tasks.length === 0) {
+            return null;
+        }
+        if (!status) {
+            return <Panel header='' key='DEFAULT'>
                 {
-                    tasksByOrder.map((task, index) => {
+                    tasks.map((task, index) => {
                         return (
                             <div key={index} style={{display: 'flex', alignItems: 'center'}}>
                                 <TaskItem
@@ -65,6 +86,54 @@ const TaskStatusPage: React.FC<TaskStatusProps> = (
                         );
                     })
                 }
+            </Panel>
+        }
+
+        return <Panel header={status.toString().replace(/_/g, ' ')} key={status.toString()}>
+            {
+                tasks.map((task, index) => {
+                    return (
+                        <div key={index} style={{display: 'flex', alignItems: 'center'}}>
+                            <TaskItem
+                                task={task}
+                                type={ProjectItemUIType.ORDER}
+                                readOnly={false}
+                                inProject={false}
+                                inModal={false}
+                                isComplete={false}
+                                completeOnlyOccurrence={false}
+                            />
+                        </div>
+                    );
+                })
+            }
+
+        </Panel>
+    };
+
+    return (
+        <div className='task-status-page'>
+            <div className='task-operation'>
+                <Tooltip title='Go to BuJo'>
+                    <div>
+                        <UpSquareOutlined
+                            onClick={(e) => history.push(`/projects/${projectId}`)}
+                        />
+                    </div>
+                </Tooltip>
+            </div>
+            <div>
+                <Collapse
+                    bordered={false}
+                    defaultActiveKey={['IN_PROGRESS', 'NEXT_TO_DO', 'READY', 'ON_HOLD', 'DEFAULT']}
+                    expandIcon={({isActive}) => <CaretRightOutlined rotate={isActive ? 90 : 0}/>}
+                >
+                    {getPanel(TaskStatus.IN_PROGRESS, inProgressTasks)}
+                    {getPanel(TaskStatus.NEXT_TO_DO, nextToDoTasks)}
+                    {getPanel(TaskStatus.READY, readyTasks)}
+                    {getPanel(TaskStatus.ON_HOLD, onHoldTasks)}
+                    {getPanel(undefined, tasks)}
+                </Collapse>
             </div>
         </div>
     );
