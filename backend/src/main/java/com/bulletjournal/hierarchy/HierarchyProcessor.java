89d7511e06ae4ade0a811c2a9edcbb6a12d9c0ec
Richard <wanismvp@gmail.com>
2020-07-06 21:11:41 -0700

    fix shared item move

--- backend/src/main/java/com/bulletjournal/hierarchy/HierarchyProcessor.java
+++ backend/src/main/java/com/bulletjournal/hierarchy/HierarchyProcessor.java
@@ -3,11 +3,9 @@ package com.bulletjournal.hierarchy;
 import com.bulletjournal.exceptions.BadRequestException;
 import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class HierarchyProcessor {
@@ -74,6 +72,39 @@ public class HierarchyProcessor {
         }
 
         return null;
+    }
+
+
+    public static Pair<List<HierarchyItem>, Set<Long>> findAllIds(String relations, Set<Long> existingIds) {
+        List<HierarchyItem> hierarchyItems = getItemsFromJson(relations);
+        Set<Long> processedIds = new HashSet<>();
+        List<HierarchyItem> ret = new ArrayList<>();
+
+        for (int i = 0; i < hierarchyItems.size(); i++) {
+            HierarchyItem hierarchyItem = findAllIds(hierarchyItems.get(i), existingIds, processedIds);
+            if (hierarchyItem != null) {
+                ret.add(hierarchyItem);
+            }
+        }
+
+        return Pair.of(ret, processedIds);
+    }
+
+    private static HierarchyItem findAllIds(HierarchyItem hierarchyItem, Set<Long> existingIds, Set<Long> processedIds) {
+        if (hierarchyItem == null || (existingIds != null && !existingIds.contains(hierarchyItem.getId()))) {
+            return null;
+        }
+        processedIds.add(hierarchyItem.getId());
+        List<HierarchyItem> children = new ArrayList<>();
+        for (HierarchyItem child : hierarchyItem.getS()) {
+            if (existingIds == null || existingIds.contains(child.getId())) {
+                findAllIds(child, existingIds, processedIds);
+                children.add(child);
+            }
+
+        }
+        hierarchyItem.setS(children);
+        return hierarchyItem;
     }
 
     /**
--- backend/src/test/java/com/bulletjournal/hierarchy/HierarchyProcessorProcessorTest.java
+++ backend/src/test/java/com/bulletjournal/hierarchy/HierarchyProcessorProcessorTest.java
@@ -4,6 +4,8 @@ import com.bulletjournal.controller.models.Project;
 import com.bulletjournal.repository.models.Group;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
 import java.util.*;
@@ -38,6 +40,69 @@ public class HierarchyProcessorProcessorTest {
         projectRelations.add(p5);
         p5.addSubProject(p6);
         return projectRelations;
+    }
+
+    /**
+     * Tests {@link HierarchyProcessor#findAllIds(String, Set)}
+     */
+    @Test
+    public void testFindAllIds() {
+        List<Project> projects = new ArrayList<>();
+        for (long i = 1; i <= 6; i++) {
+            projects.add(createProject(i));
+        }
+        Project p1 = projects.get(0);
+        Project p2 = projects.get(1);
+        Project p3 = projects.get(2);
+        Project p4 = projects.get(3);
+        Project p5 = projects.get(4);
+        Project p6 = projects.get(5);
+        List<Project> projectHierarchy = createSampleProjectRelations(p1, p2, p3, p4, p5, p6);
+
+        /**
+         *  p1
+         *   |
+         *    -- p2
+         *   |   |
+         *   |    -- p3
+         *   |
+         *    -- p4
+         *
+         *  p5
+         *   |
+         *    -- p6
+         */
+        String relations = ProjectRelationsProcessor.processRelations(projectHierarchy);
+
+        Set<Long> set = new HashSet<>();
+        set.add(p1.getId());
+        set.add(p6.getId());
+
+        Pair<List<HierarchyItem>, Set<Long>> ret = HierarchyProcessor.findAllIds(relations, set);
+
+        List<HierarchyItem> hierarchy = ret.getLeft();
+
+        assertEquals(1, hierarchy.size());
+        assertEquals(1L, hierarchy.get(0).getId().longValue());
+        assertEquals(0, hierarchy.get(0).getS().size());
+        CollectionUtils.isEqualCollection(ImmutableSet.of(1L), ret.getRight());
+
+
+        ret = HierarchyProcessor.findAllIds(relations, null);
+        hierarchy = ret.getLeft();
+        assertEquals(2, hierarchy.size());
+        CollectionUtils.isEqualCollection(ImmutableSet.of(1L, 2L, 3L, 4L, 5L, 6L), ret.getRight());
+
+        set = new HashSet<>();
+        set.add(p1.getId());
+        set.add(p3.getId());
+        set.add(p4.getId());
+        set.add(p5.getId());
+
+        ret = HierarchyProcessor.findAllIds(relations, set);
+        hierarchy = ret.getLeft();
+        assertEquals(2, hierarchy.size());
+        CollectionUtils.isEqualCollection(ImmutableSet.of(3L), ret.getRight());
     }
 
     /**
