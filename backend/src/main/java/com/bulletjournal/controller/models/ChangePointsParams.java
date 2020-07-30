5cdd289e58b1e04bf7039db6efbd331ed0f60aed
singerdmx <singerdmx@gmail.com>
2020-07-30 10:18:43 -0700

    Revert "add user point activity for admin page"
    This reverts commit 2c847f4e7a2407970df8b95bf68edbfa320eb873.

--- backend/src/main/java/com/bulletjournal/controller/models/ChangePointsParams.java
+++ backend/src/main/java/com/bulletjournal/controller/models/ChangePointsParams.java
@@ -2,17 +2,8 @@ package com.bulletjournal.controller.models;
 
 public class ChangePointsParams {
     private Integer points;
-    private String description;
 
     public ChangePointsParams() {
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public Integer getPoints() {
