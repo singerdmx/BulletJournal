14a377f446e92ac4371e83e0cf86fb2a3d097b43
Richard <wanismvp@gmail.com>
2020-07-31 10:37:47 -0700

    remove import

--- backend/src/main/java/com/bulletjournal/controller/UserController.java
+++ backend/src/main/java/com/bulletjournal/controller/UserController.java
@@ -14,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Objects;
