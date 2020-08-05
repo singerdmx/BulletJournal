f96e0c743793ea0f0b7b752acd58f1245ac1dc9f
lli12 <lli12@syr.edu>
2020-08-05 00:02:08 -0400

    updated day calendar event time

--- frontend/src/components/project-item-list/project-item-list.component.tsx
+++ frontend/src/components/project-item-list/project-item-list.component.tsx
@@ -120,6 +120,7 @@ class ProjectItemList extends React.Component<ProjectItemProps & PathProps> {
               timeslots={1}
               step={60}
               onSelectEvent={(event: any, e: any) => this.props.history.push(event.resource)}
+              formats={format}
             />
           </div>);
       case 'week':
