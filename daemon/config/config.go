8c897281ff26bb6537c6d4988c4b9edf0fd7e235
Michael Zhou <michael.zhouzzy@gmail.com>
2020-08-07 02:56:33 -0700

    Added getConfig

--- daemon/config/config.go
+++ daemon/config/config.go
@@ -17,15 +17,63 @@ var (
 )
 
 type Config struct {
-	Username string
-	Password string
-	Database string
-	Port     string
-	Host     string
-	DBDriver string
+	Username      string
+	Password      string
+	Database      string
+	Port          string
+	Host          string
+	DBDriver      string
+	ApiKeyPublic  string
+	ApiKeyPrivate string
 }
 
 var daemonServiceConfig Config
+
+func GetConfig() *Config {
+	if IsValid(&daemonServiceConfig) {
+		log.Fatal("invalid configuration")
+	}
+
+	return &daemonServiceConfig
+}
+
+func IsValid(c *Config) bool {
+	valid := true
+	if c.Username == "" {
+		valid = false
+		_ = fmt.Errorf("username is missing from config")
+	}
+	if c.Password == "" {
+		valid = false
+		_ = fmt.Errorf("password is missing from config")
+	}
+	if c.Database == "" {
+		valid = false
+		_ = fmt.Errorf("database is missing from config")
+	}
+	if c.Host == "" {
+		valid = false
+		_ = fmt.Errorf("host is missing from config")
+	}
+	if c.Port == "" {
+		valid = false
+		_ = fmt.Errorf("port is missing from config")
+	}
+	if c.DBDriver == "" {
+		valid = false
+		_ = fmt.Errorf("dbdriver is missing from config")
+	}
+	if c.ApiKeyPublic == "" {
+		valid = false
+		_ = fmt.Errorf("apikeypublic is missing from config")
+	}
+	if c.ApiKeyPrivate == "" {
+		valid = false
+		_ = fmt.Errorf("apikeyprivate is missing from config")
+	}
+
+	return valid
+}
 
 func SetProdConfig() {
 	setConfig(configNameProd)
@@ -59,4 +107,6 @@ func setConfig(configName string) {
 	fmt.Printf("Port from config: %s\n", daemonServiceConfig.Port)
 	fmt.Printf("HOST from config: %s\n", daemonServiceConfig.Host)
 	fmt.Printf("DB Driver from config: %s\n", daemonServiceConfig.DBDriver)
+	fmt.Printf("ApiKeyPublic from config: %s\n", daemonServiceConfig.ApiKeyPublic)
+	fmt.Printf("ApiKeyPrivate from config: %s\n", daemonServiceConfig.ApiKeyPrivate)
 }
