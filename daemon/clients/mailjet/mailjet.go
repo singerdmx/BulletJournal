1913f079b5611695d69d30f91e572be3f8bef947
Michael Zhou <michael.zhouzzy@gmail.com>
2020-08-07 02:57:06 -0700

    Mailjet client

---
+++ daemon/clients/mailjet/mailjet.go
@@ -0,0 +1,56 @@
+package mailjet
+
+import (
+	"fmt"
+	"github.com/mailjet/mailjet-apiv3-go"
+	"github.com/singerdmx/BulletJournal/daemon/config"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Group struct {
+	Id    uint64
+	Name  string
+	Owner string
+}
+
+const (
+	ACCEPT  string = "accept"
+	DECLINE string = "decline"
+)
+
+func GetUrl(uuid uint64, action string) string {
+	return "https://bulletjournal.us/public/notifications/" + strconv.FormatUint(uuid, 10) + "/answer?action=" + action
+}
+
+func SendJoinGroupEmail(username, email string, groupId, notificationId uint64) {
+	g := Group{groupId, "g1", "X"} // TODO: query group from db
+	acceptUrl := GetUrl(notificationId, ACCEPT)
+	declineUrl := GetUrl(notificationId, DECLINE)
+	c := *config.GetConfig()
+	mailjetClient := mailjet.NewMailjetClient(os.Getenv(c.ApiKeyPublic), os.Getenv(c.ApiKeyPrivate))
+	messagesInfo := []mailjet.InfoMessagesV31{
+		{
+			From: &mailjet.RecipientV31{
+				Email: "bulletjournal1024@hotmail.com",
+				Name:  "Bullet Journal",
+			},
+			To: &mailjet.RecipientsV31{
+				mailjet.RecipientV31{
+					Email: email,
+					Name:  username,
+				},
+			},
+			Subject:  "You are invited to Group " + g.Name + " by " + g.Owner,
+			TextPart: "Dear " + username + ",",
+			HTMLPart: "<a href=\"" + acceptUrl + "\">Accept</a><br /><a href=\"" + declineUrl + "\">Decline</a>",
+		},
+	}
+	messages := mailjet.MessagesV31{Info: messagesInfo}
+	res, err := mailjetClient.SendMailV31(&messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Data: %+v\n", res)
+}
--- daemon/go.mod
+++ daemon/go.mod
@@ -4,13 +4,15 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.4.2
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/grpc-ecosystem/grpc-gateway v1.14.6
 	github.com/lib/pq v1.8.0 // indirect
-	github.com/mailjet/mailjet-apiv3-go v0.0.0-20190724151621-55e56f74078c // indirect
-	github.com/spf13/viper v1.7.0 // indirect
+	github.com/mailjet/mailjet-apiv3-go v0.0.0-20190724151621-55e56f74078c
+	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.30.0
 	google.golang.org/protobuf v1.25.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 	upper.io/db.v3 v3.7.1+incompatible
 )
