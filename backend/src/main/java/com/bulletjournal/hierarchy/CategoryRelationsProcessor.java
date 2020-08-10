029956f842ff16f7857d9703bc604b651aef1012
singerdmx <singerdmx@gmail.com>
2020-08-10 02:22:21 -0700

    Reformat code

--- backend/src/main/java/com/bulletjournal/hierarchy/CategoryRelationsProcessor.java
+++ backend/src/main/java/com/bulletjournal/hierarchy/CategoryRelationsProcessor.java
@@ -13,17 +13,21 @@ public class CategoryRelationsProcessor {
     private static final String SUB_CATEGORIES_KEY = "subCategories";
 
     private static final Gson GSON = new GsonBuilder()
-        .excludeFieldsWithoutExposeAnnotation().create();
+            .excludeFieldsWithoutExposeAnnotation().create();
 
     private static final Gson HIERARCHY_ITEM_GSON = new Gson();
 
-    public static List<com.bulletjournal.templates.controller.model.Category> processRelations(
-        Map<Long, Category> categoryMap, String relations
-    ) {
+    public static List<com.bulletjournal.templates.controller.model.Category>
+    processRelations(Map<Long, Category> categoryMap, List<HierarchyItem> relations) {
+        return processRelations(categoryMap, HIERARCHY_ITEM_GSON.toJson(relations));
+    }
+
+    private static List<com.bulletjournal.templates.controller.model.Category> processRelations(
+            Map<Long, Category> categoryMap, String relations) {
         com.bulletjournal.templates.controller.model.Category[] list
-            = GSON.fromJson(
+                = GSON.fromJson(
                 relations.replace(HierarchyItem.SUB_ITEMS_KEY_REPLACEMENT, SUB_CATEGORIES_KEY),
-                    com.bulletjournal.templates.controller.model.Category[].class);
+                com.bulletjournal.templates.controller.model.Category[].class);
         List<com.bulletjournal.templates.controller.model.Category> categories = new ArrayList<>();
         for (com.bulletjournal.templates.controller.model.Category category : list) {
             categories.add(merge(categoryMap, category));
@@ -31,21 +35,15 @@ public class CategoryRelationsProcessor {
         return categories;
     }
 
-    public static List<com.bulletjournal.templates.controller.model.Category>
-    processRelations(Map<Long, Category> categoryMap, List<HierarchyItem> relations) {
-        return processRelations(categoryMap, HIERARCHY_ITEM_GSON.toJson(relations));
-    }
-
     public static String processRelations(List<com.bulletjournal.templates.controller.model.Category> categories) {
         String jsonString = GSON.toJson(categories);
-        // replace "subNotes" with "s" to save space
+        // replace "subCategories" with "s" to save space
         return jsonString.replace(SUB_CATEGORIES_KEY, HierarchyItem.SUB_ITEMS_KEY_REPLACEMENT);
     }
 
     private static com.bulletjournal.templates.controller.model.Category merge(
-        Map<Long, Category> categoryMap,
-        com.bulletjournal.templates.controller.model.Category cur
-    ) {
+            Map<Long, Category> categoryMap,
+            com.bulletjournal.templates.controller.model.Category cur) {
         cur.clone(categoryMap.get(cur.getId()).toPresentationModel());
         for (com.bulletjournal.templates.controller.model.Category subCategory : cur.getSubCategories()) {
             merge(categoryMap, subCategory);
