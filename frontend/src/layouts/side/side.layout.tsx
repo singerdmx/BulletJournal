3e32089aaa59460de4d3d4746afba016fc0a96ca
singerdmx <singerdmx@gmail.com>
2020-08-14 11:22:55 -0700

    Side menu slider to change width

--- frontend/src/layouts/side/side.layout.tsx
+++ frontend/src/layouts/side/side.layout.tsx
@@ -8,11 +8,51 @@ import './side.styles.less';
 
 const {Sider} = Layout;
 
-class SideLayout extends React.Component {
+type SiderProps = {
+    setLayoutMarginLeft: (w: number) => void;
+};
+
+type SiderState = {
+    w: number;
+};
+
+class SideLayout extends React.Component<SiderProps, SiderState> {
+    state: SiderState = {
+        w: 249,
+    };
+
+    onDrag = (e: React.DragEvent<HTMLDivElement>) => {
+        // const elem = e.target as HTMLDivElement;
+        // elem.setAttribute('style', 'left:' + e.pageX + 'px;top: 45%;');
+        if (e.pageX === 0) {
+            return;
+        }
+        this.props.setLayoutMarginLeft(e.pageX + 4);
+        this.setState({w: e.pageX + 3});
+    }
+
+    onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
+        if (e.pageX === 0) {
+            return;
+        }
+        const elem = e.target as HTMLDivElement;
+        elem.setAttribute('style', 'left:' + e.pageX + 'px;top: 45%;opacity: 1;');
+    }
+
+    onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+        const elem = e.target as HTMLDivElement;
+        // e.dataTransfer.effectAllowed = 'move';
+        elem.setAttribute('style', 'opacity: 0.3;');
+    }
+
+    onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+        return false;
+    }
 
     render() {
         return (
-            <Sider width={249} className="sider">
+            <Sider width={this.state.w} className="sider">
                 <div className="sider-header">
                     <img src={logo} alt="Icon" className="icon-img"/>
                     <div className="title">
@@ -20,7 +60,9 @@ class SideLayout extends React.Component {
                     </div>
                 </div>
                 <Tooltip title='Slider' placement='right'>
-                    <div id='sideMenuSlider'>
+                    <div id='sideMenuSlider' draggable={true}
+                         onDrag={this.onDrag} onDrop={this.onDrop}
+                         onDragEnd={this.onDragEnd} onDragStart={this.onDragStart}>
                         <RightSquareOutlined/>
                     </div>
                 </Tooltip>
