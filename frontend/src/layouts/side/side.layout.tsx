d65fbe640a1a436b5d6bc9e1d27ddd8e73e4e526
Rongkai Liu <milkrong121@outlook.com>
2020-08-15 10:06:37 -0700

    done with draggable

--- frontend/src/layouts/side/side.layout.tsx
+++ frontend/src/layouts/side/side.layout.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, {
+  createRef,
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+} from 'react';
 import { Layout, Tooltip } from 'antd';
 import SideMenu from '../../components/side-menu/side-menu.component';
 import * as logo from '../../assets/favicon466.ico';
@@ -18,64 +24,57 @@ type SiderState = {
   isDragging: boolean;
 };
 
-class SideLayout extends React.Component<SiderProps, SiderState> {
-  constructor(props: SiderProps) {
-    super(props);
-    this.state = {
-      w: 249,
-      isDragging: false,
+const SideLayout: React.FC<SiderProps & SiderState> = (props) => {
+  const [width, setWidth] = useState(249);
+  const [initWidth, setInitWidth] = useState(0);
+  const dragRef = useRef<HTMLDivElement>(null);
+  const dragHandler = useCallback(
+    _.throttle((e: MouseEvent) => {
+      const ele = document.getElementById('sideMenuSlider');
+      ele!.style.left = `${e.pageX}px`;
+      setWidth((prevWidth) => e.pageX);
+      props.setLayoutMarginLeft(e.pageX);
+    }, 200),
+    []
+  );
+
+  const onDragEnd = useCallback((e: MouseEvent) => {
+    // props.setLayoutMarginLeft(e.clientX);
+    document.removeEventListener('mousemove', dragHandler, false);
+  }, []);
+
+  const onDragStart = useCallback((e: MouseEvent) => {
+    console.log(e.pageX);
+    setInitWidth(e.pageX);
+    document.addEventListener('mousemove', dragHandler, false);
+  }, []);
+
+  useEffect(() => {
+    const ele = document.getElementById('sideMenuSlider');
+    ele!.addEventListener('mousedown', onDragStart, false);
+    window.addEventListener('mouseup', onDragEnd);
+    return () => {
+      //   dragRef.current!.removeEventListener('mousedown', onDragStart, false);
+      window.removeEventListener('mouseup', onDragEnd, false);
     };
-  }
+  }, [onDragStart, onDragEnd]);
 
-  dragHandler = _.throttle((e: React.DragEvent<HTMLDivElement>) => {
-    // const elem = e.target as HTMLDivElement;
-    // elem.setAttribute('style', 'left:' + e.clientX + 'px;top: 45%;');
-    if (!this.state.isDragging) {
-      return;
-    }
-    this.props.setLayoutMarginLeft(e.clientX);
-    this.setState({ w: e.clientX });
-  }, 200);
-
-  onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-    const elem = e.target as HTMLDivElement;
-    elem.setAttribute('style', `left: ${e.clientX}px;top: 45%;opacity: 1;`);
-    this.props.setLayoutMarginLeft(e.clientX);
-    this.setState({ isDragging: false, w: e.clientX });
-  };
-
-  onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
-    const elem = e.target as HTMLDivElement;
-    // e.dataTransfer.effectAllowed = 'move';
-    elem.setAttribute('style', 'opacity: 0.3;');
-    this.setState({ isDragging: true });
-  };
-
-  render() {
-    return (
-      <Sider width={this.state.w} className='sider'>
-        <div className='sider-header'>
-          <img src={logo} alt='Icon' className='icon-img' />
-          <div className='title'>
-            <h2>Bullet Journal</h2>
-          </div>
+  return (
+    <Sider width={width} className='sider'>
+      <div className='sider-header'>
+        <img src={logo} alt='Icon' className='icon-img' />
+        <div className='title'>
+          <h2>Bullet Journal</h2>
         </div>
-        <Tooltip title='Slide' placement='right'>
-          <div
-            id='sideMenuSlider'
-            draggable
-            onDrag={this.dragHandler}
-            onDragEnd={this.onDragEnd}
-            onDragStart={this.onDragStart}
-            style={{ zIndex: 19999 }}
-          >
-            <RightSquareOutlined />
-          </div>
-        </Tooltip>
-        <SideMenu />
-      </Sider>
-    );
-  }
-}
+      </div>
+      <SideMenu />
+      <Tooltip title='Slide' placement='right'>
+        <div id='sideMenuSlider'>
+          <RightSquareOutlined />
+        </div>
+      </Tooltip>
+    </Sider>
+  );
+};
 
 export default SideLayout;
