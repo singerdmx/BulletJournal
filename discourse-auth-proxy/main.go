da91838ef8c3a410cb5cc9a40a85af3d499365c2
singerdmx <singerdmx@gmail.com>
2020-08-18 14:25:15 -0700

    Upgrade auth proxy

--- discourse-auth-proxy/main.go
+++ discourse-auth-proxy/main.go
@@ -241,7 +241,7 @@ func processMobileRequest(handler http.Handler, r *http.Request, w http.Response
 	query := r.URL.Query()
 	if strings.HasPrefix(r.RequestURI, tokenPage) {
 		header := r.Header.Get("Ignore-Cookie")
-		logger.Printf("Ignore-Cookie: %s", header)
+		logger.Printf("headers: %v", r.Header)
 		if username, groups, cookieValue, err := getAuthCookie(r, w); err == nil && header == "" {
 			token := r.RequestURI[len(tokenPage) : len(tokenPage)+6]
 			logger.Printf("Saving token %s", token)
