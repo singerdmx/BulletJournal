a74d7ac3314a9e9878af83723f04149441c5a9d4
singerdmx <singerdmx@gmail.com>
2020-08-25 12:22:49 -0700

    Admin Category update

--- frontend/src/pages/admin/category.pages.tsx
+++ frontend/src/pages/admin/category.pages.tsx
@@ -4,9 +4,10 @@ import {connect} from "react-redux";
 import {deleteCategory, getCategory, updateCategory} from "../../features/templates/actions";
 import {IState} from "../../store";
 import {Category} from "../../features/templates/interface";
-import {BackTop} from "antd";
+import {BackTop, Typography} from "antd";
 import {DeleteFilled} from "@ant-design/icons/lib";
 import ColorPicker from "../../utils/color-picker/ColorPickr";
+const { Title, Text } = Typography;
 
 type AdminCategoryProps = {
     category: Category | undefined;
@@ -40,11 +41,30 @@ const AdminCategoryPage: React.FC<AdminCategoryProps> = (
         updateCategory(category.id, category.name, category.description, category.icon, input.color, category.forumId);
     }
 
+    const nameChange = (input: any) => {
+        console.log(input);
+        updateCategory(category.id, input, category.description, category.icon, category.color, category.forumId);
+    }
+
+    const descriptionChange = (input: any) => {
+        console.log(input);
+        updateCategory(category.id, category.name, input, category.icon, category.color, category.forumId);
+    }
+
+    const forumIdChange = (input: any) => {
+        console.log(input);
+        updateCategory(category.id, category.name, category.description, category.icon, category.color,
+            input ? parseInt(input) : undefined);
+    }
+
     return <div className='admin-categories-page'>
         <BackTop/>
         <div><DeleteFilled onClick={handleDelete}/></div>
         <div style={{backgroundColor: `${category.color}`}}>
-            {category.name}
+            <Title editable={{ onChange: nameChange }}>{category.name}</Title>
+            <Text editable={{ onChange: descriptionChange }}>{`${category.description ? category.description : 'description'}`}</Text>
+            <br/>
+            <Text editable={{ onChange: forumIdChange }}>{`${category.forumId ? category.forumId : 'forumId'}`}</Text>
         </div>
         <ColorPicker
             label='Color  '
