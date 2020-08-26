3b17dd130f54c1f332cf988da7770fe628739d25
mwangxx0129 <mwangxx@gmail.com>
2020-08-25 21:49:47 -0400

    Add selection fk choice_id ON DELETE CASCADE

--- backend/src/main/resources/db/migration/V46__add_selections_table.sql
+++ backend/src/main/resources/db/migration/V46__add_selections_table.sql
@@ -1,21 +1,4 @@
--- Drop table
-
--- DROP TABLE "template".selections;
-
-CREATE TABLE "template".selections (
-	id bigint NOT NULL,
-	created_at timestamp NOT NULL,
-	updated_at timestamp NOT NULL,
-	icon varchar(255) NOT NULL,
-	"text" varchar(255) NOT NULL,
-	choice_id bigint NOT NULL,
-	CONSTRAINT selections_pkey PRIMARY KEY (id),
-	CONSTRAINT fkk333p3bf7y3417x3rfd5fkxij FOREIGN KEY (choice_id) REFERENCES template.choices(id)
-);
-
--- DROP SEQUENCE "template".selection_sequence;
-
-CREATE SEQUENCE "template".selection_sequence
+CREATE SEQUENCE if not exists "template".selection_sequence
 	INCREMENT BY 2
 	MINVALUE 1
 	MAXVALUE 9223372036854775807
@@ -23,7 +6,16 @@ CREATE SEQUENCE "template".selection_sequence
 	CACHE 1
 	NO CYCLE;
 
--- Permissions
+CREATE TABLE if not exists "template".selections (
+	id bigint NOT NULL,
+	created_at timestamp NOT NULL,
+	updated_at timestamp NOT NULL,
+	icon varchar(255) NOT NULL,
+	"text" varchar(255) NOT NULL,
+	choice_id bigint NOT NULL,
+	CONSTRAINT selections_pkey PRIMARY KEY (id),
+	CONSTRAINT choice_foreign_key FOREIGN KEY (choice_id) REFERENCES template.choices(id) ON DELETE CASCADE
+);
 
 ALTER TABLE "template".selections OWNER TO postgres;
 GRANT ALL ON TABLE "template".selections TO postgres;
