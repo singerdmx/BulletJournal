6f3b145bd31d8ad9111a4770be4fb5d91f4f074f
singerdmx <singerdmx@gmail.com>
2020-08-26 20:35:59 -0700

    Add V51__add_new_choice

---
+++ backend/src/main/resources/db/migration/V51__add_new_choice.sql
@@ -0,0 +1,20 @@
+INSERT INTO template.choices (id, created_at, updated_at, multiple, name)
+VALUES (10, '2020-08-26 19:59:23.000000', '2020-08-26 19:59:27.000000', true, 'Computer Science Category');
+
+INSERT INTO template.selections (id, created_at, updated_at, icon, text, choice_id)
+VALUES (52, '2020-08-26 20:08:22.000000', '2020-08-26 20:08:25.000000', null, 'Data Structure and Algorithms', 10);
+
+INSERT INTO template.selections (id, created_at, updated_at, icon, text, choice_id)
+VALUES (53, '2020-08-26 20:09:52.000000', '2020-08-26 20:09:54.000000', null, 'Object Oriented Programming', 10);
+
+INSERT INTO template.selections (id, created_at, updated_at, icon, text, choice_id)
+VALUES (54, '2020-08-26 20:10:37.000000', '2020-08-26 20:10:38.000000', null, 'System Design', 10);
+
+INSERT INTO template.selections (id, created_at, updated_at, icon, text, choice_id)
+VALUES (55, '2020-08-26 20:10:59.000000', '2020-08-26 20:11:01.000000', null, 'Concurrency', 10);
+
+INSERT INTO template.selections (id, created_at, updated_at, icon, text, choice_id)
+VALUES (56, '2020-08-26 20:11:56.000000', '2020-08-26 20:11:57.000000', null, 'Database', 10);
+
+INSERT INTO template.selections (id, created_at, updated_at, icon, text, choice_id)
+VALUES (57, '2020-08-26 20:12:42.000000', '2020-08-26 20:12:43.000000', null, 'Shell Scripting', 10);
