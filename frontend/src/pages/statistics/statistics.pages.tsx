019ac449dc76f464619abcd1cf112a12311c14bd
KetaoChen <chenketao007@gmail.com>
2020-08-28 00:49:35 -0400

    statistics page start & end date

--- frontend/src/pages/statistics/statistics.pages.tsx
+++ frontend/src/pages/statistics/statistics.pages.tsx
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import './statistics.styles.less';
-import {Alert, Avatar, BackTop, Empty, Popover, Progress} from "antd";
+import {Alert, Avatar, BackTop, Empty, Popover, Progress, Tooltip, Select, DatePicker, Space} from "antd";
 import {IState} from '../../store';
 import {connect} from 'react-redux';
 import {TaskStatistics} from '../../features/tasks/interface';
-import {Project} from '../../features/project/interface';
+import {Project, ProjectsWithOwner} from '../../features/project/interface';
 import {getTasksByAssignee, getTaskStatistics} from '../../features/tasks/actions';
 import {getProject} from "../../features/project/actions";
 import {useHistory, useParams} from "react-router-dom";
@@ -12,11 +12,16 @@ import {User} from "../../features/group/interface";
 import TasksByAssignee from "../../components/modals/tasks-by-assignee.component";
 import {Button as FloatButton, Container, darkColors, lightColors} from "react-floating-action-button";
 import {SyncOutlined, UpSquareOutlined} from "@ant-design/icons/lib";
+import { flattenOwnedProject, flattenSharedProject } from '../projects/projects.pages';
+import { ProjectType } from '../../features/project/constants';
+import { iconMapper } from '../../components/project-dnd/project-dnd.component';
 
 type ProjectStatisticsProps = {
     projectStatistics: TaskStatistics | undefined;
     timezone: string;
     project: Project | undefined;
+    ownedProjects: Project[];
+    sharedProjects: ProjectsWithOwner[];
     getTaskStatistics: (
         projectIds: number[],
         timezone: string,
@@ -32,6 +37,8 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
         projectStatistics,
         timezone,
         project,
+        ownedProjects,
+        sharedProjects,
         getTaskStatistics,
         getProject,
         getTasksByAssignee
@@ -41,9 +48,32 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
     const [initialized, setInitialized] = useState(false);
     const [tasksByUsersShown, setTasksByUsersShown] = useState(false);
     const [assignee, setAssignee] = useState<User | undefined>(undefined);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [projectIds, setProjectIds] = useState<number[] | undefined>(project !== undefined ? [project.id] : undefined);
+    const [startTime, setStartTime] = useState<string>('');
+    const [endTime, setEndTime] = useState<string>('');
 
-    const fetchTaskStatistics = (project: Project) => {
-        getTaskStatistics([project.id], timezone, "", "")
+    useEffect(() => {
+        let allProjects = [] as Project[];
+        allProjects = flattenOwnedProject(ownedProjects, allProjects);
+        allProjects = flattenSharedProject(sharedProjects, allProjects);
+        allProjects = allProjects.filter((p) => {
+            if (!project) {
+                return true;
+            }
+            return (
+                p.projectType === ProjectType.TODO && !p.shared
+            );
+        });
+        setProjects(allProjects);
+    }, [ownedProjects, sharedProjects]);
+
+
+    const fetchTaskStatistics = () => {
+        if (projectIds === undefined) {
+            return;
+        }
+        getTaskStatistics(projectIds, timezone, startTime, endTime);
     }
 
     useEffect(() => {
@@ -57,9 +87,13 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
         }
         if (project && !initialized) {
             setInitialized(true);
-            fetchTaskStatistics(project);
+            setProjectIds([project.id]);
         }
     }, [project]);
+
+    useEffect(() => {
+        fetchTaskStatistics();
+    }, [projectIds, timezone, startTime, endTime]);
 
     //by user modal
     const handleGetTasksByAssignee = (u: User) => {
@@ -79,7 +113,35 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
     }
 
     const getPercentage = (data: any) => {
+        if (data.completed + data.uncompleted === 0) {
+            return 0;
+        }
         return Math.round(data.completed * 100 / (data.completed + data.uncompleted));
+    }
+
+    const { Option } = Select;
+    
+    function handleChange(value: any) {
+        if (value.length === 0) return;
+        setProjectIds(value);
+    }
+
+    function handleStartTimeChange(time: any, timeString: string) {
+        if (time) {
+            setStartTime(timeString);
+        }
+        else {
+            setStartTime('');
+        }
+    }
+
+    function handleEndTimeChange(time: any, timeString: string) {
+        if (time) {
+            setEndTime(timeString);
+        }
+        else {
+            setEndTime('');
+        }
     }
 
     return (
@@ -94,7 +156,60 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
                     </>}>
                     <div><Progress width={100} type="circle" percent={getPercentage(projectStatistics)}/></div>
                 </Popover>
+                <div>
+                    <Space direction="vertical" >
+                        <Tooltip title='Start Date'>
+                            <DatePicker 
+                                showTime 
+                                allowClear 
+                                onChange={handleStartTimeChange} 
+                                format="YYYY-MM-DD" 
+                                placeholder='Start Date'/>
+                        </Tooltip>
+                        <Tooltip title='End Date'>
+                            <DatePicker 
+                                showTime 
+                                allowClear 
+                                onChange={handleEndTimeChange} 
+                                format="YYYY-MM-DD"
+                                placeholder='End Date'/>
+                        </Tooltip>
+                    </Space>
+                </div>
+                <div>
+                    <Tooltip title='Select BuJo'>
+                        <Select
+                        mode="multiple"
+                        style={{ minWidth: '100px', maxWidth: '200px' }}
+                        placeholder='Select BuJo'
+                        defaultValue={project.id}
+                        onChange={handleChange}
+                        >
+                            {projects.map((project) => {
+                                return (    
+                                    <Option value={project.id} key={project.id}>
+                                    <Tooltip
+                                        title={project.owner.alias}
+                                        placement='right'
+                                    >
+                                        <span>
+                                        <Avatar size='small' src={project.owner.avatar} />
+                                        &nbsp; {iconMapper[project.projectType]}
+                                        &nbsp; <strong>{project.name}</strong>
+                                        &nbsp; (Group <strong>{project.group.name}</strong>)
+                                        </span>
+                                    </Tooltip>
+                                    </Option>
+                                );
+                            })}
+                        </Select>
+                    </Tooltip>
+                </div>
+                <div>
+                </div>
             </div>
+            
+            
             <div className='user-progress-bars'>
                 {projectStatistics.userTaskStatistics.map(t => {
                     return <Popover
@@ -129,7 +244,11 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
                         <UpSquareOutlined/>
                     </FloatButton>
                     <FloatButton
-                        onClick={() => fetchTaskStatistics(project)}
+                        onClick={() => {
+                            setProjectIds([project.id]);
+                            fetchTaskStatistics();
+                            return;
+                        }}
                         tooltip="Refresh"
                         styles={{backgroundColor: darkColors.grey, color: lightColors.white}}
                     >
@@ -144,11 +263,15 @@ const ProjectStatisticsPage: React.FC<ProjectStatisticsProps> = (
 const mapStateToProps = (state: IState) => ({
     projectStatistics: state.task.projectStatistics,
     timezone: state.settings.timezone,
-    project: state.project.project
+    project: state.project.project,
+    ownedProjects: state.project.owned,
+    sharedProjects: state.project.shared
 });
 
 export default connect(mapStateToProps, {
     getTaskStatistics,
     getProject,
-    getTasksByAssignee,
+    getTasksByAssignee, 
+    flattenOwnedProject,
+    flattenSharedProject
 })(ProjectStatisticsPage);
