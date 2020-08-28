8c5b2d2b2393262d1ed6502357c0ac32a2e95173
singerdmx <singerdmx@gmail.com>
2020-08-27 22:00:25 -0700

    Small fix

--- frontend/src/apis/taskApis.ts
+++ frontend/src/apis/taskApis.ts
@@ -367,7 +367,7 @@ export const getTaskStatistics = (
 ) => {
   // e.g. /api/taskStatistics?projectIds=11&projectIds=12&timezone=America%2FLos_Angeles&startDate=2020-01-01&endDate=2020-09-10
   if (projectIds.length === 0) {
-    return Promise.resolve({'complete': 0, 'incomplete': 0, 'userTaskStatistics': {}});
+    return Promise.resolve({'complete': 0, 'incomplete': 0, 'userTaskStatistics': []});
   }
   return doFetch(
       '/api/taskStatistics?' +
