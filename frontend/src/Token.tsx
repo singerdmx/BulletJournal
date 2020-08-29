fed0db92228cffcb2be47887bfe2ed31cc122ae3
singerdmx <singerdmx@gmail.com>
2020-08-29 12:58:22 -0700

    Update ios link

--- frontend/src/Token.tsx
+++ frontend/src/Token.tsx
@@ -14,7 +14,7 @@ const TokenPage: React.FC<PageProps> = (props) => {
     }
     // iOS detection from: http://stackoverflow.com/a/9039885/177710
     if (/iPad|iPhone|iPod/.test(userAgent)) {
-        hrefLink = 'bullet://bulletjournal.us';
+        hrefLink = 'bullet://us.bulletjournal';
     }
 
     return (
