f414eb382f8e196adab962a7199ae0d016ec28af
airztz <airztz@gmail.com>
2020-08-30 14:01:13 -0400

    Updated async daemon java client

--- backend/src/main/java/com/bulletjournal/clients/DaemonServiceClient.java
+++ backend/src/main/java/com/bulletjournal/clients/DaemonServiceClient.java
@@ -5,23 +5,45 @@ import com.bulletjournal.protobuf.daemon.grpc.types.JoinGroupEvents;
 import com.bulletjournal.protobuf.daemon.grpc.types.ReplyMessage;
 import com.bulletjournal.protobuf.daemon.grpc.types.StreamMessage;
 import com.bulletjournal.protobuf.daemon.grpc.types.SubscribeNotification;
+import com.bulletjournal.repository.GoogleCalendarProjectDaoJpa;
+import io.grpc.Status;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class DaemonServiceClient {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DaemonServiceClient.class);
 
+    @Value("${enableStreaming}")
+    private boolean enableStreaming;
+
     @GrpcClient("daemonClient")
     private DaemonGrpc.DaemonBlockingStub daemonBlockingStub;
 
     @GrpcClient("daemonClient")
     private DaemonGrpc.DaemonStub daemonAsyncStub;
+
+    @Autowired
+    private GoogleCalendarProjectDaoJpa googleCalendarProjectDaoJpa;
+
+    @PostConstruct
+    public void postConstruct() {
+        if (enableStreaming) {
+            LOGGER.info("We're enabling daemon streaming...");
+            subscribeNotification();
+        } else {
+            LOGGER.info("We don't enable daemon streaming as for now...");
+        }
+    }
 
     public String sendEmail(JoinGroupEvents joinGroupEvents) {
         try {
@@ -33,7 +55,35 @@ public class DaemonServiceClient {
         }
     }
 
-    public void subscribeNotification(SubscribeNotification subscribeNotification, StreamObserver<StreamMessage> responseObserver) {
+    private void subscribeNotification() {
+        StreamObserver<StreamMessage> responseObserver = new StreamObserver<StreamMessage>() {
+            @Override
+            public void onNext(StreamMessage stream) {
+                LOGGER.info("Got a daemon streaming message");
+                try {
+                    DaemonServiceClient.this.googleCalendarProjectDaoJpa
+                            .renewGoogleCalendarWatch(stream.getMessage());
+                } catch (Exception e) {
+                    LOGGER.error("renewGoogleCalendarWatch client side error: ", e);
+                }
+                LOGGER.info("Processed a daemon streaming message");
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                Status status = Status.fromThrowable(t);
+                LOGGER.error("renewGoogleCalendarWatch server side error: ", status);
+            }
+
+            @Override
+            public void onCompleted() {
+                LOGGER.info("Stopped receiving GoogleCalendarProjectId");
+            }
+        };
+        subscribeNotification(SubscribeNotification.newBuilder().setId("cleaner").build(), responseObserver);
+    }
+
+    private void subscribeNotification(SubscribeNotification subscribeNotification, StreamObserver<StreamMessage> responseObserver) {
         while (true) {
             try {
                 LOGGER.info("Start subscribing to daemon server");
