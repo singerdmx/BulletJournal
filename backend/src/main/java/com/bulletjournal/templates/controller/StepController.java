395034f6dc511c92c6b697920a3f0ffc75ff5a36
singerdmx <singerdmx@gmail.com>
2020-08-31 00:20:32 -0700

    Add Steps class

--- backend/src/main/java/com/bulletjournal/templates/controller/StepController.java
+++ backend/src/main/java/com/bulletjournal/templates/controller/StepController.java
@@ -1,16 +1,13 @@
 package com.bulletjournal.templates.controller;
 
+import com.bulletjournal.templates.controller.model.Steps;
 import com.bulletjournal.templates.repository.CategoryDaoJpa;
 import com.bulletjournal.templates.repository.StepDaoJpa;
-import com.bulletjournal.templates.workflow.models.Step;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class StepController {
@@ -33,10 +30,10 @@ public class StepController {
     }
 
     @GetMapping(STEPS_ROUTE)
-    public List<Step> getAllSteps() {
+    public Steps getAllSteps() {
         // get all categories with choices
         // get all steps
         // convert them to workflow.Step
-        return new ArrayList<>();
+        return new Steps();
     }
 }
