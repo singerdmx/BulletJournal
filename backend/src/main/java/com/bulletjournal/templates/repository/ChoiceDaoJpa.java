8badf1e15a341d46ef2638443b1b4e4a61ddb012
Jeff <jinian_2006@hotmail.com>
2020-09-01 11:33:20 -0700

    fix unused import

--- backend/src/main/java/com/bulletjournal/templates/repository/ChoiceDaoJpa.java
+++ backend/src/main/java/com/bulletjournal/templates/repository/ChoiceDaoJpa.java
@@ -2,15 +2,12 @@ package com.bulletjournal.templates.repository;
 
 import com.bulletjournal.exceptions.ResourceAlreadyExistException;
 import com.bulletjournal.exceptions.ResourceNotFoundException;
-import com.bulletjournal.templates.controller.model.Category;
 import com.bulletjournal.templates.repository.model.Choice;
-import com.bulletjournal.templates.repository.model.Step;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
