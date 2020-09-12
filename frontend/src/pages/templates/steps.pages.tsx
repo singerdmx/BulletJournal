5d392c24f44f0229a3ba7e1dc5bed085cf2f2bf9
singerdmx <singerdmx@gmail.com>
2020-09-12 00:47:04 -0700

    Show choices in Category page

--- frontend/src/pages/templates/steps.pages.tsx
+++ frontend/src/pages/templates/steps.pages.tsx
@@ -5,9 +5,10 @@ import {IState} from "../../store";
 import {connect} from "react-redux";
 import {getCategory} from "../../features/templates/actions";
 import {Category} from "../../features/templates/interface";
-import {Card, Empty} from "antd";
+import {Card, Empty, Select} from "antd";
 
 const {Meta} = Card;
+const {Option} = Select;
 
 type StepsProps = {
     category: Category | undefined;
@@ -36,6 +37,40 @@ const StepsPage: React.FC<StepsProps> = (
         return <Empty/>
     }
 
+    const getComingSoonDiv = () => {
+        if (category && category.choices.length > 0) {
+            return null;
+        }
+        return <div className='coming-soon'>
+            <img alt='Coming Soon'
+                 src='https://user-images.githubusercontent.com/122956/92905797-d299c600-f3d8-11ea-813a-3ac75c2f5677.gif'/>
+        </div>
+    }
+
+    const getStepsDiv = () => {
+        if (!category || category.choices.length === 0) {
+            return null;
+        }
+        return <div>
+            <div>
+                <div className='choices-card'>
+                    {category.choices.map(choice => {
+                        return <div key={choice.id} className='choice-card'>
+                            <Select mode={choice.multiple ? 'multiple' : undefined}
+                                    placeholder={choice.name}
+                                    style={{padding: '3px'}}
+                                    allowClear>
+                                {choice.selections.map(selection => {
+                                    return <Option key={selection.id} value={selection.id}>{selection.text}</Option>
+                                })}
+                            </Select>
+                        </div>
+                    })}
+                </div>
+            </div>
+        </div>
+    }
+
     return (
         <div className='steps-page'>
             <div className='steps-info'>
@@ -52,9 +87,8 @@ const StepsPage: React.FC<StepsProps> = (
                     />
                 </Card>
             </div>
-            <div className='coming-soon'>
-                <img alt='Coming Soon' src='https://user-images.githubusercontent.com/122956/92905797-d299c600-f3d8-11ea-813a-3ac75c2f5677.gif'/>
-            </div>
+            {getComingSoonDiv()}
+            {getStepsDiv()}
         </div>
     );
 };
