eb8f6f8fe1a052faa01f28e3683e8f870a49e549
singerdmx <singerdmx@gmail.com>
2020-09-14 16:55:32 -0700

    Update workflow page

--- frontend/src/pages/admin/workflow.pages.tsx
+++ frontend/src/pages/admin/workflow.pages.tsx
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import {Container} from "react-floating-action-button";
 import './workflow.styles.less';
-import {BackTop, Button, Divider, Input} from "antd";
+import {BackTop, Button, Divider, Input, Tabs} from "antd";
 import AddChoice from "../../components/modals/templates/add-choice.component";
 import {IState} from "../../store";
 import {connect} from "react-redux";
 import {addSelection, getChoices} from "../../features/templates/actions";
 import {Choice} from "../../features/templates/interface";
 import AdminChoiceElem from "./admin-choice-elem";
+
+const {TabPane} = Tabs;
 
 type WorkflowPageProps = {
     choices: Choice[];
@@ -34,33 +36,47 @@ const AdminWorkflowPage: React.FC<WorkflowPageProps> = (
     return (
         <div className='workflow-page'>
             <BackTop/>
-            <div>
-                <h3>Choices</h3>
-                {choices.map(c => {
-                    return <div onClick={() => setChoice(c)}>
-                        <AdminChoiceElem
-                            choice={c}/>
+            <Tabs defaultActiveKey="1">
+                <TabPane tab="Choices" key="1">
+                    <div>
+                        <h3>Choices</h3>
+                        {choices.map(c => {
+                            return <div onClick={() => setChoice(c)}>
+                                <AdminChoiceElem
+                                    choice={c}/>
+                            </div>
+                        })}
                     </div>
-                })}
-            </div>
-            <Divider/>
-            <div>
-                <h3>Selection</h3>
-                <div>
-                    <b>Target Choice: {choice && choice.name} ({choice && choice.id})</b>
-                </div>
-                <div>
-                    Add Selection:{' '}
-                    <span>
-                        <Input allowClear
-                               onChange={(e) => setSelectionText(e.target.value)}
-                               value={selectionText}
-                               style={{width: '140px'}} placeholder='Selection Text'></Input>
-                    </span>
-                    {' '}
-                    <Button type='primary' onClick={() => addSelection(choice.id, selectionText)}>Add to Target Choice</Button>
-                </div>
-            </div>
+                    <Divider/>
+                    <div>
+                        <h3>Selection</h3>
+                        <div>
+                            <b>Target Choice: {choice && choice.name} ({choice && choice.id})</b>
+                        </div>
+                        <div>
+                            Add Selection:{' '}
+                            <span>
+                                <Input allowClear
+                                       onChange={(e) => setSelectionText(e.target.value)}
+                                       value={selectionText}
+                                       style={{width: '140px'}} placeholder='Selection Text'></Input>
+                            </span>
+                            {' '}
+                            <Button type='primary' onClick={() => addSelection(choice.id, selectionText)}>
+                                Add to Target Choice</Button>
+                        </div>
+                    </div>
+                </TabPane>
+                <TabPane tab="Sample Tasks" key="2">
+                    <div>
+                        <h3>Steps</h3>
+                    </div>
+                    <Divider/>
+                    <div>
+                        <h3>Tasks</h3>
+                    </div>
+                </TabPane>
+            </Tabs>
             <Container>
                 <AddChoice/>
             </Container>
