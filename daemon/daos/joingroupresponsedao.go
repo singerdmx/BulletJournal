ae3fd07c25009a3bec21c2302e0ebfedff727369
Michael Zhou <michael.zhouzzy@gmail.com>
2020-09-18 01:45:57 -0700

    Add join group response dao and model

---
+++ daemon/daos/joingroupresponsedao.go
@@ -0,0 +1,52 @@
+package daos
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/singerdmx/BulletJournal/daemon/daos/models"
+	log "github.com/singerdmx/BulletJournal/daemon/logging"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+const (
+	JoinGroupResponsePrefix     string = "JoinGroupResponse"
+	JoinGroupResponseTimeToLive        = 3600000 * time.Millisecond
+)
+
+type JoinGroupResponseDao struct {
+	Ctx context.Context
+	Rdb *redis.Client
+}
+
+func (j *JoinGroupResponseDao) SingleCache(joinGroupResponse *models.JoinGroupResponse) {
+	key := JoinGroupResponsePrefix + ":" + joinGroupResponse.Uid
+	b, err := json.Marshal(joinGroupResponse.Username)
+	if err != nil {
+		log.Errorf("JoinGroupResponse encoding failed with %v", err)
+		return
+	}
+
+	err = j.Rdb.Set(j.Ctx, key, string(b), JoinGroupResponseTimeToLive).Err()
+	if err != nil {
+		log.Errorf("JoinGroupResponse stored failed with %v", err)
+		panic(err)
+	}
+}
+
+func (j *JoinGroupResponseDao) Find(uid string) *models.JoinGroupResponse {
+	key := JoinGroupResponsePrefix + ":" + uid
+	res, err := j.Rdb.Get(j.Ctx, key).Result()
+	if err != nil {
+		log.Errorf("JoinGroupResponse retrieval failed with %v", err)
+		return nil
+	}
+	var joinGroupResponse models.JoinGroupResponse
+	err = json.Unmarshal([]byte(res), &joinGroupResponse)
+	if err != nil {
+		log.Errorf("JoinGroupResponse decoding failed with %v", err)
+		return nil
+	}
+	return &joinGroupResponse
+}
---
+++ daemon/daos/models/joingroupresponse.go
@@ -0,0 +1,10 @@
+package models
+
+type JoinGroupResponse struct {
+	Uid    string `json:"uid"`
+	Username string `json:"username"`
+}
+
+func NewJoinGroupResponse(uid string, username string) *JoinGroupResponse {
+	return &JoinGroupResponse{uid, username}
+}
