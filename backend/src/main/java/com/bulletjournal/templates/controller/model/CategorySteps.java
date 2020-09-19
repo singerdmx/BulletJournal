4047054dacc308ccdaeaab9d3d2c7d750051fcc2
singerdmx <singerdmx@gmail.com>
2020-09-19 00:51:53 -0700

    Update CategorySteps class

--- backend/src/main/java/com/bulletjournal/templates/controller/model/CategorySteps.java
+++ backend/src/main/java/com/bulletjournal/templates/controller/model/CategorySteps.java
@@ -1,9 +1,12 @@
 package com.bulletjournal.templates.controller.model;
 
+import com.bulletjournal.templates.workflow.models.SelectionCombo;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class CategorySteps {
 
@@ -14,7 +17,9 @@ public class CategorySteps {
      */
     private List<Triple<Step, Rule, Step>> connections = new ArrayList<>();
 
-    private List<Step> finalSteps; // contains Map<SelectionCombo, List<SampleTask>>
+    private List<Pair<Step, Map<SelectionCombo, List<SampleTask>>>> finalSteps = new ArrayList<>();
+
+    private List<Long> stepIds = new ArrayList<>();
 
     public List<Triple<Step, Rule, Step>> getConnections() {
         return connections;
@@ -24,11 +29,19 @@ public class CategorySteps {
         this.connections = connections;
     }
 
-    public List<Step> getFinalSteps() {
+    public List<Pair<Step, Map<SelectionCombo, List<SampleTask>>>> getFinalSteps() {
         return finalSteps;
     }
 
-    public void setFinalSteps(List<Step> finalSteps) {
+    public void setFinalSteps(List<Pair<Step, Map<SelectionCombo, List<SampleTask>>>> finalSteps) {
         this.finalSteps = finalSteps;
+    }
+
+    public List<Long> getStepIds() {
+        return stepIds;
+    }
+
+    public void setStepIds(List<Long> stepIds) {
+        this.stepIds = stepIds;
     }
 }
