1d006906531715a8aab723c308365cb3f2acc491
singerdmx <singerdmx@gmail.com>
2020-09-22 14:40:14 -0700

    Add db/migration/V99__set_forum_id_for_job_hunting_category.sql

---
+++ backend/src/main/resources/db/migration/V99__set_forum_id_for_job_hunting_category.sql
@@ -0,0 +1,11 @@
+UPDATE template.categories
+SET forum_id = 111
+WHERE id = 14;
+
+UPDATE template.categories
+SET forum_id = 111
+WHERE id = 13;
+
+UPDATE template.categories
+SET forum_id = 111
+WHERE id = 10;
