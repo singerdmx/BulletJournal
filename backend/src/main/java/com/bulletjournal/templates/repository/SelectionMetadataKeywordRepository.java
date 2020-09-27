9a738e1bc7d94dad2487e9b26dd5fa0904069185
singerdmx <singerdmx@gmail.com>
2020-09-27 14:12:06 -0700

    Fix build

--- backend/src/main/java/com/bulletjournal/templates/repository/SelectionMetadataKeywordRepository.java
+++ backend/src/main/java/com/bulletjournal/templates/repository/SelectionMetadataKeywordRepository.java
@@ -1,2 +1,9 @@
-package com.bulletjournal.templates.repository;public class SelectionMetadataKeywordRepository {
+package com.bulletjournal.templates.repository;
+
+import com.bulletjournal.templates.repository.model.SelectionMetadataKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SelectionMetadataKeywordRepository extends JpaRepository<SelectionMetadataKeyword, String> {
 }
