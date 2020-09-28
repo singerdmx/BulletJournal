2e4f503818e132fc8968fbe2f73b915f70fbac63
singerdmx <singerdmx@gmail.com>
2020-09-27 19:20:05 -0700

    Add seeding for category sub

--- backend/src/main/resources/db/migration/V107__add_pending_to_sample_tasks_table.sql
+++ backend/src/main/resources/db/migration/V107__add_pending_to_sample_tasks_table.sql
@@ -1,2 +1,5 @@
 alter table template.sample_tasks
     add pending bool default false;
+
+INSERT INTO template.users_categories (user_id, category_id, selections, project_id, metadata_keyword)
+    VALUES (0, 13, '11,52,247,261,300,1000', 11, 'LEETCODE_ALGORITHM');
