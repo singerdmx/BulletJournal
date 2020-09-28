e2fc65c45660ef57bc80deafadfe052cad363c62
Michael Zhou <michael.zhouzzy@gmail.com>
2020-09-27 21:29:57 -0700

    Change file name to lower case

--- daemon/persistence/sampletask.go
+++ daemon/persistence/sampletask.go
@@ -10,7 +10,7 @@ type SampleTask struct {
 	Content				string 		`db:"content"`
 	Name				string		`db:"name"`
 	Uid					string		`db:"uid"`
-	AvailableBefore		string 	    `db:"available_before"`	// TODO
+	AvailableBefore		time.Time 	`db:"available_before"`
 	ReminderBeforeTask 	int 		`db:"reminder_before_task"`
 	DueDate				string		`db:"due_date"`
 	DueTime 			string		`db:"due_time"`
