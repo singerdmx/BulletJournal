bcdeb8554673feb79cf7efed4f8b1da4f4f65c88
singerdmx <singerdmx@gmail.com>
2020-09-27 19:41:12 -0700

    Update seeding for category sub

--- backend/src/main/resources/db/migration/V107__add_pending_to_sample_tasks_table.sql
+++ backend/src/main/resources/db/migration/V107__add_pending_to_sample_tasks_table.sql
@@ -3,3 +3,6 @@ alter table template.sample_tasks
 
 INSERT INTO template.users_categories (user_id, category_id, selections, project_id, metadata_keyword)
     VALUES (0, 13, '11,52,247,261,300,1000', 11, 'LEETCODE_ALGORITHM');
+
+INSERT INTO template.users_categories (user_id, category_id, selections, project_id, metadata_keyword)
+VALUES (0, 13, '11,56', 11, 'LEETCODE_DATABASE');
