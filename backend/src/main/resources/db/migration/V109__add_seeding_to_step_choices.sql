882d172ef821c7979a14fdbd8a007dfadfe4e150
singerdmx <singerdmx@gmail.com>
2020-09-29 16:43:22 -0700

    Fix add rule json serialization

--- backend/src/main/resources/db/migration/V109__add_seeding_to_step_choices.sql
+++ backend/src/main/resources/db/migration/V109__add_seeding_to_step_choices.sql
@@ -6,7 +6,7 @@ SET name = 'CS Data Structure'
 WHERE id = 10;
 
 INSERT INTO template.category_rules (id, name, created_at, updated_at, rule_expression, priority, category_id, connected_step_id)
-    VALUES (11, 'CS database rule', '2020-09-29 16:17:23.203000', '2020-09-29 16:17:23.203000', '[{"condition":"EXACT","selectionIds":"56"}]', 0, 13, 12);
+    VALUES (11, 'CS database rule', '2020-09-29 16:17:23.203000', '2020-09-29 16:17:23.203000', '{"rule":[{"condition":"CONTAINS","selectionIds":[56]}],"logicOperator":AND}', 0, 13, 12);
 
 UPDATE template.steps
 SET name = 'leetcode database final step'
