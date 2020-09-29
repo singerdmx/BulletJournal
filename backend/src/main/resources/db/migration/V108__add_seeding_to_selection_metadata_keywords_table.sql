cd61cda1582270b4ad2b9d1e89d7efe48bd1a5ad
singerdmx <singerdmx@gmail.com>
2020-09-28 22:02:18 -0700

    Add seeding for steps

--- backend/src/main/resources/db/migration/V108__add_seeding_to_selection_metadata_keywords_table.sql
+++ backend/src/main/resources/db/migration/V108__add_seeding_to_selection_metadata_keywords_table.sql
@@ -10,3 +10,21 @@ INSERT INTO template.selection_metadata_keywords (keyword, created_at, updated_a
            ('INVESTMENT_MATERIALS', '2020-09-28 21:44:36.000000', '2020-09-28 21:44:38.000000', 258),
            ('INVESTMENT_REAL_ESTATE', '2020-09-28 21:44:36.000000', '2020-09-28 21:44:38.000000', 259),
            ('INVESTMENT_UTILITIES', '2020-09-28 21:44:36.000000', '2020-09-28 21:44:38.000000', 260);
+
+INSERT INTO template.steps (id, created_at, updated_at, name, excluded_selections, next_step, choice_order)
+VALUES (13, '2020-09-28 21:57:23.000000', '2020-09-28 21:57:25.000000', 'leetcode algorithms final step', null, null,
+        null);
+
+INSERT INTO template.steps (id, created_at, updated_at, name, excluded_selections, next_step, choice_order)
+VALUES (12, '2020-09-28 21:56:14.000000', '2020-09-28 21:56:15.000000', 'leetcode database first step', null, 13, null);
+
+INSERT INTO template.steps (id, created_at, updated_at, name, excluded_selections, next_step, choice_order)
+VALUES (14, '2020-09-28 21:58:26.000000', '2020-09-28 21:58:28.000000', 'investment ipos final step', null, null, null);
+
+INSERT INTO template.steps (id, created_at, updated_at, name, excluded_selections, next_step, choice_order)
+VALUES (15, '2020-09-28 21:59:03.000000', '2020-09-28 21:59:05.000000', 'investment dividend final step', null, null,
+        null);
+
+INSERT INTO template.steps (id, created_at, updated_at, name, excluded_selections, next_step, choice_order)
+VALUES (16, '2020-09-28 21:59:23.000000', '2020-09-28 21:59:25.000000', 'investment earnings final step', null, null,
+        null);
