e09ff4fc22383e4f19bf5c49f8855d3268be847b
Michael Zhou <michael.zhouzzy@gmail.com>
2020-10-01 17:36:47 -0700

    Change fields of model to match original table

--- daemon/persistence/calendar.go
+++ daemon/persistence/calendar.go
@@ -4,12 +4,12 @@ import "time"
 
 //GoogleCalendarProject ...Map to table name google_calendar_projects
 type GoogleCalendarProject struct {
-	ID         int64      `db:"id"`
+	ID         string    `db:"id"`
 	CreatedAt  time.Time `db:"created_at"`
 	UpdatedAt  time.Time `db:"updated_at"`
 	Channel    string    `db:"channel"`
-	ChannelID  int64      `db:"channel_id"`
-	ProjectID  int64      `db:"project_id"`
+	ChannelID  string    `db:"channel_id"`
+	ProjectID  uint      `db:"project_id"`
 	Token      string    `db:"token"`
 	Owner      string    `db:"owner"`
 	Expiration time.Time `db:"expiration"`
