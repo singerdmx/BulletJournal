3449a01cf3078ec4b4f8b72c2066f203958f6200
Qing Wang <Gaaralmn@gmail.com>
2020-10-02 11:36:36 -0400

    Feature/dao (#37)
    * Update redis
    
    * Comment sample task test
    
    * Formated value type

--- daemon/persistence/redis_client.go
+++ daemon/persistence/redis_client.go
@@ -2,12 +2,17 @@ package persistence
 
 import (
 	"github.com/go-redis/redis/v8"
+	"github.com/singerdmx/BulletJournal/daemon/config"
 )
 
 type RedisClient struct {
-	Settings redis.Options
 }
 
-func (rc *RedisClient) RedisClient() *redis.Client {
-	return redis.NewClient(&rc.Settings)
+func GetRedisClient(serviceConfig *config.Config) *redis.Client {
+	settings := &redis.Options{
+		Addr:     serviceConfig.Host + ":" + serviceConfig.RedisPort,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
+	return redis.NewClient(settings)
 }
