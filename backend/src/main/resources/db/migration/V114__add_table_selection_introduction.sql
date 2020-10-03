282476f854c87d8cad3ec697bd5477773bb7f220
rico <>
2020-10-03 14:02:01 -0700

    selection introduction repository

--- backend/src/main/resources/db/migration/V114__add_table_selection_introduction.sql
+++ backend/src/main/resources/db/migration/V114__add_table_selection_introduction.sql
@@ -2,12 +2,11 @@ create table if not exists template.selection_introduction
 (
     id bigint not null,
 	selection_id bigint not null references template.selections,
-	image_link text,
-	description text,
-	title text,
+	image_link varchar(500),
+    description varchar(2000),
+    title varchar(500),
 	created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL
-
 );
 
 CREATE SEQUENCE if not exists "template".selection_introduction_sequence
