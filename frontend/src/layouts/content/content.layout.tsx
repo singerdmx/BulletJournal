0c97d646c9698478604054f84d057485a91528ff
singerdmx <singerdmx@gmail.com>
2020-10-05 11:58:11 -0700

    Rename SampleTaskPage from "../../pages/admin/sample-task.pages"

--- frontend/src/layouts/content/content.layout.tsx
+++ frontend/src/layouts/content/content.layout.tsx
@@ -25,8 +25,8 @@ import AdminWorkflowPage from "../../pages/admin/workflow.pages";
 import AdminCategoryPage from "../../pages/admin/category.pages";
 import AdminStepsPage from "../../pages/admin/steps.pages";
 import AdminStepPage from "../../pages/admin/step.pages";
-import SampleTaskPage from "../../pages/admin/sample-task.pages";
 import AdminMetadataPage from "../../pages/admin/admin-metadata.pages";
+import AdminSampleTaskPage from "../../pages/admin/sample-task.pages";
 
 const { Content } = Layout;
 class ContentLayout extends React.Component {
@@ -65,7 +65,7 @@ class ContentLayout extends React.Component {
           <Route exact path='/admin/categories/:categoryId' component={AdminCategoryPage} />
           <Route exact path='/admin/categories/:categoryId/steps' component={AdminStepsPage} />
           <Route exact path='/admin/steps/:stepId' component={AdminStepPage} />
-          <Route exact path='/admin/sampleTasks/:sampleTaskId' component={SampleTaskPage} />
+          <Route exact path='/admin/sampleTasks/:sampleTaskId' component={AdminSampleTaskPage} />
           <Route exact path='/admin/workflow' component={AdminWorkflowPage} />
           <Route exact path='/punchCard' component={PunchCardPage} />
           <Route exact path='/search/:term' component={SearchPage} />
