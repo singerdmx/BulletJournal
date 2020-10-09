d435f9e23324b60b6e486683312f71d18c77d5e0
singerdmx <singerdmx@gmail.com>
2020-10-09 01:06:18 -0700

    Change dae to api

--- frontend/src/apis/publicNotifications.ts
+++ frontend/src/apis/publicNotifications.ts
@@ -1,7 +1,7 @@
 import {doFetch} from './api-helper';
 
 export const answerPublicNotification = (id: string, action: string) => {
-    const endPoint = `/dae/public/notifications/${id}/answer?action=${action}`;
+    const endPoint = `/api/public/notifications/${id}/answer?action=${action}`;
     return doFetch(endPoint)
         .then(res => res)
         .catch(err => {
