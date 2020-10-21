c5384f687697f69cf44eb82de0b9b8b6db75b061
singerdmx <singerdmx@gmail.com>
2020-10-21 01:56:11 -0700

    Fix getStockTickerDetailContent

--- backend/src/main/java/com/bulletjournal/templates/repository/utils/InvestmentUtil.java
+++ backend/src/main/java/com/bulletjournal/templates/repository/utils/InvestmentUtil.java
@@ -66,54 +66,54 @@ public abstract class InvestmentUtil {
     }
 
     protected Pair<String, String> getStockTickerDetailContent(StockTickerDetails stockTickerDetails) {
-        if (stockTickerDetails != null && StringUtils.isBlank(stockTickerDetails.getDetails())) {
+        if (stockTickerDetails == null || StringUtils.isBlank(stockTickerDetails.getDetails())) {
             return Pair.of("", "");
         }
+
         String tickerDetailsDelta = "";
         String marketCapDelta = "";
         String marketCapHtml = "";
         String tickerDetailsHtml = "";
+
         if (stockTickerDetails.getMarketCap() != null) {
             marketCapDelta = ",{\"attributes\":{\"bold\":true},\"insert\":\"MarketCap\"},{\"insert\":\": " + stockTickerDetails.getMarketCap() + "\\n\"}";
             marketCapHtml = "<p><strong>MarketCap</strong>: " + stockTickerDetails.getMarketCap() + "</p>";
         }
-        if (stockTickerDetails != null) {
-            tickerDetailsDelta =
-                    ",{\"attributes\":{\"width\":\"72\"},\"insert\":{\"image\":\"" + stockTickerDetails.getLogo() + "\"}},{\"insert\":\"\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Country\"},{\"insert\":\": " + stockTickerDetails.getCountry() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Industry\"},{\"insert\":\": " + stockTickerDetails.getIndustry() + "\\n\"}" +
-                            "MARKET_CAP_DELTA" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Employees\"},{\"insert\":\": " + stockTickerDetails.getEmployees() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Phone\"},{\"insert\":\": " + stockTickerDetails.getPhone() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"CEO\"},{\"insert\":\": " + stockTickerDetails.getCeo() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"URL\"},{\"insert\":\": \"},{\"attributes\":{\"a\":\"" + stockTickerDetails.getUrl() + "\"},\"insert\":\"" + stockTickerDetails.getUrl() + "\"},{\"insert\":\"\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Description\"},{\"insert\":\": " + stockTickerDetails.getDescription() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Exchange\"},{\"insert\":\": " + stockTickerDetails.getExchange() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Name\"},{\"insert\":\": " + stockTickerDetails.getName() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"ExchangeSymbol\"},{\"insert\":\": " + stockTickerDetails.getExchangeSymbol() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"HQ Address\"},{\"insert\":\": " + stockTickerDetails.getHqAddress() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"HQ State\"},{\"insert\":\": " + stockTickerDetails.getHqState() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"HQ Country\"},{\"insert\":\": " + stockTickerDetails.getHqCountry() + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Tags\"},{\"insert\":\": " + String.join(", ", stockTickerDetails.getTags()) + "\\n\"}" +
-                            ",{\"attributes\":{\"bold\":true},\"insert\":\"Similar\"},{\"insert\":\": " + String.join(", ", stockTickerDetails.getSimilar()) + "\\n\"}";
-            tickerDetailsHtml = "<p><img src=\\\"" + stockTickerDetails.getLogo() + "\\\" width=\\\"72\\\"></p>" +
-                    "<p><strong>Country</strong>: " + stockTickerDetails.getCountry() + "</p>" +
-                    "<p><strong>Industry</strong>: " + stockTickerDetails.getIndustry() + "</p>" +
-                    "MARKET_CAP_HTML" +
-                    "<p><strong>Employees</strong>: " + stockTickerDetails.getEmployees() + "</p>" +
-                    "<p><strong>Phone</strong>: " + stockTickerDetails.getPhone() + "</p>" +
-                    "<p><strong>CEO</strong>: " + stockTickerDetails.getCeo() + "</p>" +
-                    "<p><strong>URL</strong>: <a href=\\\"" + stockTickerDetails.getUrl() + "\\\" rel=\\\"noopener noreferrer\\\" target=\\\"_blank\\\">" + stockTickerDetails.getUrl() + "</a></p>" +
-                    "<p><strong>Description</strong>: " + stockTickerDetails.getDescription() + "</p>" +
-                    "<p><strong>Exchange</strong>: " + stockTickerDetails.getExchange() + "</p>" +
-                    "<p><strong>Name</strong>: " + stockTickerDetails.getName() + "</p>" +
-                    "<p><strong>Exchange Symbol</strong>: " + stockTickerDetails.getExchangeSymbol() + "</p>" +
-                    "<p><strong>HQ Address</strong>: " + stockTickerDetails.getHqAddress() + "</p>" +
-                    "<p><strong>HQ State</strong>: " + stockTickerDetails.getHqState() + "</p>" +
-                    "<p><strong>HQ Country</strong>: " + stockTickerDetails.getHqCountry() + "</p>" +
-                    "<p><strong>Tags</strong>: " + String.join(", ", stockTickerDetails.getTags()) + "</p>" +
-                    "<p><strong>Similar</strong>: " + String.join(", ", stockTickerDetails.getSimilar()) + "</p>";
-        }
+        tickerDetailsDelta =
+                ",{\"attributes\":{\"width\":\"72\"},\"insert\":{\"image\":\"" + stockTickerDetails.getLogo() + "\"}},{\"insert\":\"\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Country\"},{\"insert\":\": " + stockTickerDetails.getCountry() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Industry\"},{\"insert\":\": " + stockTickerDetails.getIndustry() + "\\n\"}" +
+                        "MARKET_CAP_DELTA" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Employees\"},{\"insert\":\": " + stockTickerDetails.getEmployees() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Phone\"},{\"insert\":\": " + stockTickerDetails.getPhone() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"CEO\"},{\"insert\":\": " + stockTickerDetails.getCeo() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"URL\"},{\"insert\":\": \"},{\"attributes\":{\"a\":\"" + stockTickerDetails.getUrl() + "\"},\"insert\":\"" + stockTickerDetails.getUrl() + "\"},{\"insert\":\"\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Description\"},{\"insert\":\": " + stockTickerDetails.getDescription() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Exchange\"},{\"insert\":\": " + stockTickerDetails.getExchange() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Name\"},{\"insert\":\": " + stockTickerDetails.getName() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"ExchangeSymbol\"},{\"insert\":\": " + stockTickerDetails.getExchangeSymbol() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"HQ Address\"},{\"insert\":\": " + stockTickerDetails.getHqAddress() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"HQ State\"},{\"insert\":\": " + stockTickerDetails.getHqState() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"HQ Country\"},{\"insert\":\": " + stockTickerDetails.getHqCountry() + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Tags\"},{\"insert\":\": " + String.join(", ", stockTickerDetails.getTags()) + "\\n\"}" +
+                        ",{\"attributes\":{\"bold\":true},\"insert\":\"Similar\"},{\"insert\":\": " + String.join(", ", stockTickerDetails.getSimilar()) + "\\n\"}";
+        tickerDetailsHtml = "<p><img src=\\\"" + stockTickerDetails.getLogo() + "\\\" width=\\\"72\\\"></p>" +
+                "<p><strong>Country</strong>: " + stockTickerDetails.getCountry() + "</p>" +
+                "<p><strong>Industry</strong>: " + stockTickerDetails.getIndustry() + "</p>" +
+                "MARKET_CAP_HTML" +
+                "<p><strong>Employees</strong>: " + stockTickerDetails.getEmployees() + "</p>" +
+                "<p><strong>Phone</strong>: " + stockTickerDetails.getPhone() + "</p>" +
+                "<p><strong>CEO</strong>: " + stockTickerDetails.getCeo() + "</p>" +
+                "<p><strong>URL</strong>: <a href=\\\"" + stockTickerDetails.getUrl() + "\\\" rel=\\\"noopener noreferrer\\\" target=\\\"_blank\\\">" + stockTickerDetails.getUrl() + "</a></p>" +
+                "<p><strong>Description</strong>: " + stockTickerDetails.getDescription() + "</p>" +
+                "<p><strong>Exchange</strong>: " + stockTickerDetails.getExchange() + "</p>" +
+                "<p><strong>Name</strong>: " + stockTickerDetails.getName() + "</p>" +
+                "<p><strong>Exchange Symbol</strong>: " + stockTickerDetails.getExchangeSymbol() + "</p>" +
+                "<p><strong>HQ Address</strong>: " + stockTickerDetails.getHqAddress() + "</p>" +
+                "<p><strong>HQ State</strong>: " + stockTickerDetails.getHqState() + "</p>" +
+                "<p><strong>HQ Country</strong>: " + stockTickerDetails.getHqCountry() + "</p>" +
+                "<p><strong>Tags</strong>: " + String.join(", ", stockTickerDetails.getTags()) + "</p>" +
+                "<p><strong>Similar</strong>: " + String.join(", ", stockTickerDetails.getSimilar()) + "</p>";
         return Pair.of(tickerDetailsDelta.replace("MARKET_CAP_DELTA", marketCapDelta), tickerDetailsHtml.replace("MARKET_CAP_HTML", marketCapHtml));
     }
 }
