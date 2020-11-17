ca91ac3e1c3d220ac69e0a449a0afb45eacab48c
RandallDW <dong0125@vt.edu>
2020-11-17 18:11:19 -0500

    update TTL

--- backend/src/main/java/com/bulletjournal/redis/models/CachedContent.java
+++ backend/src/main/java/com/bulletjournal/redis/models/CachedContent.java
@@ -5,9 +5,8 @@ import java.io.Serializable;
 import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.TimeToLive;
 
-@RedisHash(value = "CachedContent", timeToLive = 3)
+@RedisHash(value = "CachedContent", timeToLive = 2)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CachedContent implements Serializable {
 
@@ -31,8 +30,12 @@ public class CachedContent implements Serializable {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
     CachedContent cachedContent = (CachedContent) obj;
     return Objects.equals(contentId, cachedContent.contentId);
   }
