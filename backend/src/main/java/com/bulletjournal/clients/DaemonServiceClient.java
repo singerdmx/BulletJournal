e3f4b938f5b0f6df1847b0e6d09a724395e72b1c
singerdmx <singerdmx@gmail.com>
2020-12-06 18:51:34 -0800

    Disable DaemonServiceClient

--- backend/src/main/java/com/bulletjournal/clients/DaemonServiceClient.java
+++ backend/src/main/java/com/bulletjournal/clients/DaemonServiceClient.java
@@ -90,7 +90,8 @@ public class DaemonServiceClient {
                         case SAMPLETASKMSG:
                             SubscribeSampleTaskMsg msg = streamMsg.getSampleTaskMsg();
                             LOGGER.info("Received SubscribeInvestmentSampleTaskMsg with sampleTaskId: {}", msg.getSampleTaskId());
-                            DaemonServiceClient.this.notificationService.addSampleTaskChange(new SampleTaskChange(msg.getSampleTaskId()));
+                            // TODO: ADD IT BACK
+                            // DaemonServiceClient.this.notificationService.addSampleTaskChange(new SampleTaskChange(msg.getSampleTaskId()));
                             break;
                         default:
                             LOGGER.warn("Unsupported NotificationStreamMsg body");
