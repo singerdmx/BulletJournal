8b559a9c6475ac78c2fb96170b0aca5b9d624ac1
singerdmx <singerdmx@gmail.com>
2020-12-06 21:23:48 -0800

    Revert "Disable DaemonServiceClient"
    This reverts commit e3f4b938f5b0f6df1847b0e6d09a724395e72b1c.

--- backend/src/main/java/com/bulletjournal/clients/DaemonServiceClient.java
+++ backend/src/main/java/com/bulletjournal/clients/DaemonServiceClient.java
@@ -90,8 +90,7 @@ public class DaemonServiceClient {
                         case SAMPLETASKMSG:
                             SubscribeSampleTaskMsg msg = streamMsg.getSampleTaskMsg();
                             LOGGER.info("Received SubscribeInvestmentSampleTaskMsg with sampleTaskId: {}", msg.getSampleTaskId());
-                            // TODO: ADD IT BACK
-                            // DaemonServiceClient.this.notificationService.addSampleTaskChange(new SampleTaskChange(msg.getSampleTaskId()));
+                            DaemonServiceClient.this.notificationService.addSampleTaskChange(new SampleTaskChange(msg.getSampleTaskId()));
                             break;
                         default:
                             LOGGER.warn("Unsupported NotificationStreamMsg body");
