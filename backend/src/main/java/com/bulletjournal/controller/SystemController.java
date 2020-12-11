6a6e04db6927fe05f253326fcf8da219f7324ff6
singerdmx <singerdmx@gmail.com>
2020-12-11 15:03:31 -0800

    Temporarily enable system update API

--- backend/src/main/java/com/bulletjournal/controller/SystemController.java
+++ backend/src/main/java/com/bulletjournal/controller/SystemController.java
@@ -6,7 +6,6 @@ import com.bulletjournal.contents.ContentType;
 import com.bulletjournal.controller.models.*;
 import com.bulletjournal.controller.utils.EtagGenerator;
 import com.bulletjournal.daemon.Reminder;
-import com.bulletjournal.daemon.models.ReminderRecord;
 import com.bulletjournal.exceptions.BadRequestException;
 import com.bulletjournal.exceptions.UnAuthorizedException;
 import com.bulletjournal.redis.RedisEtagDaoJpa;
@@ -30,9 +29,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.time.ZonedDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpHeaders.IF_NONE_MATCH;
 
@@ -165,19 +162,19 @@ public class SystemController {
         }
 
         if (targetEtags == null || targetEtags.contains("taskReminders")) {
-            final ZonedDateTime startTime = ZonedDateTime.now().minusHours(2);
-            final ZonedDateTime endTime = ZonedDateTime.now().plusMinutes(2);
-            List<ReminderRecord> reminderRecords = this.reminder.getTasksAssignedThatNeedsWebPopupReminder(
-                    username, startTime, endTime);
-            remindingTasks = this.labelDaoJpa.getLabelsForProjectItemList(
-                    this.reminder.getRemindingTasks(reminderRecords, startTime)
-                            .stream().map(t -> t.toPresentationModel()).collect(Collectors.toList()));
+//            final ZonedDateTime startTime = ZonedDateTime.now().minusHours(2);
+//            final ZonedDateTime endTime = ZonedDateTime.now().plusMinutes(2);
+//            List<ReminderRecord> reminderRecords = this.reminder.getTasksAssignedThatNeedsWebPopupReminder(
+//                    username, startTime, endTime);
+//            remindingTasks = this.labelDaoJpa.getLabelsForProjectItemList(
+//                    this.reminder.getRemindingTasks(reminderRecords, startTime)
+//                            .stream().map(t -> t.toPresentationModel()).collect(Collectors.toList()));
             remindingTaskEtag = EtagGenerator.generateEtag(EtagGenerator.HashAlgorithm.MD5,
                     EtagGenerator.HashType.TO_HASHCODE,
-                    remindingTasks);
-            if (remindingTaskRequestEtag.isPresent() && remindingTaskEtag.equals(remindingTaskRequestEtag.get())) {
-                remindingTasks = null;
-            }
+                    Collections.emptyList());
+//            if (remindingTaskRequestEtag.isPresent() && remindingTaskEtag.equals(remindingTaskRequestEtag.get())) {
+//                remindingTasks = null;
+//            }
         }
 
         if (cachingEtags.size() > 0) {
