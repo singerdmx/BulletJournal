f9b5626379d3f53b031d0fbdc21de93799baa1cb
KetaoChen <chenketao007@gmail.com>
2021-01-07 23:00:44 -0800

    set color in task

--- frontend/src/components/modals/project-setting.component.tsx
+++ frontend/src/components/modals/project-setting.component.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import CSS from 'csstype';
 import {
   Checkbox,
   Modal,
-  Tooltip,
 } from 'antd';
+import { CompactPicker, RGBColor } from 'react-color';
 import { IState } from '../../store';
 import { connect } from 'react-redux';
 import './modals.styles.less';
@@ -24,6 +25,51 @@ const ProjectSetting: React.FC<ProjectSettingProps> = (props) => {
 
   
   const onCheck = (e: any) => console.log(e.target.checked);
+  const [displayColorPicker, setDisplayColorPicker] = useState(false);
+
+  const handleClickSetColor = () => {
+      setDisplayColorPicker(!displayColorPicker);
+  };
+
+  const handleChange = (c : any , event : any) => {
+    setBgColor(c.rgb);
+  };
+
+  const [bgColor, setBgColor] = useState({
+    r: '255',
+    g: '255',
+    b: '255',
+    a: '100',
+  })
+
+  const swatch : CSS.Properties = {
+    padding: '5px',
+    borderRadius: '1px',
+    boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+    display: 'inline-block',
+    cursor: 'pointer',
+    background: `rgba(${ bgColor.r }, ${ bgColor.g }, ${ bgColor.b }, ${ bgColor.a })`,
+  }
+
+  const popover : CSS.Properties = {
+    position: 'absolute', 
+    zIndex: 2
+  }
+
+  const cover : CSS.Properties = {
+    position: 'fixed', 
+    top: '0px', 
+    right: '0px', 
+    bottom: '0px', 
+    left: '0px'
+  }
+
+  const color : RGBColor = {
+    r: Number(bgColor.r),
+    g: Number(bgColor.g),
+    b: Number(bgColor.b),
+    a: Number(bgColor.a),
+  }
 
   return (
     <Modal
@@ -33,13 +79,23 @@ const ProjectSetting: React.FC<ProjectSettingProps> = (props) => {
       footer={false}
     >
       <div>
+          
         <Checkbox
             style={{ marginRight: '0.5rem', marginTop: '-0.5em' }}
             onChange={onCheck}
         >
             Auto Delete
         </Checkbox>
-          
+
+        <div>
+            <div style={ swatch } onClick={ handleClickSetColor }> select color 
+            </div> 
+            { displayColorPicker ? <div style={ popover }>
+            <div style={ cover } onClick={ handleClickSetColor }/>
+            <CompactPicker color={color}  onChange={ handleChange } />
+            </div> : null }
+
+        </div>
       </div>
     </Modal>
   );
