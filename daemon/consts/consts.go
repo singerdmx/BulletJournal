6418996f02a7677e1dc09b5faaec90550f8d6b0f
Xiaozheng(Will) Guo <xguo.tufts@gmail.com>
2021-01-10 10:23:25 -0800

    rename service name "investment" to "sampleTask"

--- daemon/consts/consts.go
+++ daemon/consts/consts.go
@@ -3,6 +3,6 @@ package consts
 // Define all shared constants here
 const (
 	CLEANER_SERVICE_NAME    string = "cleaner"
-	INVESTMENT_SERVICE_NAME string = "investment"
+	INVESTMENT_SERVICE_NAME string = "sampleTask"
 	REMINDER_SERVICE_NAME   string = "reminder"
 )
