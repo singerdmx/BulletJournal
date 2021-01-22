7b4e3ae7f3fb5de0922e2ceb5f07a0f0d08a642a
Michael Zhou <michael.zhouzzy@gmail.com>
2021-01-21 18:14:07 -0800

    Add null check in transaction query

--- backend/src/main/java/com/bulletjournal/repository/TransactionRepository.java
+++ backend/src/main/java/com/bulletjournal/repository/TransactionRepository.java
@@ -16,21 +16,21 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long>,
     List<Long> findUniqueLabelsByProject(@Param("project_id") Long projectId);
 
     @Query("SELECT transaction FROM Transaction transaction where transaction.project = :project AND "
-            + "((transaction.startTime >= :startTime AND transaction.startTime <= :endTime) OR "
-            + "(transaction.endTime >= :startTime AND transaction.endTime <= :endTime))")
+            + "((transaction.startTime IS NOT NULL AND transaction.startTime >= :startTime AND transaction.startTime <= :endTime) OR "
+            + "(transaction.endTime IS NOT NULL AND transaction.endTime >= :startTime AND transaction.endTime <= :endTime))")
     List<Transaction> findTransactionsByProjectBetween(@Param("project") Project project,
                                                        @Param("startTime") Timestamp startTime, @Param("endTime") Timestamp endTime);
 
     @Query("SELECT transaction FROM Transaction transaction where transaction.project IN :projects AND transaction.payer = :payer AND "
-            + "((transaction.startTime >= :startTime AND transaction.startTime <= :endTime) OR "
-            + "(transaction.endTime >= :startTime AND transaction.endTime <= :endTime))")
+            + "((transaction.startTime IS NOT NULL AND transaction.startTime >= :startTime AND transaction.startTime <= :endTime) OR "
+            + "(transaction.endTime IS NOT NULL AND transaction.endTime >= :startTime AND transaction.endTime <= :endTime))")
     List<Transaction> findTransactionsOfPayerBetween(@Param("payer") String payer,
                                                      @Param("startTime") Timestamp startTime, @Param("endTime") Timestamp endTime,
                                                      @Param("projects") List<Project> projects);
 
     @Query("SELECT transaction FROM Transaction transaction where transaction.project = :project AND transaction.payer = :payer AND "
-            + "((transaction.startTime >= :startTime AND transaction.startTime <= :endTime) OR "
-            + "(transaction.endTime >= :startTime AND transaction.endTime <= :endTime))")
+            + "((transaction.startTime IS NOT NULL AND transaction.startTime >= :startTime AND transaction.startTime <= :endTime) OR "
+            + "(transaction.endTime IS NOT NULL AND transaction.endTime >= :startTime AND transaction.endTime <= :endTime))")
     List<Transaction> findTransactionsInProjectByPayerBetween(@Param("payer") String payer,
                                                               @Param("project") Project project, @Param("startTime") Timestamp startTime,
                                                               @Param("endTime") Timestamp endTime);
