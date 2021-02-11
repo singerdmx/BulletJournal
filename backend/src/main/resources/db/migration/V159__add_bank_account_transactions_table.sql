e45635da17d2432e5cbdbec52d6a36afbd68304e
singerdmx <singerdmx@gmail.com>
2021-02-11 00:40:03 -0800

    Add bank_account_transactions table

---
+++ backend/src/main/resources/db/migration/V159__add_bank_account_transactions_table.sql
@@ -0,0 +1,21 @@
+CREATE SEQUENCE public.bank_account_transactions_sequence
+    START WITH 100
+    INCREMENT BY 2
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER TABLE public.bank_account_transactions_sequence OWNER TO postgres;
+
+CREATE TABLE public.bank_account_transactions (
+    id bigint PRIMARY KEY,
+    amount double precision not null,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    bank_account bigint not null
+        constraint bank_account_transactions__bank_accounts_fk
+            references bank_accounts
+            on delete cascade
+);
+
+ALTER TABLE public.bank_account_transactions OWNER TO postgres;
