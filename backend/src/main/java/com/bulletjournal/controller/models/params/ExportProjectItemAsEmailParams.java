9a879d7c72f5937b63a996ed5be7eeeda6902905
RandallDW <dong0125@vt.edu>
2021-02-16 15:30:01 -0500

    remove function

--- backend/src/main/java/com/bulletjournal/controller/models/params/ExportProjectItemAsEmailParams.java
+++ backend/src/main/java/com/bulletjournal/controller/models/params/ExportProjectItemAsEmailParams.java
@@ -52,39 +52,4 @@ public class ExportProjectItemAsEmailParams {
   public void setContents(List<Content> contents) {
     this.contents = contents;
   }
-
-  public String generateExportHtml() {
-    StringBuilder result = new StringBuilder();
-    for (Content content : this.contents) {
-      User owner = content.getOwner();
-      String username = owner.getName();
-      String avatar = owner.getAvatar();
-      String createAt = content.getCreatedAt().toString();
-      String updateAt = content.getUpdatedAt().toString();
-      String htmlText = content.getText();
-
-      String contentInfo =
-          "<div>"
-              + "<div>"
-              + "<div style=\"float: left\">\n"
-              + "   <img src=" + avatar
-              + " alt=\"owner_avatar\" style=\"border-radius: 50%;width:20px;height:20px;\">\n"
-              + "</div>"
-              + "<div style=\"float: left\">\n"
-              + "   <span style=\"margin-left: 5px; color: #e8aed0;font-size: 17px; margin-right: 8px\">"
-              + username + "</span>\n"
-              + "</div>"
-              + "<div style=\"float: left\">\n"
-              + "   <span style=\"margin-left: 5px\"> Created at: " + createAt + "</span>\n"
-              + "</div>"
-              + "<div style=\"float: left\">\n"
-              + "   <span style=\"margin-left: 5px\"> Updated at: " + updateAt + "</span>\n"
-              + "</div>"
-              + "<br><div>" + htmlText + "</div>"
-              + "</div>";
-
-      result.append(contentInfo);
-    }
-    return "<div>" + result.toString() + "</div>";
-  }
 }
