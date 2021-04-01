0a0d280ca513dcfcb8f27ba11a0a030f69d4c98b
Dong.Wang <randalldw0125@gmail.com>
2021-04-01 14:05:21 -0400

    clean up

--- backend/src/main/java/com/bulletjournal/controller/models/params/ExportProjectItemParams.java
+++ backend/src/main/java/com/bulletjournal/controller/models/params/ExportProjectItemParams.java
@@ -7,19 +7,9 @@ import java.util.List;
 
 public class ExportProjectItemParams {
 
-    private ExportType exportType;
-
     private List<Content> contents;
 
     public ExportProjectItemParams() {
-    }
-
-    public ExportType getExportType() {
-        return exportType;
-    }
-
-    public void setExportType(ExportType exportType) {
-        this.exportType = exportType;
     }
 
     public List<Content> getContents() {
