26ec7b531e526bff1a93c95ab432432563cf14e8
Xin Yao <singerdmx@gmail.com>
2021-04-17 09:57:18 -0700

    Add links for urls in delta

--- backend/src/main/java/com/bulletjournal/util/DeltaContent.java
+++ backend/src/main/java/com/bulletjournal/util/DeltaContent.java
@@ -6,16 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DeltaContent {
 
     public static final String DELTA = "delta";
     public static final String HTML_TAG = "###html###";
     public static final String EMPTY_CONTENT = "{\"delta\":{\"ops\":[{\"insert\":\" \\n\"}]}}";
+    private static final Pattern URL_REGEX = Pattern.compile(
+            "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)",
+            Pattern.CASE_INSENSITIVE);
     private static final Gson GSON = new GsonBuilder().
             registerTypeAdapter(Double.class, new JsonSerializer<Double>() {
                 @Override
@@ -49,11 +51,41 @@ public class DeltaContent {
         List<Map<String, Object>> deltaList = new ArrayList<>();
         List<LinkedHashMap> opsList = (ArrayList) (deltaMap.get("ops"));
         for (Map<String, Object> innerDeltaMap : opsList) {
-            if (innerDeltaMap.containsKey("insert") && (innerDeltaMap.get("insert")) instanceof Map) {
-                Map insertMap = (Map) innerDeltaMap.get("insert");
-                if (insertMap.containsKey("emoji")) {
-                    deltaList.add(DeltaConverter.WebToMobile.webToMobileEmoji(insertMap));
-                    continue;
+            if (innerDeltaMap.containsKey("insert")) {
+                Object insertContent = innerDeltaMap.get("insert");
+                if (insertContent instanceof Map) {
+                    Map insertMap = (Map) insertContent;
+                    if (insertMap.containsKey("emoji")) {
+                        deltaList.add(DeltaConverter.WebToMobile.webToMobileEmoji(insertMap));
+                        continue;
+                    }
+                } else if (insertContent instanceof String) {
+                    String insertString = (String) insertContent;
+                    Object attributes = innerDeltaMap.get("attributes");
+                    Matcher matcher = URL_REGEX.matcher(insertString);
+                    Map attributesMap = new LinkedHashMap();
+                    if (attributes != null && attributes instanceof Map) {
+                        attributesMap = (Map) attributes;
+                    }
+                    if (matcher.find() && !attributesMap.containsKey("link")) {
+                        int beg = 0;
+                        do {
+                            int urlStart = matcher.start(0);
+                            int urlEnd = matcher.end(0);
+                            if (beg < urlStart) {
+                                addToList(deltaList, innerDeltaMap, null,
+                                        attributesMap, insertString.substring(beg, urlStart));
+                            }
+                            String link = insertString.substring(urlStart, urlEnd);
+                            addToList(deltaList, innerDeltaMap, link, attributesMap, link);
+                            beg = urlEnd;
+                        } while (matcher.find());
+                        if (beg < insertString.length()) {
+                            addToList(deltaList, innerDeltaMap, null,
+                                    attributesMap, insertString.substring(beg));
+                        }
+                        continue;
+                    }
                 }
             }
             deltaList.add(innerDeltaMap);
@@ -62,6 +94,26 @@ public class DeltaContent {
         LinkedHashMap opsMap = new LinkedHashMap();
         opsMap.put("ops", deltaList);
         return opsMap;
+    }
+
+    private void addToList(
+            List<Map<String, Object>> deltaList,
+            Map<String, Object> innerDeltaMap,
+            String link,
+            Map attributesMap,
+            String substring) {
+        if (link == null) {
+            attributesMap.remove("link");
+        } else {
+            attributesMap.put("link", link);
+        }
+        if (attributesMap.isEmpty()) {
+            innerDeltaMap.remove("attributes");
+        } else {
+            innerDeltaMap.put("attributes", new LinkedHashMap<>(attributesMap));
+        }
+        innerDeltaMap.put("insert", substring);
+        deltaList.add(new LinkedHashMap<>(innerDeltaMap));
     }
 
     public String getHtml() {
