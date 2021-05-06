21f5f5b366507dafd6d320076f2d8c42e2c8ddf5
Dong.Wang <randalldw0125@gmail.com>
2021-05-06 06:52:06 -0400

    Apply time range over note auditables pagination

--- backend/src/main/java/com/bulletjournal/repository/NoteAuditableRepository.java
+++ backend/src/main/java/com/bulletjournal/repository/NoteAuditableRepository.java
@@ -6,7 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+
 @Repository
 public interface NoteAuditableRepository extends PagingAndSortingRepository<NoteAuditable, Long> {
     Page<NoteAuditable> findAllByNoteId(Long noteId, Pageable pageable);
+
+    Page<NoteAuditable> findAllByNoteIdAndActivityTimeBetween(
+            Long noteId, Timestamp activityTimeStart, Timestamp activityTimeEnd, Pageable pageable);
 }
