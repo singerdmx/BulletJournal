10bf6b55c972c8e7fd0f19b7b3d39b5e6a59b043
RandallDW <randalldw0125@gmail.com>
2021-05-06 20:10:59 -0400

    Trans Auditables - Apply time range over pagination (#90)

--- backend/src/main/java/com/bulletjournal/repository/TransactionAuditableDaoJpa.java
+++ backend/src/main/java/com/bulletjournal/repository/TransactionAuditableDaoJpa.java
@@ -1,5 +1,6 @@
 package com.bulletjournal.repository;
 
+import com.bulletjournal.controller.utils.ZonedDateTimeHelper;
 import com.bulletjournal.notifications.ProjectItemAuditable;
 import com.bulletjournal.repository.models.Transaction;
 import com.bulletjournal.repository.models.TransactionAuditable;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -27,7 +30,19 @@ public class TransactionAuditableDaoJpa {
   }
 
   @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
-  public Page<TransactionAuditable> getHistory(Transaction transaction, int pageInd, int pageSize) {
+  public Page<TransactionAuditable> getHistory(
+      Transaction transaction, int pageInd, int pageSize, String startDate, String endDate, String timezone) {
+
+    if (startDate != null && endDate != null && timezone != null) {
+      ZonedDateTime startTime = ZonedDateTimeHelper.getStartTime(startDate, null, timezone);
+      ZonedDateTime endTime = ZonedDateTimeHelper.getEndTime(endDate, null, timezone);
+      return this.transactionAuditableRepository.findAllByTransactionAndActivityTimeBetween(
+          transaction,
+          Timestamp.from(startTime.toInstant()),
+          Timestamp.from(endTime.toInstant()),
+          PageRequest.of(pageInd, pageSize, Sort.by("activityTime").descending()));
+    }
+
     return this.transactionAuditableRepository.findAllByTransaction(
         transaction, PageRequest.of(pageInd, pageSize, Sort.by("activityTime").descending()));
   }
--- backend/src/main/java/com/bulletjournal/repository/TransactionAuditableRepository.java
+++ backend/src/main/java/com/bulletjournal/repository/TransactionAuditableRepository.java
@@ -7,8 +7,16 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+
 @Repository
 public interface TransactionAuditableRepository
     extends PagingAndSortingRepository<TransactionAuditable, Long> {
   Page<TransactionAuditable> findAllByTransaction(Transaction transaction, Pageable pageable);
+
+  Page<TransactionAuditable> findAllByTransactionAndActivityTimeBetween(
+      Transaction transaction,
+      Timestamp activityTimeStart,
+      Timestamp activityTimeEnd,
+      Pageable pageable);
 }
