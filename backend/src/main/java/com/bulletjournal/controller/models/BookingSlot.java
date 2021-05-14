4855dfa7a6c65924c2ae6bb947d9e2c3d71f3701
pengw00 <water685@hotmail.com>
2021-05-14 01:08:42 -0500

    update bookinglink model

--- backend/src/main/java/com/bulletjournal/controller/models/BookingSlot.java
+++ backend/src/main/java/com/bulletjournal/controller/models/BookingSlot.java
@@ -1,10 +1,13 @@
 package com.bulletjournal.controller.models;
 
+import java.util.Objects;
+
 public class BookingSlot {
     private boolean isOn;
     private int index;
     private String startTime;
     private String endTime;
+    private String date;
 
 
     public boolean isOn() {
@@ -39,4 +42,28 @@ public class BookingSlot {
         this.endTime = endTime;
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookingSlot bookingSlot = (BookingSlot) o;
+        return isOn == bookingSlot.isOn &&
+                index == bookingSlot.index &&
+                Objects.equals(startTime, bookingSlot.startTime) &&
+                Objects.equals(endTime, bookingSlot.endTime) &&
+                Objects.equals(date, bookingSlot.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isOn(), getIndex(), getStartTime(), getEndTime(), getDate());
+    }
 }
