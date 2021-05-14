9fd9c60c45bab9cc0a761cd8055f8bd76ad5317d
li3317 <li3317@purdue.edu>
2021-05-13 21:10:42 -0700

    booklink add create in dao

--- backend/src/main/java/com/bulletjournal/repository/BookingLinkDaoJpa.java
+++ backend/src/main/java/com/bulletjournal/repository/BookingLinkDaoJpa.java
@@ -1,13 +1,12 @@
 package com.bulletjournal.repository;
 
+import com.bulletjournal.controller.models.params.CreateBookingLinkParams;
+import com.bulletjournal.exceptions.ResourceNotFoundException;
 import com.bulletjournal.repository.models.BookingLink;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
-
 
 @Repository
 public class BookingLinkDaoJpa {
@@ -16,9 +15,26 @@ public class BookingLinkDaoJpa {
 
     @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
     public BookingLink getBookingLink(String id) {
-        Optional<BookingLink> bookingLinkOptional = this.bookingLinkRepository.findById(id);
-        return bookingLinkOptional.orElseGet(BookingLink::new);
+        return this.bookingLinkRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Booking link " + id + " not found"));
     }
 
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
+    public BookingLink create(String id, String owner, CreateBookingLinkParams createBookingLinkParams) {
+        BookingLink bookingLink = new BookingLink();
+        bookingLink.setId(id);
+        bookingLink.setOwner(owner);
+        bookingLink.setBufferInMin(createBookingLinkParams.getBufferInMin());
+        bookingLink.setStartDate(createBookingLinkParams.getStartDate());
+        bookingLink.setEndDate(createBookingLinkParams.getEndDate());
+        bookingLink.setExpireOnBooking(createBookingLinkParams.getExpireOnBooking());
+        bookingLink.setIncludeTaskWithoutDuration(createBookingLinkParams.getIncludeTaskWithoutDuration());
+        bookingLink.setSlotSpan(createBookingLinkParams.getSlotSpan());
+        bookingLink.setTimezone(createBookingLinkParams.getTimezone());
+        // slots
+
+        this.bookingLinkRepository.save(bookingLink);
+        return bookingLink;
+    }
 
 }
