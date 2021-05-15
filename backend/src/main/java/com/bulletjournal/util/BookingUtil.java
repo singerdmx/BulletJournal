50bf90a2a6f436255ac625f26d73e7f6ac20a289
pengw00 <water685@hotmail.com>
2021-05-15 11:25:09 -0500

    update bookingslots override match

--- backend/src/main/java/com/bulletjournal/util/BookingUtil.java
+++ backend/src/main/java/com/bulletjournal/util/BookingUtil.java
@@ -1,43 +1,46 @@
 package com.bulletjournal.util;
 
-import com.bulletjournal.controller.BookingLinksController;
 import com.bulletjournal.controller.models.BookingSlot;
 import com.bulletjournal.controller.utils.ZonedDateTimeHelper;
 import com.bulletjournal.repository.models.BookingLink;
 import com.google.gson.Gson;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.*;
 
 public class BookingUtil {
     private static final Gson GSON = new Gson();
-    private static final Logger LOGGER = LoggerFactory.getLogger(BookingLinksController.class);
 
     public static List<BookingSlot> calculateSlots(
             List<BookingSlot> slotsOverride, String startDate, String endDate, int slotSpan) {
         List<BookingSlot> slots = new ArrayList<>();
         ZonedDateTime startTime = ZonedDateTimeHelper.getStartTime(startDate, null, "America/Chicago");
-        ZonedDateTime endTime = ZonedDateTimeHelper.getStartTime(endDate, null, "America/Chicago");
-        endTime.plusDays(1);
+        ZonedDateTime endTime = ZonedDateTimeHelper.getEndTime(endDate, null, "America/Chicago");
+        endTime.plusMinutes(1);
 
-        if(slotSpan <= 0) return slots;
+        if (slotSpan <= 0) return slots;
 
-        int durationMinutes = ZonedDateTimeHelper.getDuration(startTime, endTime);
+        int durationMinutes = (int) Duration.between(startTime, endTime).toMinutes();
 
         int slotsNumber = durationMinutes / slotSpan;
 
         int totalIndexes = (24 * 60) / slotSpan;
 
-        for(int i = 0; i < slotsNumber; i++){
+        for (int i = 0; i < slotsNumber; i++) {
             BookingSlot bookingSlot = new BookingSlot();
             bookingSlot.setIndex(i % totalIndexes);
             bookingSlot.setDate(ZonedDateTimeHelper.getDate(startTime.plusMinutes(i * slotSpan)));
             bookingSlot.setStartTime(ZonedDateTimeHelper.getTime(startTime.plusMinutes(i * slotSpan)));
             bookingSlot.setEndTime(ZonedDateTimeHelper.getTime(startTime.plusMinutes((i + 1) * slotSpan)));
-            slots.add(bookingSlot);
+
+            Optional<BookingSlot> match = slotsOverride.stream().filter(s -> Objects.equals(s, bookingSlot)).findFirst();
+            if (match.isPresent()) {
+                slots.add(match.get());
+            } else {
+                slots.add(bookingSlot);
+            }
         }
 
         return slots;
