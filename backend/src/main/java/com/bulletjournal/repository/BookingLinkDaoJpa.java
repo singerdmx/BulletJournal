e7b123d6f3a5fbd2a7d138e112328b7947392318
li3317 <li3317@purdue.edu>
2021-05-14 18:19:07 -0700

    booking link add update dao

--- backend/src/main/java/com/bulletjournal/repository/BookingLinkDaoJpa.java
+++ backend/src/main/java/com/bulletjournal/repository/BookingLinkDaoJpa.java
@@ -1,15 +1,23 @@
 package com.bulletjournal.repository;
 
+import com.bulletjournal.controller.models.BookingSlot;
 import com.bulletjournal.controller.models.params.CreateBookingLinkParams;
 import com.bulletjournal.exceptions.ResourceNotFoundException;
 import com.bulletjournal.repository.models.BookingLink;
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Repository
 public class BookingLinkDaoJpa {
+    private static final Gson GSON = new Gson();
+
     @Autowired
     private BookingLinkRepository bookingLinkRepository;
 
@@ -31,6 +39,28 @@ public class BookingLinkDaoJpa {
         bookingLink.setIncludeTaskWithoutDuration(createBookingLinkParams.getIncludeTaskWithoutDuration());
         bookingLink.setSlotSpan(createBookingLinkParams.getSlotSpan());
         bookingLink.setTimezone(createBookingLinkParams.getTimezone());
+        this.bookingLinkRepository.save(bookingLink);
+        return bookingLink;
+    }
+
+    @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
+    public BookingLink updateSlot(String bookingLinkId, BookingSlot slot) {
+        BookingLink bookingLink = this.bookingLinkRepository.findById(bookingLinkId)
+                .orElseThrow(() -> new ResourceNotFoundException("Booking link " + bookingLinkId + " not found"));
+        List<BookingSlot> slots = bookingLink.getSlots() == null ? new ArrayList<>() :
+                Arrays.asList(GSON.fromJson(bookingLink.getSlots(), BookingSlot[].class));
+        // equals check exclude isON?
+        if (!slots.contains(slot)) {
+            slots.add(slot);
+        } else {
+            for (BookingSlot s : slots) {
+                if (s.equals(slot)) {
+                    s.setOn(slot.isOn());
+                }
+            }
+        }
+        String updatedSlots = GSON.toJson(slots);
+        bookingLink.setSlots(updatedSlots);
         this.bookingLinkRepository.save(bookingLink);
         return bookingLink;
     }
