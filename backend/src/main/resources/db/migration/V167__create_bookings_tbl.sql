d04881e8ff28687a2bb84148f4cd3edfe193473b
Dong.Wang <randalldw0125@gmail.com>
2021-05-17 20:59:24 -0400

    Add preliminary booking table

--- backend/src/main/resources/db/migration/V167__create_bookings_tbl.sql
+++ backend/src/main/resources/db/migration/V167__create_bookings_tbl.sql
@@ -1,12 +1,14 @@
-ALTER TABLE  public.booking_links DROP COLUMN  invitees;
+ALTER TABLE public.booking_links DROP COLUMN invitees;
 
 CREATE TABLE public.bookings
 (
-    id               varchar(8) PRIMARY KEY,
-    booking_link_id  varchar(8) NOT NULL,
-    task_id          bigint     NOT NULL,
-    invitee          text       NOT NULL,
-    slot_index       int        NOT NULL
+    id              varchar(8) PRIMARY KEY,
+    booking_link_id varchar(8) NOT NULL,
+    task_id         bigint     NOT NULL,
+    invitee         text       NOT NULL,
+    slot_index      int        NOT NULL,
+    created_at      timestamp  NOT NULL,
+    updated_at      timestamp  NOT NULL
 );
 
 -- booking links id fk
@@ -15,8 +17,8 @@ alter table bookings
         foreign key (booking_link_id) references booking_links
             on delete cascade;
 
--- task id fk. on delete set as null
+-- task id fk.
 alter table bookings
     add constraint bookings_tasks_id_fk
         foreign key (task_id) references tasks
-            on delete set null;
+            on delete cascade;
