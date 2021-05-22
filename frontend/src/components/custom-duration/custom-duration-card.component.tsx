99e869b39ad5808290fbb6ed1b6ff6a72b30d8f5
Xin Yao <singerdmx@gmail.com>
2021-05-21 19:52:22 -0700

    Restrict custom duration for booking

--- frontend/src/components/custom-duration/custom-duration-card.component.tsx
+++ frontend/src/components/custom-duration/custom-duration-card.component.tsx
@@ -16,25 +16,19 @@ type CustomDurationCardProps = {
     setCurrentSlotSpan: (span: number) => void,
 }
 
-const getHours = () => {
-    const ans = [];
-    for (let i = 0; i <= 24; i++) {
-        ans.push(i);
-    }
-    return ans;
-}
-
-const hours = getHours();
+const hours = [0, 1, 2, 3, 4, 6, 8, 12, 24];
 
 const hourOptions = hours.map(hour => <Option value={(hour).toString()} key={(hour).toString()}>{hour}</Option>);
-const mins = [0, 15, 30, 45];
-const minOptions = mins.map(min => <Option value={(min).toString()} key={(min).toString()}>{min}</Option>);
 
 const CustomDurationCard: React.FC<CustomDurationCardProps> = (props) => {
     const {backgroundColor, img, imgHeight, imgWidth, setCardIsClicked, setDisableCreateNewProjectOrBooking, setCurrentSlotSpan} = props;
+    const getMinOptions = (mins : number[]) => {
+        return mins.map(min => <Option value={(min).toString()} key={(min).toString()}>{min}</Option>);
+    }
     const [hr, setHr] = useState("0");
     const [min, setMin] = useState("45");
     const [error, setError] = useState();
+    const [minOptions, setMinOptions] = useState(getMinOptions([45]));
 
     const invalidInput = () => {
         return (hr === "0" && min === "0");
@@ -64,13 +58,29 @@ const CustomDurationCard: React.FC<CustomDurationCardProps> = (props) => {
             <div style={{color: "red", marginBottom:"1px"}}>{error}</div>
             <span>
                 <Select defaultValue="0" style={{width: 60}}
-                        onChange={(value: string) => setHr(value)}
+                        onChange={(value: string) => {
+                            setHr(value);
+                            switch (value) {
+                                case "0":
+                                    setMinOptions(getMinOptions([45]));
+                                    setMin("45");
+                                    break;
+                                case "1":
+                                    setMinOptions(getMinOptions([0, 30]));
+                                    setMin("0");
+                                    break;
+                                default:
+                                    setMin("0");
+                                    setMinOptions(getMinOptions([0]));
+                            }
+
+                        }}
                         onClick={(e) =>  e.stopPropagation()}
                 >
                     {hourOptions}
                 </Select>
                 {' '}hr{' '}
-                <Select defaultValue="45" style={{width: 60}}
+                <Select value={min} style={{width: 60}}
                         onChange={(value: string) => setMin(value)}
                         onClick={(e) =>  e.stopPropagation()}
                 >
