a7d3310e3e8a5d2da65d4ff2508df4242f509c3d
Xin Yao <singerdmx@gmail.com>
2021-05-21 21:40:46 -0700

    Update book me drawer

--- frontend/src/components/book-me/book-me-drawer.tsx
+++ frontend/src/components/book-me/book-me-drawer.tsx
@@ -1,6 +1,6 @@
 import {IState} from "../../store";
 import {connect} from "react-redux";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {AutoComplete, Avatar, Button, Checkbox, DatePicker, Drawer, Input, Select, Tooltip} from "antd";
 import moment from "moment";
 import {dateFormat} from "../../features/myBuJo/constants";
@@ -10,6 +10,8 @@ import {Project} from "../../features/project/interface";
 import {zones} from "../settings/constants";
 import './book-me.styles.less';
 import {iconMapper} from "../side-menu/side-menu.component";
+import ReactQuill from "react-quill";
+import {formats, modules} from "../content-editor/content-editor-toolbar";
 
 const {Option} = Select;
 
@@ -24,6 +26,7 @@ const {RangePicker} = DatePicker;
 
 const BookMeDrawer: React.FC<BookMeDrawerProps> = (props) => {
     const {setBookMeDrawerVisible, bookMeDrawerVisible, link, projects} = props;
+    const quillRef = useRef<ReactQuill>(null);
     const [dateArray, setDateArray] = useState(['', '']);
     const [location, setLocation] = useState();
     const [timezone, setTimezone] = useState();
@@ -126,7 +129,7 @@ const BookMeDrawer: React.FC<BookMeDrawerProps> = (props) => {
                             dateArray[0] ? [moment(dateArray[0]), moment(dateArray[1])] : null
                         }
                         size="small"
-                        allowClear={true}
+                        allowClear={false}
                         format={dateFormat}
                         placeholder={['Start Date', 'End Date']}
                         onChange={handleRangeChange}
@@ -145,6 +148,23 @@ const BookMeDrawer: React.FC<BookMeDrawerProps> = (props) => {
                             </Option>
                         ))}
                     </Select>
+                </div>
+                <div className="location booking-option-container">
+                    <span style={{marginRight: "10px"}}>Where{' '}
+                        <Tooltip
+                            title={<span>Use the "Where" field to specify how and where both parties will connect at the scheduled time.<br/><br/>What is entered here will appear on the confirmation page after events are scheduled and in the calendar event added to both you and your invitee's calendars.</span>}
+                        >
+                            <span className="question-icon">
+                                <QuestionCircleOutlined/>
+                            </span>
+                        </Tooltip>
+                    </span>
+                    <Input
+                        placeholder="web link, phone number or address"
+                        value={location}
+                        style={{width: "250px"}}
+                        onChange={(e) => handleLocationChange(e.target.value)}
+                    />
                 </div>
                 <span>Want to add time before or after your events?{' '}
                     <Tooltip
@@ -178,23 +198,6 @@ const BookMeDrawer: React.FC<BookMeDrawerProps> = (props) => {
                             <Input/>
                         </AutoComplete>
                     </div>
-                </div>
-                <div className="location booking-option-container">
-                    <span style={{marginRight: "10px"}}>Where{' '}
-                        <Tooltip
-                            title={<span>Use the "Where" field to specify how and where both parties will connect at the scheduled time.<br/><br/>What is entered here will appear on the confirmation page after events are scheduled and in the calendar event added to both you and your invitee's calendars.</span>}
-                        >
-                            <span className="question-icon">
-                                <QuestionCircleOutlined/>
-                            </span>
-                        </Tooltip>
-                    </span>
-                    <Input
-                        placeholder="web link, phone number or physical address"
-                        value={location}
-                        style={{width: "350px"}}
-                        onChange={(e) => handleLocationChange(e.target.value)}
-                    />
                 </div>
                 <div className="checkboxes booking-option-container">
                     <Checkbox
@@ -237,6 +240,36 @@ const BookMeDrawer: React.FC<BookMeDrawerProps> = (props) => {
                             );
                         })}
                     </Select>
+                </div>
+                <div className='note-editor'>
+                    <div className='note-editor-title'>
+                        Do Not Schedule&nbsp;&nbsp;
+                        <Tooltip
+                            title="Unavailable time on calendar">
+                                    <span className="question-icon">
+                                    <QuestionCircleOutlined/>
+                                </span>
+                        </Tooltip>
+                    </div>
+                </div>
+                <div className='note-editor'>
+                    <div className='note-editor-title'>
+                        Description/Instructions&nbsp;&nbsp;
+                        <Tooltip
+                            title="Use this optional field to provide a description of your event">
+                                    <span className="question-icon">
+                                    <QuestionCircleOutlined/>
+                                </span>
+                        </Tooltip>
+                    </div>
+                    <ReactQuill
+                        bounds={'.note-editor'}
+                        ref={quillRef}
+                        theme="snow"
+                        modules={modules}
+                        formats={formats}
+                        style={{height: '70px'}}
+                    />
                 </div>
                 <div className="buttons" style={{marginTop: "50px", paddingTop: "10px"}}>
                     <Button
