f16855eebfb9d28c0a838ac6e91a27f967c4719f
Xin Yao <singerdmx@gmail.com>
2021-05-22 00:53:39 -0700

    UI to update BookMeUsername

--- frontend/src/pages/book-me/manage-booking.pages.tsx
+++ frontend/src/pages/book-me/manage-booking.pages.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Empty, Input, Tooltip} from "antd";
-import {QuestionCircleOutlined} from "@ant-design/icons";
+import {CheckCircleOutlined, CloseCircleOutlined, QuestionCircleOutlined} from "@ant-design/icons";
 
 import './book-me.styles.less';
 import {IState} from "../../store";
@@ -17,6 +17,26 @@ const ManageBooking: React.FC<ManageBookingProps> = (
         bookMeUsername
     }
 ) => {
+    const [name, setName] = useState(bookMeUsername ? bookMeUsername : myself);
+    const [nameChanged, setNameChanged] = useState(false);
+
+    useEffect(() => {
+        if (!bookMeUsername) {
+            // make GET request to retrieve
+        } else {
+            setName(bookMeUsername);
+        }
+    }, [bookMeUsername]);
+
+    const handleOnClick = (save: boolean) => {
+        if (save) {
+            // make PUT request to update bookMeUsername
+        } else {
+            setName(bookMeUsername);
+        }
+        setNameChanged(false);
+    }
+
     return <div>
         <div className='book-me-name'>
             Name&nbsp;&nbsp;
@@ -26,7 +46,32 @@ const ManageBooking: React.FC<ManageBookingProps> = (
                                     <QuestionCircleOutlined/>
                                 </span>
             </Tooltip>
-            <Input className='book-me-name-input' value={bookMeUsername ? bookMeUsername : myself}/>
+            <Input className='book-me-name-input' value={name} onChange={(e) => {
+                setNameChanged(true);
+                setName(e.target.value);
+            }}/>
+            <Tooltip placement="top" title='Save'>
+                <CheckCircleOutlined
+                    onClick={() => handleOnClick(true)}
+                    style={{
+                        marginLeft: '20px',
+                        cursor: 'pointer',
+                        color: '#00e600',
+                        fontSize: 20,
+                        visibility: nameChanged ? 'visible' : 'hidden'
+                    }} />
+            </Tooltip>
+            <Tooltip placement="top" title='Cancel'>
+                <CloseCircleOutlined
+                    onClick={() => handleOnClick(false)}
+                    style={{
+                        marginLeft: '20px',
+                        cursor: 'pointer',
+                        color: '#ff0000',
+                        fontSize: 20,
+                        visibility: nameChanged ? 'visible' : 'hidden'
+                    }} />
+            </Tooltip>
         </div>
         <Empty/>
     </div>
