7788f82d365cd53dd26c41da8e943d6e80d1ba33
Xin Yao <singerdmx@gmail.com>
2021-05-23 23:25:34 -0700

    Able to flipt slots in calendar

--- frontend/src/components/book-me/book-me-calendar.component.tsx
+++ frontend/src/components/book-me/book-me-calendar.component.tsx
@@ -1,10 +1,10 @@
-import React, {useState} from "react";
-import {Calendar} from "antd";
+import React, {useEffect, useState} from "react";
+import {Calendar, Tooltip} from "antd";
 
 import './book-me-calendar.styles.less';
 import moment from "moment";
-import {BookingLink} from "../../features/bookingLink/interface";
-import {SwapRightOutlined} from "@ant-design/icons";
+import {BookingLink, Slot} from "../../features/bookingLink/interface";
+import {CheckCircleOutlined, CloseCircleOutlined, SwapRightOutlined} from "@ant-design/icons";
 import {inPublicPage} from "../../index";
 
 type BookMeCalendarProps = {
@@ -16,10 +16,22 @@ const BookMeCalendar: React.FC<BookMeCalendarProps> = (
         link
     }
 ) => {
+    const isPublic = inPublicPage();
     const [selectedDate, setSelectedDate] = useState(moment(link.startDate));
-    const [slots, setSlots] = useState(link.slots.filter(d => d.displayDate === link.startDate));
+    const [slots, setSlots] = useState(link.slots.filter(d => {
+        if (isPublic && !d.on) {
+            return false;
+        }
+        return d.displayDate === link.startDate;
+    }));
+    const [visibleSlot, setVisibleSlot] = useState('');
+
+    useEffect(() => {
+        onDateChange(moment(link.startDate));
+    }, [link]);
 
     function onDateChange(date: moment.Moment) {
+        setVisibleSlot('');
         if (date.isAfter(moment(link.endDate))) {
             date = moment(link.endDate);
         }
@@ -28,11 +40,70 @@ const BookMeCalendar: React.FC<BookMeCalendarProps> = (
         }
         setSelectedDate(date);
         setSlots(link.slots.filter(d => {
-            if (inPublicPage() && !d.on) {
+            if (isPublic && !d.on) {
                 return false;
             }
             return d.displayDate === date.format('YYYY-MM-DD')
         }));
+    }
+
+    function handleOnSlotClick(slot: Slot) {
+        setVisibleSlot(slot.date + '#' + slot.index);
+    }
+
+    function markSlot(slot: Slot, on: boolean) {
+        // send API request to mark
+        setVisibleSlot('');
+    }
+
+    function getSlotButtons(slot: Slot) {
+        if (slot.date + '#' + slot.index !== visibleSlot) {
+            return <></>;
+        }
+        if (isPublic) {
+            return <div className='slot-button'>
+                <Tooltip placement="top" title="Book">
+                    <CheckCircleOutlined
+                        // onClick={() => this.handleOnThemeClick(true)}
+                        style={{
+                            marginLeft: '20px',
+                            cursor: 'pointer',
+                            color: '#00e600',
+                            fontSize: 20
+                        }}
+                    />
+                </Tooltip>
+            </div>
+        }
+
+        if (slot.on) {
+            return <div className='slot-button'>
+                <Tooltip placement="top" title="Change to unavailable">
+                    <CloseCircleOutlined
+                        onClick={() => markSlot(slot, false)}
+                        style={{
+                            marginLeft: '20px',
+                            cursor: 'pointer',
+                            color: '#ff0000',
+                            fontSize: 20,
+                        }}
+                    />
+                </Tooltip>
+            </div>
+        }
+        return <div className='slot-button'>
+                <Tooltip placement="top" title="Change to available">
+                    <CheckCircleOutlined
+                        onClick={() => markSlot(slot, true)}
+                        style={{
+                            marginLeft: '20px',
+                            cursor: 'pointer',
+                            color: '#00e600',
+                            fontSize: 20,
+                        }}
+                    />
+                </Tooltip>
+            </div>;
     }
 
     return <div className='book-me-calendar'>
@@ -45,8 +116,11 @@ const BookMeCalendar: React.FC<BookMeCalendarProps> = (
         <div className='selector-div'>
             <div className='slots'>
                 {slots.map(slot => {
-                    return <div className='slot' style={{backgroundColor: `${slot.on ? '#52c41a' : '#f78cba'}`}}>
-                        {slot.startTime}<SwapRightOutlined />{slot.endTime}
+                    return <div className='slot-card' onClick={() => handleOnSlotClick(slot)}>
+                        <div className='slot' style={{backgroundColor: `${slot.on ? '#52c41a' : '#f78cba'}`}}>
+                            {slot.startTime}<SwapRightOutlined/>{slot.endTime}
+                        </div>
+                        {getSlotButtons(slot)}
                     </div>
                 })}
             </div>
