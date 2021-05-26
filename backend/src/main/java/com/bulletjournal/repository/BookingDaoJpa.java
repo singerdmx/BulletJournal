3b9d70bdf6b0c86499e4ed73e52ec67871e3b301
Xin Yao <singerdmx@gmail.com>
2021-05-26 15:52:13 -0700

    Small fix

--- backend/src/main/java/com/bulletjournal/repository/BookingDaoJpa.java
+++ backend/src/main/java/com/bulletjournal/repository/BookingDaoJpa.java
@@ -7,7 +7,6 @@ import com.bulletjournal.controller.utils.ZonedDateTimeHelper;
 import com.bulletjournal.repository.models.Booking;
 import com.bulletjournal.repository.models.BookingLink;
 import com.bulletjournal.repository.models.Task;
-import com.bulletjournal.util.StringUtil;
 import com.google.common.collect.ImmutableList;
 import com.google.gson.Gson;
 import org.apache.commons.lang3.RandomStringUtils;
